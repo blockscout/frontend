8ab8cf0a1589cb381c20bd7f1f575ab678590aa2
Natalia Stus <natix@yandex-team.ru>
2022-06-10 11:34:14 +0300

    watchlist fixes

--- components/CopyToClipboard/CopyToClipboard.tsx
+++ components/CopyToClipboard/CopyToClipboard.tsx
@@ -3,7 +3,7 @@ import React, { useCallback, useEffect } from 'react';
 import { Icon, useClipboard, useToast } from '@chakra-ui/react';
 import { FaCopy } from 'react-icons/fa';
 
-const WatchListAddressItem = ({ text }: {text: string}) => {
+const CopyToClipboard = ({ text }: {text: string}) => {
   const { hasCopied, onCopy } = useClipboard(text);
   const toast = useToast();
 
@@ -21,4 +21,4 @@ const WatchListAddressItem = ({ text }: {text: string}) => {
   return <Icon as={ FaCopy } w="15px" h="15px" color="blue.500" cursor="pointer" onClick={ copyToClipboardCallback }/>;
 }
 
-export default WatchListAddressItem;
+export default CopyToClipboard;
