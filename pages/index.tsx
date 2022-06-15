a9d1521a6a04e199189728943d8e6371f9f81a3f
tom <tom@ohhhh.me>
2022-06-15 21:18:44 +0300

    use customized Input in modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import { Center, FormControl, Input, FormLabel } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
 import Page from '../components/Page/Page';
 
 const Home: NextPage = () => {
   return (
     <Page>
       <Center h="100%">
-        <FormControl variant="floating" id="address" isRequired>
-          { /* Placeholder should be present */ }
-          <Input placeholder=" "/>
-          { /* It is important that the Label comes after the Control due to css selectors */ }
-          <FormLabel>Address</FormLabel>
-        </FormControl>
+        Home Page
       </Center>
     </Page>
   );
