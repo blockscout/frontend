a71b855613dab74dd55bbe9a47e2262c9ef8d440
tom <tom@ohhhh.me>
2022-07-18 19:09:09 +0300

    fix errors

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withReactSvg = require('next-react-svg');
+import path from 'path';
+
+module.exports = withReactSvg({
+  include: path.resolve(__dirname, 'icons'),
+  reactStrictMode: true,
+  webpack(config: unknown) {
+    return config
+  },
+})
