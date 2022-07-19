0127bc420f4cf50cca91c35319b4bcfef87afde4
tom <tom@ohhhh.me>
2022-07-19 22:57:12 +0300

    hmmm

--- pages/api-helpers/getDefaultHeaders.ts
+++ pages/api-helpers/getDefaultHeaders.ts
@@ -1,7 +1,7 @@
 export default function getDefaultHeaders() {
   return {
-    Accept: 'application/json',
-    Authorization: `Bearer ${ process.env.API_AUTHORIZATION_TOKEN }`,
-    'Content-type': 'application/json',
+    accept: 'application/json',
+    authorization: `Bearer ${ process.env.API_AUTHORIZATION_TOKEN }`,
+    'content-type': 'application/json',
   }
 }
