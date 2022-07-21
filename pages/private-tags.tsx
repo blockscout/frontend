74c8890558841f817b2ba0d1d1b9d91be1a486d0
tom <tom@ohhhh.me>
2022-07-21 18:41:31 +0300

    ssr - no, csr - yes

--- pages/private-tags.tsx
+++ pages/private-tags.tsx
@@ -2,16 +2,7 @@ import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head'
 
-import fetch from 'api/utils/fetch';
-
 import PrivateTags from 'ui/pages/PrivateTags';
-
-export async function getServerSideProps() {
-  const response = await fetch('/account/v1/user/tags/address')
-  const data = await response.json()
-
-  return { props: { data } }
-}
 
 const PrivateTagsPage: NextPage = () => {
   return (
