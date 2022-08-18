60a9e90519a8e0e35f4be231de95b414c7eca72f
isstuev <isstuev@gmail.com>
2022-08-18 21:33:10 +0300

    address-link

--- ui/shared/AddressLinkWithTooltip.tsx
+++ ui/shared/AddressLinkWithTooltip.tsx
@@ -1,16 +1,21 @@
 import { HStack, Link } from '@chakra-ui/react';
 import React from 'react';
 
+import useBasePath from 'lib/hooks/useBasePath';
+
 import AddressWithDots from './AddressWithDots';
 import CopyToClipboard from './CopyToClipboard';
 
 const FONT_WEIGHT = '600';
 
 const AddressLinkWithTooltip = ({ address }: {address: string}) => {
+  const basePath = useBasePath();
+  const url = basePath + '/address/' + address + '/tokens#address-tabs';
   return (
     <HStack spacing={ 2 } alignContent="center" overflow="hidden">
       <Link
-        href="#"
+        href={ url }
+        target="_blank"
         overflow="hidden"
         fontWeight={ FONT_WEIGHT }
         lineHeight="24px"
