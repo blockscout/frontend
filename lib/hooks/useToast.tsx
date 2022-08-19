e8d28615b20af1d5fe1040725d473b4505221e78
tom <tom@ohhhh.me>
2022-08-19 17:12:44 +0300

    clean-up

--- lib/hooks/useToast.tsx
+++ lib/hooks/useToast.tsx
@@ -1,10 +1,13 @@
-import type { UseToastOptions } from '@chakra-ui/react';
+import type { UseToastOptions, ToastProps } from '@chakra-ui/react';
 import { createToastFn, useChakra } from '@chakra-ui/react';
 import React from 'react';
 
 import Toast from 'ui/shared/Toast';
 
-const defaultOptions = {
+// there is no toastComponent prop in UseToastOptions type
+// but these options will be passed to createRenderToast under the hood
+// and it accepts custom toastComponent
+const defaultOptions: UseToastOptions & { toastComponent?: React.FC<ToastProps> } = {
   toastComponent: Toast,
   position: 'top-right',
   duration: 10000000,
@@ -15,11 +18,7 @@ export default function useToastModified() {
   const { theme } = useChakra();
 
   return React.useMemo(
-    // there is no toastComponent prop in UseToastOptions type
-    // but these options will be passed to createRenderToast under the hood
-    // and it accepts custom toastComponent
-    // so we safely (I hope) do this type conversion
-    () => createToastFn(theme.direction, defaultOptions as UseToastOptions),
+    () => createToastFn(theme.direction, defaultOptions),
     [ theme.direction ],
   );
 }
