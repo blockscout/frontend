cfed09f1a4456d3ebb375b111fbaff1baa7a9121
tom <tom@ohhhh.me>
2022-08-30 22:46:51 +0300

    fix ts

--- pages/api/account/profile/index.ts
+++ pages/api/account/profile/index.ts
@@ -2,6 +2,6 @@ import type { UserInfo } from 'types/api/account';
 
 import handler from 'lib/api/handler';
 
-const profileHandler = handler<UserInfo>(() => '/account/v1/user/info', [ 'GET' ]);
+const profileHandler = handler<UserInfo, unknown>(() => '/account/v1/user/info', [ 'GET' ]);
 
 export default profileHandler;
