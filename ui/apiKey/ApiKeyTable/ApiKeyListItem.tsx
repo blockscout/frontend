b3bcc87a67270f56c510f44e1cc2ecbe9807a137
tom <tom@ohhhh.me>
2022-09-02 12:12:14 +0300

    key icon for api keys

--- ui/apiKey/ApiKeyTable/ApiKeyListItem.tsx
+++ ui/apiKey/ApiKeyTable/ApiKeyListItem.tsx
@@ -1,10 +1,9 @@
-import { HStack, Text, Box } from '@chakra-ui/react';
 import React, { useCallback } from 'react';
 
 import type { ApiKey } from 'types/api/account';
 
 import AccountListItemMobile from 'ui/shared/AccountListItemMobile';
-import CopyToClipboard from 'ui/shared/CopyToClipboard';
+import ApiKeySnippet from 'ui/shared/ApiKeySnippet';
 import TableItemActionButtons from 'ui/shared/TableItemActionButtons';
 
 interface Props {
@@ -25,13 +24,7 @@ const ApiKeyListItem = ({ item, onEditClick, onDeleteClick }: Props) => {
 
   return (
     <AccountListItemMobile>
-      <Box>
-        <HStack alignItems="flex-start">
-          <Text fontSize="md" fontWeight={ 600 }>{ item.api_key }</Text>
-          <CopyToClipboard text={ item.api_key }/>
-        </HStack>
-        <Text fontSize="sm" marginTop={ 0.5 } variant="secondary">{ item.name }</Text>
-      </Box>
+      <ApiKeySnippet apiKey={ item.api_key } name={ item.name }/>
       <TableItemActionButtons onDeleteClick={ onItemDeleteClick } onEditClick={ onItemEditClick }/>
     </AccountListItemMobile>
   );
--- ui/apiKey/ApiKeyTable/ApiKeyTableItem.tsx
+++ ui/apiKey/ApiKeyTable/ApiKeyTableItem.tsx
@@ -1,14 +1,12 @@
 import {
   Tr,
   Td,
-  HStack,
-  Text,
 } from '@chakra-ui/react';
 import React, { useCallback } from 'react';
 
 import type { ApiKey } from 'types/api/account';
 
-import CopyToClipboard from 'ui/shared/CopyToClipboard';
+import ApiKeySnippet from 'ui/shared/ApiKeySnippet';
 import TableItemActionButtons from 'ui/shared/TableItemActionButtons';
 
 interface Props {
@@ -30,11 +28,7 @@ const ApiKeyTableItem = ({ item, onEditClick, onDeleteClick }: Props) => {
   return (
     <Tr alignItems="top" key={ item.api_key }>
       <Td>
-        <HStack>
-          <Text fontSize="md" fontWeight={ 600 }>{ item.api_key }</Text>
-          <CopyToClipboard text={ item.api_key }/>
-        </HStack>
-        <Text fontSize="sm" marginTop={ 0.5 } variant="secondary">{ item.name }</Text>
+        <ApiKeySnippet apiKey={ item.api_key } name={ item.name }/>
       </Td>
       <Td>
         <TableItemActionButtons onDeleteClick={ onItemDeleteClick } onEditClick={ onItemEditClick }/>
