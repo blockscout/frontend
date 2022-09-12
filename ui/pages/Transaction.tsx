3c4a964faecbe36382c7008b56b8a995ad4a660e
isstuev <isstuev@gmail.com>
2022-09-12 19:13:44 +0300

    tx raw trace

--- ui/pages/Transaction.tsx
+++ ui/pages/Transaction.tsx
@@ -14,6 +14,7 @@ import PageHeader from 'ui/shared/PageHeader';
 import TxDetails from 'ui/tx/TxDetails';
 import TxInternals from 'ui/tx/TxInternals';
 import TxLogs from 'ui/tx/TxLogs';
+import TxRawTrace from 'ui/tx/TxRawTrace';
 
 interface Tab {
   type: 'details' | 'internal_txn' | 'logs' | 'raw_trace' | 'state';
@@ -27,7 +28,7 @@ const TABS: Array<Tab> = [
   { type: 'internal_txn', path: '/internal-transactions', name: 'Internal txn', component: <TxInternals/> },
   { type: 'logs', path: '/logs', name: 'Logs', component: <TxLogs/> },
   { type: 'state', path: '/state', name: 'State' },
-  { type: 'raw_trace', path: '/raw-trace', name: 'Raw trace' },
+  { type: 'raw_trace', path: '/raw-trace', name: 'Raw trace', component: <TxRawTrace/> },
 ];
 
 export interface Props {
