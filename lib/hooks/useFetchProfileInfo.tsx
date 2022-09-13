3998f76558648d7b181c00818ee0f7ac558a5398
tom <tom@ohhhh.me>
2022-09-13 18:23:15 +0300

    limit retries for user profile

--- lib/hooks/useFetchProfileInfo.tsx
+++ lib/hooks/useFetchProfileInfo.tsx
@@ -4,8 +4,25 @@ import type { UserInfo } from 'types/api/account';
 
 import fetch from 'lib/client/fetch';
 
+interface Error {
+  error?: {
+    status?: number;
+    statusText?: string;
+  };
+}
+
 export default function useFetchProfileInfo() {
-  return useQuery<unknown, unknown, UserInfo>([ 'profile' ], async() => {
+  return useQuery<unknown, Error, UserInfo>([ 'profile' ], async() => {
     return fetch('/api/account/profile');
-  }, { refetchOnMount: false });
+  }, {
+    refetchOnMount: false,
+    retry: (failureCount, error) => {
+      if (error?.error?.status === 401) {
+        // for unauthorized users don't do retry
+        return false;
+      }
+
+      return failureCount < 2;
+    },
+  });
 }
