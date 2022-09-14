c14b164b0a8a535040ebf4ee45900806b25b5abb
tom <tom@ohhhh.me>
2022-09-14 11:07:14 +0300

    correct links for network with no sub type

--- lib/link/link.ts
+++ lib/link/link.ts
@@ -1,4 +1,5 @@
 import isBrowser from 'lib/isBrowser';
+import findNetwork from 'lib/networks/findNetwork';
 
 import { ROUTES } from './routes';
 import type { RouteName } from './routes';
@@ -11,7 +12,16 @@ export function link(routeName: RouteName, urlParams?: Record<string, string | u
     return '';
   }
 
+  const network = findNetwork({
+    network_type: urlParams?.network_type || '',
+    network_sub_type: urlParams?.network_sub_type,
+  });
+
   const path = route.pattern.replace(PATH_PARAM_REGEXP, (_, paramName: string) => {
+    if (paramName === 'network_sub_type' && !network?.subType) {
+      return '';
+    }
+
     const paramValue = urlParams?.[paramName];
     return paramValue ? `/${ paramValue }` : '';
   });
