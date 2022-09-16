12d88139d98ddbededef283d9a600849493deafd
Yuri Mikhin <urasergeevich@mail.ru>
2022-09-16 15:08:19 +0300

    Add apps filtering.

--- ui/apps/AppList.tsx
+++ ui/apps/AppList.tsx
@@ -1,4 +1,5 @@
-import { Text, Grid, GridItem, VisuallyHidden } from '@chakra-ui/react';
+import { Box, Text, Grid, GridItem, VisuallyHidden, Icon } from '@chakra-ui/react';
+import emptyIcon from '~/icons/empty.svg';
 import AppCard from '~/ui/apps/AppCard';
 import React from 'react';
 
@@ -15,28 +16,59 @@ const AppList = ({ apps }: Props) => {
         <Text as="h2">App list</Text>
       </VisuallyHidden>
 
-      <Grid
-        templateColumns={{
-          base: 'repeat(auto-fill, minmax(140px, 1fr))',
-          lg: 'repeat(auto-fill, minmax(260px, 1fr))',
-        }}
-        autoRows="1fr"
-        gap={{ base: '1px', lg: '24px' }}
-      >
-        { apps.map((app) => (
-          <GridItem
-            key={ app.id }
+      { apps.length > 0 ? (
+        <Grid
+          templateColumns={{
+            base: 'repeat(auto-fill, minmax(140px, 1fr))',
+            lg: 'repeat(auto-fill, minmax(260px, 1fr))',
+          }}
+          autoRows="1fr"
+          gap={{ base: '1px', lg: '24px' }}
+        >
+          { apps.map((app) => (
+            <GridItem
+              key={ app.id }
+            >
+              <AppCard
+                id={ app.id }
+                title={ app.title }
+                logo={ app.logo }
+                shortDescription={ app.shortDescription }
+                categories={ app.categories }
+              />
+            </GridItem>
+          )) }
+        </Grid>
+      ) : (
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Icon
+            as={ emptyIcon }
+            boxSize={ 60 }
+            display="block"
+          />
+
+          <Text
+            as="h3"
+            marginBottom={ 2 }
+            fontSize={{ base: '2xl', lg: '3xl' }}
+            fontWeight="semibold"
           >
-            <AppCard
-              id={ app.id }
-              title={ app.title }
-              logo={ app.logo }
-              shortDescription={ app.shortDescription }
-              categories={ app.categories }
-            />
-          </GridItem>
-        )) }
-      </Grid>
+            No results
+          </Text>
+
+          <Text
+            fontSize={{ base: 'sm' }}
+            variant="secondary"
+            align="center"
+          >
+              Couldn&apos;t find an app that matches your filter query.
+          </Text>
+        </Box>
+      ) }
     </>
   );
 };
