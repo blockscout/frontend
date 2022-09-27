524c68a7c4e226b79434ede18df26021072cb8b6
tom <tom@ohhhh.me>
2022-09-27 16:57:07 +0300

    internal tx sort

--- ui/tx/internals/TxInternalsTable.tsx
+++ ui/tx/internals/TxInternalsTable.tsx
@@ -1,10 +1,20 @@
-import { Table, Thead, Tbody, Tr, Th, TableContainer } from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tr, Th, TableContainer, Link, Icon } from '@chakra-ui/react';
 import React from 'react';
 
 import type { data as txData } from 'data/txInternal';
+import arrowIcon from 'icons/arrows/east.svg';
 import TxInternalsTableItem from 'ui/tx/internals/TxInternalsTableItem';
+import type { Sort, SortField } from 'ui/tx/internals/utils';
 
-const TxInternalsTable = ({ data }: { data: typeof txData}) => {
+interface Props {
+  data: typeof txData;
+  sort: Sort | undefined;
+  onSortToggle: (field: SortField) => () => void;
+}
+
+const TxInternalsTable = ({ data, sort, onSortToggle }: Props) => {
+  const sortIconTransform = sort?.includes('asc') ? 'rotate(-90deg)' : 'rotate(90deg)';
+
   return (
     <TableContainer width="100%" mt={ 6 }>
       <Table variant="simple" size="sm">
@@ -14,8 +24,18 @@ const TxInternalsTable = ({ data }: { data: typeof txData}) => {
             <Th width="20%">From</Th>
             <Th width="24px" px={ 0 }/>
             <Th width="20%">To</Th>
-            <Th width="16%" isNumeric>Value</Th>
-            <Th width="16%" isNumeric>Gas limit</Th>
+            <Th width="16%" isNumeric>
+              <Link display="flex" alignItems="center" justifyContent="flex-end" onClick={ onSortToggle('value') } columnGap={ 1 }>
+                { sort?.includes('value') && <Icon as={ arrowIcon } boxSize={ 4 } transform={ sortIconTransform }/> }
+                Value
+              </Link>
+            </Th>
+            <Th width="16%" isNumeric>
+              <Link display="flex" alignItems="center" justifyContent="flex-end" onClick={ onSortToggle('gas-limit') } columnGap={ 1 }>
+                { sort?.includes('gas-limit') && <Icon as={ arrowIcon } boxSize={ 4 } transform={ sortIconTransform }/> }
+                Gas limit
+              </Link>
+            </Th>
           </Tr>
         </Thead>
         <Tbody>
