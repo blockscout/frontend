c2879a56220809a7984821beb99fc17a34498cb5
tom <tom@ohhhh.me>
2022-09-28 11:44:09 +0300

    details tab desktop

--- ui/pages/Block.tsx
+++ ui/pages/Block.tsx
@@ -1,13 +1,15 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import type { RoutedTab } from 'ui/shared/RoutedTabs/types';
 
+import BlockDetails from 'ui/block/BlockDetails';
 import Page from 'ui/shared/Page';
 import PageHeader from 'ui/shared/PageHeader';
 import RoutedTabs from 'ui/shared/RoutedTabs/RoutedTabs';
 
 const TABS: Array<RoutedTab> = [
-  { routeName: 'block_index', title: 'Details', component: <div>Details</div> },
+  { routeName: 'block_index', title: 'Details', component: <BlockDetails/> },
   { routeName: 'block_txs', title: 'Transactions', component: <div>Transactions</div> },
 ];
 
@@ -16,9 +18,11 @@ export interface Props {
 }
 
 const BlockPageContent = ({ tab }: Props) => {
+  const router = useRouter();
+
   return (
     <Page>
-      <PageHeader text="Block"/>
+      <PageHeader text={ `Block #${ router.query.id || '' }` }/>
       <RoutedTabs
         tabs={ TABS }
         defaultActiveTab={ tab }
