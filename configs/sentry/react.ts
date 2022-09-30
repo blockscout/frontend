5b5c0274790ce2a698d9625a4e8321e22937a810
tom <tom@ohhhh.me>
2022-09-30 13:55:18 +0300

    add release to sentry

--- configs/sentry/react.ts
+++ configs/sentry/react.ts
@@ -4,6 +4,7 @@ import { BrowserTracing } from '@sentry/tracing';
 export const config: Sentry.BrowserOptions = {
   environment: process.env.VERCEL_ENV || process.env.NODE_ENV,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  release: process.env.NEXT_PUBLIC_GIT_COMMIT_SHA || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || 'unknown',
   integrations: [ new BrowserTracing() ],
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
