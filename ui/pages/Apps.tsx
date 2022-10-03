fc4cd5faf469810ee70403489ec932db6a69af2e
Yuri Mikhin <urasergeevich@mail.ru>
2022-10-03 17:29:55 +0300

    Add link to dapp submission form for the marketplace.

--- ui/pages/Apps.tsx
+++ ui/pages/Apps.tsx
@@ -1,9 +1,11 @@
+import { Icon, Link } from '@chakra-ui/react';
 import debounce from 'lodash/debounce';
 import React, { useCallback, useState } from 'react';
 
 import type { AppItemOverview } from 'types/client/apps';
 
 import { TEMPORARY_DEMO_APPS } from 'data/apps';
+import PlusIcon from 'icons/plus.svg';
 import AppList from 'ui/apps/AppList';
 import FilterInput from 'ui/shared/FilterInput';
 
@@ -33,12 +35,33 @@ const Apps = () => {
   return (
     <>
       <FilterInput onChange={ debounceFilterApps } marginBottom={{ base: '4', lg: '6' }} placeholder="Find app"/>
+
       <AppList
         apps={ displayedApps }
         onAppClick={ showAppInfo }
         displayedAppId={ displayedAppId }
         onModalClose={ clearDisplayedAppId }
       />
+
+      { process.env.NEXT_PUBLIC_MARKETPLACE_SUBMIT_FORM && (
+        <Link
+          fontWeight="bold"
+          display="inline-flex"
+          alignItems="baseline"
+          marginTop={{ base: 8, sm: 16 }}
+          href={ process.env.NEXT_PUBLIC_MARKETPLACE_SUBMIT_FORM }
+          isExternal
+        >
+          <Icon
+            as={ PlusIcon }
+            w={ 3 }
+            h={ 3 }
+            mr={ 2 }
+          />
+
+            Submit an App
+        </Link>
+      ) }
     </>
   );
 };
