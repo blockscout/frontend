9a8e35f8b0f9c037e72f83adc81baf26d978a270
isstuev <isstuev@gmail.com>
2022-10-04 10:44:26 +0300

    playwright setup and git lfs

---
+++ ui/shared/Utilization.spec.tsx
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/experimental-ct-react';
+import React from 'react';
+
+import RenderWithChakra from '../test-helpers/RenderWithChakra';
+import Utilization from './Utilization';
+
+test.use({ viewport: { width: 100, height: 50 } });
+
+test('Utilization light', async({ mount }) => {
+  const component = await mount(
+    <RenderWithChakra>
+      <Utilization value={ 0.1 }/>
+    </RenderWithChakra>,
+  );
+  await expect(component).toHaveScreenshot();
+});
+
+test('Utilization dark', async({ mount }) => {
+  const component = await mount(
+    <RenderWithChakra colorMode="dark">
+      <Utilization value={ 0.1 }/>
+    </RenderWithChakra>,
+  );
+  await expect(component).toHaveScreenshot();
+});
