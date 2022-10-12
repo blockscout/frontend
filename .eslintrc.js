7fe06036ec495c944947a43a01dfedb40c4b3b38
tom <tom@ohhhh.me>
2022-10-12 14:22:05 +0300

    fix eslint rule application

--- .eslintrc.js
+++ .eslintrc.js
@@ -283,7 +283,7 @@ module.exports = {
       },
     },
     {
-      files: [ 'configs/**/*.js', 'configs/**/*.ts' ],
+      files: [ 'configs/**/*.js', 'configs/**/*.ts', '*.config.ts' ],
       rules: {
         // for configs allow to consume env variables from process.env directly
         'no-restricted-properties': [ 0 ],
