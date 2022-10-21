73b95b5f818f4db09c37d61d4c8424d2c83ace67
tom <tom@ohhhh.me>
2022-10-21 18:48:54 +0300

    blocks layout fix for mobile devices

--- ui/blocks/BlocksListItem.tsx
+++ ui/blocks/BlocksListItem.tsx
@@ -55,7 +55,7 @@ const BlocksListItem = ({ data, isPending, enableTimeIncrement }: Props) => {
       </Flex>
       <Box>
         <Text fontWeight={ 500 }>Gas used</Text>
-        <Flex columnGap={ 4 }>
+        <Flex columnGap={ 4 } mt={ 2 }>
           <Text variant="secondary">{ BigNumber(data.gas_used || 0).toFormat() }</Text>
           <Utilization colorScheme="gray" value={ BigNumber(data.gas_used || 0).div(BigNumber(data.gas_limit)).toNumber() }/>
           <GasUsedToTargetRatio value={ data.gas_target_percentage || undefined }/>
@@ -65,12 +65,16 @@ const BlocksListItem = ({ data, isPending, enableTimeIncrement }: Props) => {
         <Text fontWeight={ 500 }>Reward { appConfig.network.currency.symbol }</Text>
         <Text variant="secondary">{ totalReward.div(WEI).toFixed() }</Text>
       </Flex>
-      <Flex>
+      <Box>
         <Text fontWeight={ 500 }>Burnt fees</Text>
-        <Icon as={ flameIcon } boxSize={ 5 } color="gray.500" ml={ 2 }/>
-        <Text variant="secondary" ml={ 1 }>{ burntFees.div(WEI).toFixed() }</Text>
-        <Utilization ml={ 4 } value={ burntFees.div(txFees).toNumber() }/>
-      </Flex>
+        <Flex columnGap={ 4 } mt={ 2 }>
+          <Flex>
+            <Icon as={ flameIcon } boxSize={ 5 } color="gray.500"/>
+            <Text variant="secondary" ml={ 1 }>{ burntFees.div(WEI).toFixed() }</Text>
+          </Flex>
+          <Utilization ml={ 4 } value={ burntFees.div(txFees).toNumber() }/>
+        </Flex>
+      </Box>
     </AccountListItemMobile>
   );
 };
