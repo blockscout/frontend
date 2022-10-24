410f2f0dcee1a154db3293b3f82220c612674c72
tom <tom@ohhhh.me>
2022-10-24 13:32:35 +0300

    pass search params to api call

--- pages/api/transactions/index.ts
+++ pages/api/transactions/index.ts
@@ -1,13 +1,10 @@
 import type { NextApiRequest } from 'next';
 
+import getSearchParams from 'lib/api/getSearchParams';
 import handler from 'lib/api/handler';
 
 const getUrl = (req: NextApiRequest) => {
-  const searchParams: Record<string, string> = {};
-  Object.entries(req.query).forEach(([ key, value ]) => {
-    searchParams[key] = Array.isArray(value) ? value.join(',') : (value || '');
-  });
-  const searchParamsStr = new URLSearchParams(searchParams).toString();
+  const searchParamsStr = getSearchParams(req);
   return `/v2/transactions/${ searchParamsStr ? '?' + searchParamsStr : '' }`;
 };
 
