f95a0eaac75503ba92c01b33eae3e9161850056b
isstuev <isstuev@gmail.com>
2022-10-25 18:29:52 +0300

    shadows

--- ui/snippets/searchBar/SearchBarMobile.tsx
+++ ui/snippets/searchBar/SearchBarMobile.tsx
@@ -1,9 +1,12 @@
 import { InputGroup, Input, InputLeftElement, Icon, useColorModeValue, chakra } from '@chakra-ui/react';
+import throttle from 'lodash/throttle';
 import React from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
 
 import searchIcon from 'icons/search.svg';
 import useScrollDirection from 'lib/hooks/useScrollDirection';
+
+const TOP = 55;
 
 interface Props {
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
@@ -12,7 +15,30 @@ interface Props {
 
 const SearchBarMobile = ({ onChange, onSubmit }: Props) => {
 
-  const isVisible = useScrollDirection() === 'up';
+  const scrollDirection = useScrollDirection();
+  const isVisible = !scrollDirection || scrollDirection === 'up';
+
+  const [ isSticky, setIsSticky ] = React.useState(false);
+
+  const handleScroll = React.useCallback(() => {
+    if (window.pageYOffset === 0 || !isVisible) {
+      setIsSticky(false);
+    } else {
+      setIsSticky(true);
+    }
+  }, [ isVisible ]);
+
+  React.useEffect(() => {
+    const throttledHandleScroll = throttle(handleScroll, 300);
+
+    window.addEventListener('scroll', throttledHandleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', throttledHandleScroll);
+    };
+  // replicate componentDidMount
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const searchIconColor = useColorModeValue('blackAlpha.600', 'whiteAlpha.600');
   const inputBorderColor = useColorModeValue('blackAlpha.100', 'whiteAlpha.200');
@@ -26,15 +52,16 @@ const SearchBarMobile = ({ onChange, onSubmit }: Props) => {
       paddingTop={ 1 }
       paddingBottom={ 2 }
       position="fixed"
-      top="56px"
+      top={ `${ TOP }px` }
       left="0"
-      zIndex="docked"
+      zIndex="sticky1"
       bgColor={ bgColor }
-      transform={ isVisible ? 'translateY(0)' : 'translateY(-108px)' }
-      transitionProperty="transform"
+      transform={ isVisible ? 'translateY(0)' : 'translateY(-112px)' }
+      transitionProperty="transform,box-shadow"
       transitionDuration="slow"
       display={{ base: 'block', lg: 'none' }}
       w="100%"
+      boxShadow={ isVisible && isSticky ? 'md' : 'none' }
     >
       <InputGroup size="sm">
         <InputLeftElement >
