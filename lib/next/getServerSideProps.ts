42ea53d1f6a891d38db8ecc2f46e746767789790
isstuev <isstuev@gmail.com>
2022-11-03 18:20:03 +0300

    ssr referrer

--- lib/next/getServerSideProps.ts
+++ lib/next/getServerSideProps.ts
@@ -2,12 +2,14 @@ import type { GetServerSideProps, GetServerSidePropsResult } from 'next';
 
 export type Props = {
   cookies: string;
+  referrer: string;
 }
 
 export const getServerSideProps: GetServerSideProps = async({ req }): Promise<GetServerSidePropsResult<Props>> => {
   return {
     props: {
       cookies: req.headers.cookie || '',
+      referrer: req.headers.referer || '',
     },
   };
 };
