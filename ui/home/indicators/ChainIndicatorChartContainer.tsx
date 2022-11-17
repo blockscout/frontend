cbb1e6932f515ffea9dffe3af33b846ad3dd01dd
Yuri Mikhin <urasergeevich@mail.ru>
2022-11-17 16:57:11 +0300

    Replace ChainIndicatorChartData type for TimeChartData type.

--- ui/home/indicators/ChainIndicatorChartContainer.tsx
+++ ui/home/indicators/ChainIndicatorChartContainer.tsx
@@ -2,14 +2,14 @@ import { Flex } from '@chakra-ui/react';
 import type { UseQueryResult } from '@tanstack/react-query';
 import React from 'react';
 
-import type { ChainIndicatorChartData } from './types';
+import type { TimeChartData } from 'ui/shared/chart/types';
 
 import ContentLoader from 'ui/shared/ContentLoader';
 import DataFetchAlert from 'ui/shared/DataFetchAlert';
 
 import ChainIndicatorChart from './ChainIndicatorChart';
 
-type Props = UseQueryResult<ChainIndicatorChartData>;
+type Props = UseQueryResult<TimeChartData>;
 
 const ChainIndicatorChartContainer = ({ data, isError, isLoading }: Props) => {
 
--- ui/home/indicators/useFetchChartData.tsx
+++ ui/home/indicators/useFetchChartData.tsx
@@ -2,13 +2,14 @@ import type { UseQueryResult } from '@tanstack/react-query';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
-import type { TChainIndicator, ChartsResponse, ChartsQueryKeys, ChainIndicatorChartData } from './types';
+import type { TChainIndicator, ChartsResponse, ChartsQueryKeys } from './types';
+import type { TimeChartData } from 'ui/shared/chart/types';
 
 import useFetch from 'lib/hooks/useFetch';
 
 type NotUndefined<T> = T extends undefined ? never : T;
 
-export default function useFetchCharData<Q extends ChartsQueryKeys>(indicator: TChainIndicator<Q> | undefined): UseQueryResult<ChainIndicatorChartData> {
+export default function useFetchCharData<Q extends ChartsQueryKeys>(indicator: TChainIndicator<Q> | undefined): UseQueryResult<TimeChartData> {
   const fetch = useFetch();
 
   type ResponseType = ChartsResponse<NotUndefined<typeof indicator>['api']['queryName']>;
@@ -23,6 +24,6 @@ export default function useFetchCharData<Q extends ChartsQueryKeys>(indicator: T
     return {
       ...queryResult,
       data: queryResult.data && indicator ? indicator.api.dataFn(queryResult.data) : queryResult.data,
-    } as UseQueryResult<ChainIndicatorChartData>;
+    } as UseQueryResult<TimeChartData>;
   }, [ indicator, queryResult ]);
 }
