f33759c90e1774876a959c495c0d3bc0dff9dce5
isstuev <isstuev@gmail.com>
2022-11-23 20:07:31 +0300

    target blanc for docs

--- ui/pages/ApiKeys.tsx
+++ ui/pages/ApiKeys.tsx
@@ -57,7 +57,7 @@ const ApiKeysPage: React.FC = () => {
   const description = (
     <AccountPageDescription>
       Create API keys to use for your RPC and EthRPC API requests. For more information, see { space }
-      <Link href="https://docs.blockscout.com/for-users/api#api-keys">“How to use a Blockscout API key”</Link>.
+      <Link href="https://docs.blockscout.com/for-users/api#api-keys" target="_blank">“How to use a Blockscout API key”</Link>.
     </AccountPageDescription>
   );
 
