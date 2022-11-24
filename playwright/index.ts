66320d007f743cb18e05f54675d8bf9a110816d1
tom <tom@ohhhh.me>
2022-11-24 09:35:16 +0400

    inline fonts and mock date globally

--- playwright/index.ts
+++ playwright/index.ts
@@ -1,4 +1,16 @@
-// Import styles, initialize component theme here.
-// import '../src/common.css';
+import './fonts.css';
+import { beforeMount } from '@playwright/experimental-ct-react/hooks';
+import MockDate from 'mockdate';
+import * as router from 'next/router';
+
+beforeMount(async({ hooksConfig }) => {
+  // Before mount, redefine useRouter to return mock value from test.
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore: I really want to redefine this property :)
+  router.useRouter = () => hooksConfig.router;
+
+  // set current date
+  MockDate.set('2022-11-11');
+});
 
 export {};
