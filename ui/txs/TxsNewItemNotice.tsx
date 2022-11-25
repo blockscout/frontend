d178bbb0a4d741e6dab1495e0c78af8e2f7c8d80
tom <tom@ohhhh.me>
2022-11-25 18:12:58 +0400

    update texts

--- ui/txs/TxsNewItemNotice.tsx
+++ ui/txs/TxsNewItemNotice.tsx
@@ -1,4 +1,4 @@
-import { Alert, Spinner, Text, Link, chakra } from '@chakra-ui/react';
+import { Alert, Link, chakra } from '@chakra-ui/react';
 import React from 'react';
 
 import useNewTxsSocket from 'lib/hooks/useNewTxsSocket';
@@ -36,14 +36,16 @@ const TxsNewItemNotice = ({ children, className }: Props) => {
     }
 
     if (!num) {
-      return null;
+      return (
+        <Alert className={ className } status="warning" p={ 4 } fontWeight={ 400 }>
+          scanning new transactions...
+        </Alert>
+      );
     }
 
     return (
       <Alert className={ className } status="warning" p={ 4 } fontWeight={ 400 }>
-        <Spinner size="sm" mr={ 3 }/>
-        <Text as="span" whiteSpace="pre">+ { num } new transaction{ num > 1 ? 's' : '' }. </Text>
-        <Link onClick={ handleClick }>View in list</Link>
+        <Link onClick={ handleClick }>{ num } more transaction have come in</Link>
       </Alert>
     );
   })();
