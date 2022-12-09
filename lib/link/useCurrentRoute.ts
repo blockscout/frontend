e8059908424ff0a45d07aaa29b8a26b1d230db87
tom <tom@ohhhh.me>
2022-12-09 17:27:47 +0300

    fix current route

--- lib/link/useCurrentRoute.ts
+++ lib/link/useCurrentRoute.ts
@@ -4,13 +4,18 @@ import React from 'react';
 import type { RouteName } from 'lib/link/routes';
 import { ROUTES } from 'lib/link/routes';
 
+const PATH_PARAM_REGEXP = /\/:(\w+)/g;
+
 export default function useCurrentRoute() {
   const { route: nextRoute } = useRouter();
 
   return React.useCallback((): RouteName => {
     for (const routeName in ROUTES) {
       const route = ROUTES[routeName as RouteName];
-      if (route.pattern === nextRoute) {
+      const formattedRoute = route.pattern.replace(PATH_PARAM_REGEXP, (_, paramName: string) => {
+        return `/[${ paramName }]`;
+      });
+      if (formattedRoute === nextRoute) {
         return routeName as RouteName;
       }
     }
