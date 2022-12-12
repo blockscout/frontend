bd1e76f2694f45d60ccbaff2b320f79263c9368c
tom <tom@ohhhh.me>
2022-12-12 12:07:28 +0300

    mobile view

--- ui/address/coinBalance/AddressCoinBalanceHistory.tsx
+++ ui/address/coinBalance/AddressCoinBalanceHistory.tsx
@@ -1,4 +1,4 @@
-import { Box, Table, Tbody, Th, Tr } from '@chakra-ui/react';
+import { Box, Hide, Show, Table, Tbody, Th, Tr } from '@chakra-ui/react';
 import type { UseQueryResult } from '@tanstack/react-query';
 import React from 'react';
 
@@ -8,9 +8,11 @@ import { QueryKeys } from 'types/client/queries';
 import appConfig from 'configs/app/config';
 import useQueryWithPages from 'lib/hooks/useQueryWithPages';
 import ActionBar from 'ui/shared/ActionBar';
+import DataFetchAlert from 'ui/shared/DataFetchAlert';
 import Pagination from 'ui/shared/Pagination';
 import { default as Thead } from 'ui/shared/TheadSticky';
 
+import AddressCoinBalanceListItem from './AddressCoinBalanceListItem';
 import AddressCoinBalanceTableItem from './AddressCoinBalanceTableItem';
 
 interface Props {
@@ -26,11 +28,45 @@ const AddressCoinBalanceHistory = ({ addressQuery }: Props) => {
     },
   });
 
-  if (query.isError || query.isLoading) {
-    return <Box>loading</Box>;
-  }
-
   const isPaginatorHidden = !query.isLoading && !query.isError && query.pagination.page === 1 && !query.pagination.hasNextPage;
+
+  const content = (() => {
+    if (query.isLoading) {
+      return <Box>loading</Box>;
+    }
+
+    if (query.isError) {
+      return <DataFetchAlert/>;
+    }
+
+    return (
+      <>
+        <Hide below="lg">
+          <Table variant="simple" size="sm">
+            <Thead top={ 80 }>
+              <Tr>
+                <Th width="25%">Block</Th>
+                <Th width="25%">Txn</Th>
+                <Th width="25%">Age</Th>
+                <Th width="25%" isNumeric pr={ 1 }/>
+                <Th width="120px" isNumeric>Balance { appConfig.network.currency.symbol }</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              { query.data.items.map((item) => (
+                <AddressCoinBalanceTableItem key={ item.block_number } { ...item }/>
+              )) }
+            </Tbody>
+          </Table>
+        </Hide>
+        <Show below="lg">
+          { query.data.items.map((item) => (
+            <AddressCoinBalanceListItem key={ item.block_number } { ...item }/>
+          )) }
+        </Show>
+      </>
+    );
+  })();
 
   return (
     <Box mt={ 8 }>
@@ -39,22 +75,7 @@ const AddressCoinBalanceHistory = ({ addressQuery }: Props) => {
           <Pagination ml="auto" { ...query.pagination }/>
         </ActionBar>
       ) }
-      <Table variant="simple" size="sm">
-        <Thead top={ 80 }>
-          <Tr>
-            <Th width="25%">Block</Th>
-            <Th width="25%">Txn</Th>
-            <Th width="25%">Age</Th>
-            <Th width="25%" isNumeric pr={ 1 }/>
-            <Th width="120px" isNumeric>Balance { appConfig.network.currency.symbol }</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          { query.data.items.map((item) => (
-            <AddressCoinBalanceTableItem key={ item.block_number } { ...item }/>
-          )) }
-        </Tbody>
-      </Table>
+      { content }
     </Box>
   );
 };
