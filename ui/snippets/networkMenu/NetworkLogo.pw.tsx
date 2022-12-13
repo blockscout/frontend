4cd8bfec2aa22fb890d061f15cd6f11549b67221
tom <tom@ohhhh.me>
2022-12-13 17:57:49 +0300

    network logo tests

---
+++ ui/snippets/networkMenu/NetworkLogo.pw.tsx
@@ -0,0 +1,71 @@
+import { test, expect } from '@playwright/experimental-ct-react';
+import React from 'react';
+
+import contextWithEnvs from 'playwright/fixtures/contextWithEnvs';
+import TestApp from 'playwright/TestApp';
+
+import NetworkLogo from './NetworkLogo';
+
+test('fallback logo +@desktop-xl +@dark-mode +@dark-mode-xl', async({ mount }) => {
+  const component = await mount(
+    <TestApp>
+      <NetworkLogo/>
+    </TestApp>,
+  );
+
+  await expect(component.locator('a')).toHaveScreenshot();
+});
+
+test.describe('placeholder logo', () => {
+  const extendedTest = test.extend({
+    context: contextWithEnvs([
+      { name: 'NEXT_PUBLIC_NETWORK_TYPE', value: 'unknown' },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ]) as any,
+  });
+
+  extendedTest('+@desktop-xl +@dark-mode +@dark-mode-xl', async({ mount }) => {
+    const component = await mount(
+      <TestApp>
+        <NetworkLogo/>
+      </TestApp>,
+    );
+
+    await expect(component.locator('a')).toHaveScreenshot();
+  });
+});
+
+test.describe('custom logo', () => {
+  const LOGO_URL = 'https://example.com/my-logo.png';
+  const SMALL_LOGO_URL = 'https://example.com/my-logo-short.png';
+  const extendedTest = test.extend({
+    context: contextWithEnvs([
+      { name: 'NEXT_PUBLIC_NETWORK_LOGO', value: LOGO_URL },
+      { name: 'NEXT_PUBLIC_NETWORK_SMALL_LOGO', value: SMALL_LOGO_URL },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ]) as any,
+  });
+
+  extendedTest.only('+@desktop-xl +@dark-mode +@dark-mode-xl', async({ mount, page }) => {
+    await page.route(LOGO_URL, (route) => {
+      return route.fulfill({
+        status: 200,
+        path: './playwright/giant_duck_long.jpg',
+      });
+    });
+    await page.route(SMALL_LOGO_URL, (route) => {
+      return route.fulfill({
+        status: 200,
+        path: './playwright/image_s.jpg',
+      });
+    });
+
+    const component = await mount(
+      <TestApp>
+        <NetworkLogo/>
+      </TestApp>,
+    );
+
+    await expect(component.locator('a')).toHaveScreenshot();
+  });
+});
