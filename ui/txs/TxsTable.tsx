729dacb3be0684d7523f98ac0285c22e5000ced7
tom <tom@ohhhh.me>
2022-12-21 12:10:18 +0300

    tx fixes

--- ui/txs/TxsTable.tsx
+++ ui/txs/TxsTable.tsx
@@ -27,21 +27,21 @@ const TxsTable = ({ txs, sort, sorting, top, showBlockInfo, showSocketInfo, curr
       <TheadSticky top={ top }>
         <Tr>
           <Th width="54px"></Th>
-          <Th width="18%">Txn hash</Th>
-          <Th width="20%">Type</Th>
-          <Th width="15%">Method</Th>
-          { showBlockInfo && <Th width="11%">Block</Th> }
+          <Th width="22%">Txn hash</Th>
+          <Th width="160px">Type</Th>
+          <Th width="20%">Method</Th>
+          { showBlockInfo && <Th width="18%">Block</Th> }
           <Th width={{ xl: '128px', base: '66px' }}>From</Th>
           <Th width={{ xl: currentAddress ? '48px' : '36px', base: '0' }}></Th>
           <Th width={{ xl: '128px', base: '66px' }}>To</Th>
-          <Th width="18%" isNumeric>
+          <Th width="20%" isNumeric>
             <Link onClick={ sort('val') } display="flex" justifyContent="end">
               { sorting === 'val-asc' && <Icon boxSize={ 5 } as={ rightArrowIcon } transform="rotate(-90deg)"/> }
               { sorting === 'val-desc' && <Icon boxSize={ 5 } as={ rightArrowIcon } transform="rotate(90deg)"/> }
               { `Value ${ appConfig.network.currency.symbol }` }
             </Link>
           </Th>
-          <Th width="18%" isNumeric pr={ 5 }>
+          <Th width="20%" isNumeric pr={ 5 }>
             <Link onClick={ sort('fee') } display="flex" justifyContent="end">
               { sorting === 'fee-asc' && <Icon boxSize={ 5 } as={ rightArrowIcon } transform="rotate(-90deg)"/> }
               { sorting === 'fee-desc' && <Icon boxSize={ 5 } as={ rightArrowIcon } transform="rotate(90deg)"/> }
