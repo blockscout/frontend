cb603dd3e6276ba428ae79826e6688fdada391f8
tom <tom@ohhhh.me>
2022-12-26 16:36:28 +0300

    add prefix to proxy path

--- playwright/utils/buildApiUrl.ts
+++ playwright/utils/buildApiUrl.ts
@@ -5,5 +5,5 @@ import { RESOURCES } from 'lib/api/resources';
 
 export default function buildApiUrl(resourceName: ResourceName, pathParams?: Record<string, string>) {
   const resource = RESOURCES[resourceName];
-  return compile('/proxy/poa/core' + resource.path)(pathParams);
+  return compile('/node-api/proxy/poa/core' + resource.path)(pathParams);
 }
