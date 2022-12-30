1b525077112c9c5fcecd3a58b6403daf3bd7b20a
Yuri Mikhin <urasergeevich@mail.ru>
2022-12-30 22:19:47 +0300

    Add metric prefixes for stats and charts.

--- ui/stats/NumberWidgetsList.tsx
+++ ui/stats/NumberWidgetsList.tsx
@@ -2,6 +2,7 @@ import { Grid } from '@chakra-ui/react';
 import React from 'react';
 
 import useApiQuery from 'lib/api/useApiQuery';
+import formatNumberToMetricPrefix from 'lib/formatNumberToMetricPrefix';
 
 import { numberWidgetsScheme } from './constants/number-widgets-scheme';
 import NumberWidget from './NumberWidget';
@@ -12,15 +13,23 @@ const skeletonsCount = 8;
 const NumberWidgetsList = () => {
   const { data, isLoading } = useApiQuery('stats_counters');
 
+  const skeletonElement = [ ...Array(skeletonsCount) ]
+    .map((e, i) => <NumberWidgetSkeleton key={ i }/>);
+
   return (
     <Grid
       gridTemplateColumns={{ base: 'repeat(2, 1fr)', lg: 'repeat(4, 1fr)' }}
       gridGap={ 4 }
     >
-      { isLoading ? [ ...Array(skeletonsCount) ]
-        .map((e, i) => <NumberWidgetSkeleton key={ i }/>) :
+      { isLoading ? skeletonElement :
         numberWidgetsScheme.map(({ id, title }) =>
-          data?.counters[id] ? <NumberWidget key={ id } label={ title } value={ Number(data.counters[id]).toLocaleString() }/> : null) }
+          data?.counters[id] ? (
+            <NumberWidget
+              key={ id }
+              label={ title }
+              value={ formatNumberToMetricPrefix(Number(data.counters[id])) }
+            />
+          ) : null) }
     </Grid>
   );
 };
