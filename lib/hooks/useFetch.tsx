37e202c47f9f64ec9e1898999d81170659f30764
tom <tom@ohhhh.me>
2023-01-12 09:51:02 -0600

    call api from client on preview stands

--- lib/hooks/useFetch.tsx
+++ lib/hooks/useFetch.tsx
@@ -27,7 +27,6 @@ export default function useFetch() {
       headers: {
         ...(hasBody ? { 'Content-type': 'application/json' } : undefined),
         ...params?.headers,
-        // ...(token ? { 'x-csrf-token': token } : {}),
       },
     };
 
