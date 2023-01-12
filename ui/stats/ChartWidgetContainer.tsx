c235e99a11f0de53fbf6f1a3ceebb5545d304f85
Yuri Mikhin <urasergeevich@mail.ru>
2023-01-12 13:15:29 +0300

    Add network error state for charts.

--- ui/stats/ChartWidgetContainer.tsx
+++ ui/stats/ChartWidgetContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import type { StatsIntervalIds } from 'types/client/stats';
 
@@ -12,19 +12,20 @@ type Props = {
   title: string;
   description: string;
   interval: StatsIntervalIds;
+  onLoadingError: () => void;
 }
 
 function formatDate(date: Date) {
   return date.toISOString().substring(0, 10);
 }
 
-const ChartWidgetContainer = ({ id, title, description, interval }: Props) => {
+const ChartWidgetContainer = ({ id, title, description, interval, onLoadingError }: Props) => {
   const selectedInterval = STATS_INTERVALS[interval];
 
   const endDate = selectedInterval.start ? formatDate(new Date()) : undefined;
   const startDate = selectedInterval.start ? formatDate(selectedInterval.start) : undefined;
 
-  const { data, isLoading } = useApiQuery('stats_charts', {
+  const { data, isLoading, isError } = useApiQuery('stats_charts', {
     queryParams: {
       name: id,
       from: startDate,
@@ -37,8 +38,16 @@ const ChartWidgetContainer = ({ id, title, description, interval }: Props) => {
       return { date: new Date(item.date), value: Number(item.value) };
     });
 
+  useEffect(() => {
+    if (isError) {
+      onLoadingError();
+    }
+  }, [ isError, onLoadingError ]);
+
   return (
     <ChartWidget
+      chartHeight="100%"
+      isError={ isError }
       items={ items }
       title={ title }
       description={ description }
