5d45007f46df2328f0b94392a323b6d6025eac2b
isstuev <isstuev@gmail.com>
2023-01-26 19:09:09 -0600

    test

---
+++ pages/test2.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import React from 'react';
+
+const Page: NextPage = () => {
+  return (
+    <div>
+        Привет!
+    </div>
+  );
+};
+
+export default Page;
+
+export type Props = Record<string, unknown>;
