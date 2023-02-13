b6626be825bbc9211a4568ff9b48565775700b0f
tom <tom@ohhhh.me>
2023-02-13 13:04:16 -0500

    default router config for pw

--- playwright/index.ts
+++ playwright/index.ts
@@ -1,5 +1,6 @@
 import './fonts.css';
 import { beforeMount } from '@playwright/experimental-ct-react/hooks';
+import _defaultsDeep from 'lodash/defaultsDeep';
 import MockDate from 'mockdate';
 import * as router from 'next/router';
 
@@ -12,7 +13,7 @@ beforeMount(async({ hooksConfig }) => {
   // Before mount, redefine useRouter to return mock value from test.
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore: I really want to redefine this property :)
-  router.useRouter = () => hooksConfig?.router || NEXT_ROUTER_MOCK;
+  router.useRouter = () => _defaultsDeep(hooksConfig?.router, NEXT_ROUTER_MOCK);
 
   // set current date
   MockDate.set('2022-11-11T12:00:00Z');
