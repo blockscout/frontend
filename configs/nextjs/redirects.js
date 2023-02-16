3cc098e90f55eb2e20cea6422fdd1c382f18a4a7
tom <tom@ohhhh.me>
2023-02-16 13:09:54 -0500

    remove link

--- configs/nextjs/redirects.js
+++ configs/nextjs/redirects.js
@@ -1,94 +1,92 @@
-const PATHS = require('../../lib/link/paths.json');
-
 const oldUrls = [
   {
     oldPath: '/account/tag_transaction',
-    newPath: `${ PATHS.private_tags }?tab=tx`,
+    newPath: '/account/tag_address?tab=tx',
   },
   {
     oldPath: '/pending-transactions',
-    newPath: `${ PATHS.txs }?tab=pending`,
+    newPath: '/txs?tab=pending',
   },
   {
     oldPath: '/tx/:hash/internal-transactions',
-    newPath: `${ PATHS.tx }?tab=internal`,
+    newPath: '/tx/:hash?tab=internal',
   },
   {
     oldPath: '/tx/:hash/logs',
-    newPath: `${ PATHS.tx }?tab=logs`,
+    newPath: '/tx/:hash?tab=logs',
   },
   {
     oldPath: '/tx/:hash/raw-trace',
-    newPath: `${ PATHS.tx }?tab=raw_trace`,
+    newPath: '/tx/:hash?tab=raw_trace',
   },
   {
     oldPath: '/tx/:hash/state',
-    newPath: `${ PATHS.tx }?tab=state`,
+    newPath: '/tx/:hash?tab=state',
   },
   {
     oldPath: '/uncles',
-    newPath: `${ PATHS.blocks }?tab=uncles`,
+    newPath: '/blocks?tab=uncles',
   },
   {
     oldPath: '/reorgs',
-    newPath: `${ PATHS.blocks }?tab=reorgs`,
+    newPath: '/blocks?tab=reorgs',
   },
   {
     oldPath: '/block/:height/transactions',
-    newPath: `${ PATHS.block }?tab=txs`,
+    newPath: '/block/:height?tab=txs',
   },
   {
     oldPath: '/address/:hash/transactions',
-    newPath: `${ PATHS.address_index }`,
+    newPath: '/address/:hash',
   },
   {
     oldPath: '/address/:hash/token-transfers',
-    newPath: `${ PATHS.address_index }?tab=token_transfers`,
+    newPath: '/address/:hash?tab=token_transfers',
   },
   {
     oldPath: '/address/:hash/tokens',
-    newPath: `${ PATHS.address_index }?tab=tokens`,
+    newPath: '/address/:hash?tab=tokens',
   },
   {
     oldPath: '/address/:hash/internal-transactions',
-    newPath: `${ PATHS.address_index }?tab=internal_txns`,
+    newPath: '/address/:hash?tab=internal_txns',
   },
   {
     oldPath: '/address/:hash/coin-balances',
-    newPath: `${ PATHS.address_index }?tab=coin_balance_history`,
+    newPath: '/address/:hash?tab=coin_balance_history',
   },
   {
     oldPath: '/address/:hash/validations',
-    newPath: `${ PATHS.address_index }?tab=blocks_validated`,
+    newPath: '/address/:hash?tab=blocks_validated',
   },
   {
     oldPath: '/address/:hash/tokens/:token_hash/token-transfers',
-    newPath: `${ PATHS.address_index }?tab=token_transfers&token=:token_hash`,
+    newPath: '/address/:hash?tab=token_transfers&token=:token_hash',
   },
   // contract verification
   {
     oldPath: '/address/:hash/contract_verifications/new',
-    newPath: `${ PATHS.address_contract_verification }`,
+    newPath: '/address/:hash/contract_verification',
   },
   {
     oldPath: '/address/:hash/verify-via-flattened-code/new',
-    newPath: `${ PATHS.address_contract_verification }?method=flatten_source_code`,
+    newPath: '/address/:hash/contract_verification?method=flatten_source_code',
   },
   {
     oldPath: '/address/:hash/verify-via-standard-json-input/new',
-    newPath: `${ PATHS.address_contract_verification }?method=standard_input`,
+    newPath: '/address/:hash/contract_verification?method=standard_input',
   },
   {
     oldPath: '/address/:hash/verify-via-metadata-json/new',
-    newPath: `${ PATHS.address_contract_verification }?method=sourcify`,
+    newPath: '/address/:hash/contract_verification?method=sourcify',
   },
   {
     oldPath: '/address/:hash/verify-via-multi-part-files/new',
-    newPath: `${ PATHS.address_contract_verification }?method=multi_part_file`,
+    newPath: '/address/:hash/contract_verification?method=multi_part_file',
   },
   {
     oldPath: '/address/:hash/verify-vyper-contract/new',
-    newPath: `${ PATHS.address_contract_verification }?method=vyper_contract`,
+    newPath: '/address/:hash/contract_verification?method=vyper_contract',
   },
 ];
 
