7220f92698ca224045d4b0bbb576e2cd662ae383
tom <tom@ohhhh.me>
2023-02-24 11:51:03 -0500

    remove number formatters

--- lib/formatNumberToMetricPrefix.ts
+++
@@ -1,6 +0,0 @@
-export default function formatNumberToMetricPrefix(number: number) {
-  return Intl.NumberFormat('en-US', {
-    notation: 'compact',
-    maximumFractionDigits: 3,
-  }).format(number);
-}
