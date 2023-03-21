0b885d8a456b6f38f44630682303562f7e38eb9b
tom <tom@ohhhh.me>
2023-03-21 18:41:21 -0300

    temporary pin api host to staging

--- lib/api/isNeedProxy.ts
+++ lib/api/isNeedProxy.ts
@@ -1,9 +1,11 @@
-import appConfig from 'configs/app/config';
+// import appConfig from 'configs/app/config';
 
 // FIXME
 // I was not able to figure out how to send CORS with credentials from localhost
 // unsuccessfully tried different ways, even custom local dev domain
 // so for local development we have to use next.js api as proxy server
 export default function isNeedProxy() {
-  return appConfig.host === 'localhost' && appConfig.host !== appConfig.api.host;
+// todo_tom rollback this
+  return true;
+  // return appConfig.host === 'localhost' && appConfig.host !== appConfig.api.host;
 }
