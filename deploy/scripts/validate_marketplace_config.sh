3af5a9024f44d41391dc4d4010cd39aad76c62a5
tom <tom@ohhhh.me>
2023-03-31 17:15:12 -0300

    rollback changes related to storing config in the repo

--- deploy/scripts/validate_marketplace_config.sh
+++
@@ -1,3 +0,0 @@
-yarn ts-to-zod ./types/client/apps.ts ./tools/marketplace-config-validator/schema.ts
-yarn tsc -p ./tools/marketplace-config-validator/tsconfig.json
-node ./tools/marketplace-config-validator/index.js
