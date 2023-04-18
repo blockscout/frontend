8dc94e6bdaf9e63157c4449809d4842d8ca37e3c
tom <tom@ohhhh.me>
2023-04-18 11:55:27 -0300

    disable csv export feature if recaptcha key is not provided

--- pages/csv-export.tsx
+++ pages/csv-export.tsx
@@ -1,8 +1,11 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
 
+import appConfig from 'configs/app/config';
 import getNetworkTitle from 'lib/networks/getNetworkTitle';
+import type { Props } from 'lib/next/getServerSideProps';
+import { getServerSideProps as getServerSidePropsBase } from 'lib/next/getServerSideProps';
 import CsvExport from 'ui/pages/CsvExport';
 
 const CsvExportPage: NextPage = () => {
@@ -19,4 +22,12 @@ const CsvExportPage: NextPage = () => {
 
 export default CsvExportPage;
 
-export { getServerSideProps } from 'lib/next/getServerSideProps';
+export const getServerSideProps: GetServerSideProps<Props> = async(args) => {
+  if (!appConfig.reCaptcha.siteKey) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return getServerSidePropsBase(args);
+};
