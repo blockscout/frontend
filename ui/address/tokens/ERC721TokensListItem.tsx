de3956e2103212af4904d1b07007e1d132d01da9
isstuev <isstuev@gmail.com>
2023-05-17 10:49:32 -0300

    change link to address-token

--- ui/address/tokens/ERC721TokensListItem.tsx
+++ ui/address/tokens/ERC721TokensListItem.tsx
@@ -1,4 +1,5 @@
 import { Flex, HStack, Text } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import type { AddressTokenBalance } from 'types/api/address';
@@ -12,6 +13,9 @@ import TokenLogo from 'ui/shared/TokenLogo';
 type Props = AddressTokenBalance;
 
 const ERC721TokensListItem = ({ token, value }: Props) => {
+  const router = useRouter();
+
+  const hash = router.query.hash?.toString() || '';
 
   const tokenString = [ token.name, token.symbol && `(${ token.symbol })` ].filter(Boolean).join(' ');
 
@@ -19,7 +23,7 @@ const ERC721TokensListItem = ({ token, value }: Props) => {
     <ListItemMobile rowGap={ 2 }>
       <Flex alignItems="center" width="100%">
         <TokenLogo hash={ token.address } name={ token.name } boxSize={ 6 } mr={ 2 }/>
-        <AddressLink fontWeight="700" hash={ token.address } type="token" alias={ tokenString }/>
+        <AddressLink fontWeight="700" hash={ hash } tokenHash={ token.address } type="address_token" alias={ tokenString }/>
       </Flex>
       <Flex alignItems="center" pl={ 8 }>
         <AddressLink hash={ token.address } type="address" truncation="constant"/>
