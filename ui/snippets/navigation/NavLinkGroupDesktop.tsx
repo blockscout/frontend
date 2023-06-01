25f1becadc2a27a60afd6f8edb0ad712148b5894
isstuev <isstuev@gmail.com>
2023-06-01 14:10:20 -0300

    fix keys

--- ui/snippets/navigation/NavLinkGroupDesktop.tsx
+++ ui/snippets/navigation/NavLinkGroupDesktop.tsx
@@ -88,7 +88,7 @@ const NavLinkGroupDesktop = ({ item, isCollapsed }: Props) => {
                   { subItem.map(subSubItem => <NavLink key={ subSubItem.text } item={ subSubItem } isCollapsed={ false }/>) }
                 </Box>
               ) :
-                <NavLink key={ item.text } item={ subItem } isCollapsed={ false }/>,
+                <NavLink key={ subItem.text } item={ subItem } isCollapsed={ false }/>,
               ) }
             </VStack>
           </PopoverBody>
