46b0eaad006132c187cdb628c6b59e1b1f28295d
isstuev <isstuev@gmail.com>
2023-06-06 10:26:25 -0300

    fix home skeleton

--- ui/home/LatestTxsItem.tsx
+++ ui/home/LatestTxsItem.tsx
@@ -14,7 +14,6 @@ import appConfig from 'configs/app/config';
 import rightArrowIcon from 'icons/arrows/east.svg';
 import transactionIcon from 'icons/transactions.svg';
 import getValueWithUnit from 'lib/getValueWithUnit';
-import useIsMobile from 'lib/hooks/useIsMobile';
 import useTimeAgoIncrement from 'lib/hooks/useTimeAgoIncrement';
 import Address from 'ui/shared/address/Address';
 import AddressIcon from 'ui/shared/address/AddressIcon';
@@ -33,16 +32,15 @@ const LatestTxsItem = ({ tx, isLoading }: Props) => {
   const dataTo = tx.to ? tx.to : tx.created_contract;
   const timeAgo = useTimeAgoIncrement(tx.timestamp || '0', true);
 
-  const isMobile = useIsMobile();
-
-  if (isMobile) {
-    return (
+  return (
+    <>
       <Box
         width="100%"
         borderTop="1px solid"
         borderColor="divider"
         py={ 4 }
         _last={{ borderBottom: '1px solid', borderColor: 'divider' }}
+        display={{ base: 'block', lg: 'none' }}
       >
         <Flex justifyContent="space-between">
           <HStack>
@@ -127,103 +125,101 @@ const LatestTxsItem = ({ tx, isLoading }: Props) => {
           <Text as="span" variant="secondary">{ getValueWithUnit(tx.fee.value).dp(5).toFormat() }</Text>
         </Skeleton>
       </Box>
-    );
-  }
-
-  return (
-    <Box
-      width="100%"
-      minW="700px"
-      borderTop="1px solid"
-      borderColor="divider"
-      p={ 4 }
-      _last={{ borderBottom: '1px solid', borderColor: 'divider' }}
-    >
-      <Grid width="100%" gridTemplateColumns="3fr 2fr 150px" gridGap={ 8 }>
-        <Flex overflow="hidden" w="100%">
-          <TxAdditionalInfo tx={ tx } isLoading={ isLoading }/>
-          <Box ml={ 3 } w="calc(100% - 40px)">
-            <HStack>
-              <TxType types={ tx.tx_types } isLoading={ isLoading }/>
-              <TxStatus status={ tx.status } errorText={ tx.status === 'error' ? tx.result : undefined } isLoading={ isLoading }/>
-            </HStack>
-            <Flex
-              mt={ 2 }
-              alignItems="center"
-            >
-              <Icon
-                as={ transactionIcon }
-                boxSize="30px"
-                color="link"
-                display="inline"
-                isLoading={ isLoading }
-                borderRadius="base"
-              />
-              <Address overflow="hidden" w="calc(100% - 130px)" maxW="calc(100% - 130px)" ml={ 2 } mr={ 2 }>
-                <AddressLink
-                  hash={ tx.hash }
-                  type="transaction"
-                  fontWeight="700"
+      <Box
+        width="100%"
+        minW="700px"
+        borderTop="1px solid"
+        borderColor="divider"
+        p={ 4 }
+        _last={{ borderBottom: '1px solid', borderColor: 'divider' }}
+        display={{ base: 'none', lg: 'block' }}
+      >
+        <Grid width="100%" gridTemplateColumns="3fr 2fr 150px" gridGap={ 8 }>
+          <Flex overflow="hidden" w="100%">
+            <TxAdditionalInfo tx={ tx } isLoading={ isLoading }/>
+            <Box ml={ 3 } w="calc(100% - 40px)">
+              <HStack>
+                <TxType types={ tx.tx_types } isLoading={ isLoading }/>
+                <TxStatus status={ tx.status } errorText={ tx.status === 'error' ? tx.result : undefined } isLoading={ isLoading }/>
+              </HStack>
+              <Flex
+                mt={ 2 }
+                alignItems="center"
+              >
+                <Icon
+                  as={ transactionIcon }
+                  boxSize="30px"
+                  color="link"
+                  display="inline"
                   isLoading={ isLoading }
+                  borderRadius="base"
                 />
-              </Address>
-              { tx.timestamp && (
-                <Skeleton isLoaded={ !isLoading } color="text_secondary" fontWeight="400" fontSize="sm">
-                  <span>{ timeAgo }</span>
-                </Skeleton>
-              ) }
-            </Flex>
-          </Box>
-        </Flex>
-        <Grid alignItems="center" templateColumns="24px auto">
-          <Icon
-            as={ rightArrowIcon }
-            boxSize={ 6 }
-            color="gray.500"
-            transform="rotate(90deg)"
-            isLoading={ isLoading }
-          />
-          <Box overflow="hidden" ml={ 1 }>
-            <Address mb={ 2 }>
-              <AddressIcon address={ tx.from } isLoading={ isLoading }/>
-              <AddressLink
-                type="address"
-                hash={ tx.from.hash }
-                alias={ tx.from.name }
-                fontWeight="500"
-                ml={ 2 }
-                fontSize="sm"
-                isLoading={ isLoading }
-              />
-            </Address>
-            { dataTo && (
-              <Address>
-                <AddressIcon address={ dataTo } isLoading={ isLoading }/>
+                <Address overflow="hidden" w="calc(100% - 130px)" maxW="calc(100% - 130px)" ml={ 2 } mr={ 2 }>
+                  <AddressLink
+                    hash={ tx.hash }
+                    type="transaction"
+                    fontWeight="700"
+                    isLoading={ isLoading }
+                  />
+                </Address>
+                { tx.timestamp && (
+                  <Skeleton isLoaded={ !isLoading } color="text_secondary" fontWeight="400" fontSize="sm">
+                    <span>{ timeAgo }</span>
+                  </Skeleton>
+                ) }
+              </Flex>
+            </Box>
+          </Flex>
+          <Grid alignItems="center" templateColumns="24px auto">
+            <Icon
+              as={ rightArrowIcon }
+              boxSize={ 6 }
+              color="gray.500"
+              transform="rotate(90deg)"
+              isLoading={ isLoading }
+            />
+            <Box overflow="hidden" ml={ 1 }>
+              <Address mb={ 2 }>
+                <AddressIcon address={ tx.from } isLoading={ isLoading }/>
                 <AddressLink
                   type="address"
-                  hash={ dataTo.hash }
-                  alias={ dataTo.name }
+                  hash={ tx.from.hash }
+                  alias={ tx.from.name }
                   fontWeight="500"
                   ml={ 2 }
                   fontSize="sm"
                   isLoading={ isLoading }
                 />
               </Address>
-            ) }
+              { dataTo && (
+                <Address>
+                  <AddressIcon address={ dataTo } isLoading={ isLoading }/>
+                  <AddressLink
+                    type="address"
+                    hash={ dataTo.hash }
+                    alias={ dataTo.name }
+                    fontWeight="500"
+                    ml={ 2 }
+                    fontSize="sm"
+                    isLoading={ isLoading }
+                  />
+                </Address>
+              ) }
+            </Box>
+          </Grid>
+          <Box>
+            <Skeleton isLoaded={ !isLoading } mb={ 2 }>
+              <Text as="span" whiteSpace="pre">{ appConfig.network.currency.symbol } </Text>
+              <Text as="span" variant="secondary">{ getValueWithUnit(tx.value).dp(5).toFormat() }</Text>
+            </Skeleton>
+            <Skeleton isLoaded={ !isLoading }>
+              <Text as="span">Fee </Text>
+              <Text as="span" variant="secondary">{ getValueWithUnit(tx.fee.value).dp(5).toFormat() }</Text>
+            </Skeleton>
           </Box>
         </Grid>
-        <Box>
-          <Skeleton isLoaded={ !isLoading } mb={ 2 }>
-            <Text as="span" whiteSpace="pre">{ appConfig.network.currency.symbol } </Text>
-            <Text as="span" variant="secondary">{ getValueWithUnit(tx.value).dp(5).toFormat() }</Text>
-          </Skeleton>
-          <Skeleton isLoaded={ !isLoading }>
-            <Text as="span">Fee </Text>
-            <Text as="span" variant="secondary">{ getValueWithUnit(tx.fee.value).dp(5).toFormat() }</Text>
-          </Skeleton>
-        </Box>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   );
 };
 
