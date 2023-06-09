6f12064a5a1eed5de2bf044048336fcaad00457a
isstuev <isstuev@gmail.com>
2023-06-08 21:37:28 -0300

    maybe not the best solution

--- ui/shared/ad/SliseBanner.tsx
+++ ui/shared/ad/SliseBanner.tsx
@@ -2,18 +2,25 @@ import { Flex, chakra } from '@chakra-ui/react';
 import { SliseAd } from '@slise/embed-react';
 import React from 'react';
 
-import useIsMobile from 'lib/hooks/useIsMobile';
-
 const SliseBanner = ({ className }: { className?: string }) => {
-  const isMobile = useIsMobile();
+
   return (
-    <Flex className={ className } h="90px">
-      <SliseAd
-        slotId="leaderboard"
-        pub="pub-10"
-        format={ isMobile ? '270x90' : '728x90' }
-        style={{ width: isMobile ? '270px' : '728px', height: '90px' }}/>
-    </Flex>
+    <>
+      <Flex className={ className } h="90px" display={{ base: 'none', lg: 'flex' }}>
+        <SliseAd
+          slotId="leaderboard"
+          pub="pub-10"
+          format="728x90"
+          style={{ width: '728px', height: '90px' }}/>
+      </Flex>
+      <Flex className={ className } h="90px" display={{ base: 'flex', lg: 'none' }}>
+        <SliseAd
+          slotId="leaderboard"
+          pub="pub-10"
+          format="270x90"
+          style={{ width: '270px', height: '90px' }}/>
+      </Flex>
+    </>
   );
 };
 
