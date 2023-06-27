51ede4f6e1487d1c70eaa3059ae3e5047c1384f9
isstuev <isstuev@gmail.com>
2023-06-27 12:44:05 -0300

    add network name to slise slot id

--- ui/shared/ad/SliseBanner.tsx
+++ ui/shared/ad/SliseBanner.tsx
@@ -2,20 +2,22 @@ import { Flex, chakra } from '@chakra-ui/react';
 import { SliseAd } from '@slise/embed-react';
 import React from 'react';
 
+import appConfig from 'configs/app/config';
+
 const SliseBanner = ({ className }: { className?: string }) => {
 
   return (
     <>
       <Flex className={ className } h="90px" display={{ base: 'none', lg: 'flex' }}>
         <SliseAd
-          slotId="leaderboard"
+          slotId={ appConfig.network.name || '' }
           pub="pub-10"
           format="728x90"
           style={{ width: '728px', height: '90px' }}/>
       </Flex>
       <Flex className={ className } h="90px" display={{ base: 'flex', lg: 'none' }}>
         <SliseAd
-          slotId="leaderboard"
+          slotId={ appConfig.network.name || '' }
           pub="pub-10"
           format="270x90"
           style={{ width: '270px', height: '90px' }}/>
