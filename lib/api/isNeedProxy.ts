7271849de6a02de6a4e0a70b85add668e61be78e
tom <tom@ohhhh.me>
2023-07-10 14:40:25 -0400

    rollback review env changes

--- lib/api/isNeedProxy.ts
+++ lib/api/isNeedProxy.ts
@@ -5,10 +5,5 @@ import appConfig from 'configs/app/config';
 // unsuccessfully tried different ways, even custom local dev domain
 // so for local development we have to use next.js api as proxy server
 export default function isNeedProxy() {
-  // eslint-disable-next-line no-restricted-properties
-  if (process.env.NEXT_PUBLIC_APP_INSTANCE === 'pw') {
-    return false;
-  }
-
   return appConfig.host === 'localhost' && appConfig.host !== appConfig.api.host;
 }
