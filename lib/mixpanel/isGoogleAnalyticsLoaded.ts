656ab2bfc71fc9f81a8f4861616d03903a09d76b
isstuev <isstuev@gmail.com>
2023-07-24 11:20:33 -0300

    fix

--- lib/mixpanel/isGoogleAnalyticsLoaded.ts
+++ lib/mixpanel/isGoogleAnalyticsLoaded.ts
@@ -1,10 +1,9 @@
-function wait() {
-  return new Promise(resolve => setTimeout(resolve, 500));
-}
+import appConfig from 'configs/app/config';
+import delay from 'lib/delay';
 
 export default function isGoogleAnalyticsLoaded(retries = 3): Promise<boolean> {
-  if (!retries) {
+  if (!retries || !appConfig.googleAnalytics.propertyId) {
     return Promise.resolve(false);
   }
-  return typeof window.ga?.getAll === 'function' ? Promise.resolve(true) : wait().then(() => isGoogleAnalyticsLoaded(retries - 1));
+  return typeof window.ga?.getAll === 'function' ? Promise.resolve(true) : delay(500).then(() => isGoogleAnalyticsLoaded(retries - 1));
 }
