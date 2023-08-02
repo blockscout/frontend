767393e258102042789099909b1426d23804ea1f
tom <tom@ohhhh.me>
2023-08-02 13:29:14 -0400

    add resizer img and styles for FF

--- theme/foundations/scrollbar.ts
+++ theme/foundations/scrollbar.ts
@@ -1,23 +1,39 @@
+import { getCSSVar } from '@chakra-ui/styled-system';
 import { mode } from '@chakra-ui/theme-tools';
 import type { StyleFunctionProps } from '@chakra-ui/theme-tools';
 
-const scrollbar = (props: StyleFunctionProps) => ({
-  'body *::-webkit-scrollbar': {
-    width: '20px',
-  },
-  'body *::-webkit-scrollbar-track': {
-    backgroundColor: 'transparent',
-  },
-  'body *::-webkit-scrollbar-thumb': {
-    backgroundColor: mode('blackAlpha.300', 'whiteAlpha.300')(props),
-    borderRadius: '20px',
-    border: `8px solid rgba(0,0,0,0)`,
-    backgroundClip: 'content-box',
-    minHeight: '32px',
-  },
-  'body *::-webkit-scrollbar-button': {
-    display: 'none',
-  },
-});
+const scrollbar = (props: StyleFunctionProps) => {
+  const bgColor = mode('blackAlpha.300', 'whiteAlpha.300')(props);
+
+  return {
+    'body *::-webkit-scrollbar': {
+      width: '20px',
+    },
+    'body *::-webkit-scrollbar-track': {
+      backgroundColor: 'transparent',
+    },
+    'body *::-webkit-scrollbar-thumb': {
+      backgroundColor: bgColor,
+      borderRadius: '20px',
+      border: `8px solid rgba(0,0,0,0)`,
+      backgroundClip: 'content-box',
+      minHeight: '32px',
+    },
+    'body *::-webkit-scrollbar-button': {
+      display: 'none',
+    },
+    'body *::-webkit-scrollbar-corner': {
+      backgroundColor: 'transparent',
+    },
+    'body *::-webkit-resizer': {
+      backgroundImage: 'url(/static/resizer.png)',
+      backgroundSize: '20px',
+    },
+    'body *': {
+      scrollbarWidth: 'thin',
+      scrollbarColor: `${ getCSSVar(props.theme, 'colors', bgColor) } transparent`,
+    },
+  };
+};
 
 export default scrollbar;
