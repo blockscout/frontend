9d6494257d2d125a345c457c7ca40077ea37f516
tom <tom@ohhhh.me>
2023-09-05 16:53:02 -0300

    add contract name

--- ui/address/AddressPageTitle.tsx
+++ ui/address/AddressPageTitle.tsx
@@ -1,18 +1,21 @@
-import { Flex, Skeleton } from '@chakra-ui/react';
+import { Flex, Skeleton, useColorModeValue } from '@chakra-ui/react';
 import type { UseQueryResult } from '@tanstack/react-query';
 import React from 'react';
 
 import type { Address } from 'types/api/address';
 
+import iconFlashlight from 'icons/flashlight.svg';
 import { useAppContext } from 'lib/contexts/app';
 import useIsMobile from 'lib/hooks/useIsMobile';
 import TextAd from 'ui/shared/ad/TextAd';
 import AddressActionButtons from 'ui/shared/AddressActionButtons';
+import Icon from 'ui/shared/chakra/Icon';
 import AddressEntity from 'ui/shared/entities/address/AddressEntity';
 import TokenEntity from 'ui/shared/entities/token/TokenEntity';
 import EntityTags from 'ui/shared/EntityTags';
 import NetworkExplorers from 'ui/shared/NetworkExplorers';
 import * as PageTitle from 'ui/shared/PageTitle/PageTitle';
+import TruncatedValue from 'ui/shared/TruncatedValue';
 
 interface Props {
   addressQuery: UseQueryResult<Address>;
@@ -38,6 +41,73 @@ const AddressPageTitle = ({ addressQuery }: Props) => {
   }, [ appProps.referrer ]);
 
   const titlePrefix = addressQuery.data?.is_contract ? 'Contract ' : 'Address ';
+
+  const actionButtons = (
+    <AddressActionButtons
+      address={ addressQuery.data }
+      token={ addressQuery.data?.token }
+      isLoading={ isLoading }
+    />
+  );
+  const iconColor = useColorModeValue('gray.500', 'gray.400');
+
+  const bottomRow = (() => {
+    if (addressQuery.data?.token) {
+      return (
+        <PageTitle.BottomRow
+          flexWrap="wrap"
+          columnGap={ 6 }
+        >
+          <Flex
+            alignItems="center"
+            fontSize="lg"
+            lineHeight={ 6 }
+            fontWeight={ 500 }
+            w={{ base: '100%', lg: 'auto' }}
+          >
+            <TokenEntity
+              token={ addressQuery.data.token }
+              isLoading={ isLoading }
+              noCopy
+              jointSymbol
+              maxW="600px"
+            />
+            { addressQuery.data.token.exchange_rate && (
+              <Skeleton isLoaded={ !isLoading } ml={ 1 }>
+                { `$${ Number(addressQuery.data.token.exchange_rate).toLocaleString(undefined, { minimumSignificantDigits: 4 }) }` }
+              </Skeleton>
+            ) }
+          </Flex>
+          { actionButtons }
+        </PageTitle.BottomRow>
+      );
+    }
+
+    if (addressQuery.data?.is_contract && addressQuery.data?.name) {
+      return (
+        <PageTitle.BottomRow columnGap={ 6 } flexWrap="wrap">
+          <Flex alignItems="center" maxW={{ base: '100%', lg: '600px' }}>
+            <Icon as={ iconFlashlight } boxSize={ 5 } isLoading={ isLoading } color={ iconColor }/>
+            <TruncatedValue
+              fontFamily="heading"
+              fontSize="lg"
+              fontWeight={ 500 }
+              ml={ 2 }
+              value={ `Name: ${ addressQuery.data.name }` }
+              isLoading={ isLoading }
+            />
+          </Flex>
+          { actionButtons }
+        </PageTitle.BottomRow>
+      );
+    }
+
+    return (
+      <PageTitle.BottomRow>
+        { actionButtons }
+      </PageTitle.BottomRow>
+    );
+  })();
 
   return (
     <PageTitle.Container>
@@ -77,38 +147,7 @@ const AddressPageTitle = ({ addressQuery }: Props) => {
           />
         </PageTitle.SecondaryContent>
       </PageTitle.MainRow>
-      <PageTitle.BottomRow
-        flexWrap={ addressQuery.data?.token ? 'wrap' : 'nowrap' }
-        columnGap={ 6 }
-      >
-        { addressQuery.data?.token && (
-          <Flex
-            alignItems="center"
-            fontSize="lg"
-            lineHeight={ 6 }
-            fontWeight={ 500 }
-            w={{ base: '100%', lg: 'auto' }}
-          >
-            <TokenEntity
-              token={ addressQuery.data.token }
-              isLoading={ isLoading }
-              noCopy
-              jointSymbol
-              maxW="600px"
-            />
-            { addressQuery.data.token.exchange_rate && (
-              <Skeleton isLoaded={ !isLoading } ml={ 1 }>
-                { `$${ Number(addressQuery.data.token.exchange_rate).toLocaleString(undefined, { minimumSignificantDigits: 4 }) }` }
-              </Skeleton>
-            ) }
-          </Flex>
-        ) }
-        <AddressActionButtons
-          address={ addressQuery.data }
-          token={ addressQuery.data?.token }
-          isLoading={ isLoading }
-        />
-      </PageTitle.BottomRow>
+      { bottomRow }
     </PageTitle.Container>
   );
 };
--- ui/address/details/AddressNameInfo.tsx
+++ ui/address/details/AddressNameInfo.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import type { Address } from 'types/api/address';
 
 import DetailsInfoItem from 'ui/shared/DetailsInfoItem';
-import TruncatedValue from 'ui/shared/TruncatedValue';
 
 interface Props {
   data: Pick<Address, 'name' | 'token' | 'is_contract'>;
@@ -12,19 +11,7 @@ interface Props {
 }
 
 const AddressNameInfo = ({ data, isLoading }: Props) => {
-  if (data.is_contract && data.name) {
-    return (
-      <DetailsInfoItem
-        title="Contract name"
-        hint="The name found in the source code of the Contract"
-        isLoading={ isLoading }
-      >
-        <TruncatedValue value={ data.name } isLoading={ isLoading }/>
-      </DetailsInfoItem>
-    );
-  }
-
-  if (data.name) {
+  if (data.name && !data.is_contract) {
     return (
       <DetailsInfoItem
         title="Validator name"
