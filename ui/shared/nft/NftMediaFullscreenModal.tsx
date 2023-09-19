85b0c40c93fbbdca630fc67d17986643a798aaae
isstuev <isstuev@gmail.com>
2023-09-19 10:32:33 +0200

    modal background fix

--- ui/shared/nft/NftMediaFullscreenModal.tsx
+++ ui/shared/nft/NftMediaFullscreenModal.tsx
@@ -16,7 +16,7 @@ const NftMediaFullscreenModal = ({ isOpen, onClose, children }: Props) => {
   return (
     <Modal isOpen={ isOpen } onClose={ onClose } motionPreset="none">
       <ModalOverlay/>
-      <ModalContent w="unset" maxW="100vw" p={ 0 }>
+      <ModalContent w="unset" maxW="100vw" p={ 0 } background="none" boxShadow="none">
         <ModalCloseButton position="fixed" top={{ base: 2.5, lg: 8 }} right={{ base: 2.5, lg: 8 }} color="whiteAlpha.800"/>
         { children }
       </ModalContent>
