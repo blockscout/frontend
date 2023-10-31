f47d0bff1ac9f00abe1f3f8eab7ce72b68eb3ac7
Wolmin <lamonos123@gmail.com>
2023-10-31 15:44:00 +0100

    Fix linter

--- ui/shared/entities/base/components.tsx
+++ ui/shared/entities/base/components.tsx
@@ -64,7 +64,6 @@ const Link = chakra(({ isLoading, children, isExternal, onClick, href, noLink }:
   const Component = isExternal ? LinkExternal : LinkInternal;
 
   return (
-    // @ts-ignore
     <Component
       { ...styles }
       href={ href }
