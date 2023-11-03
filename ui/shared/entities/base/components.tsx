8877b6d96abfca6cdb0c4f9244c036573319b350
Wolmin <lamonos123@gmail.com>
2023-11-03 17:25:34 +0100

    Fix union type too complex

--- ui/shared/entities/base/components.tsx
+++ ui/shared/entities/base/components.tsx
@@ -61,7 +61,7 @@ const Link = chakra(({ isLoading, children, isExternal, onClick, href, noLink }:
     return <Skeleton isLoaded={ !isLoading } { ...styles }>{ children }</Skeleton>;
   }
 
-  const Component = isExternal ? LinkExternal : LinkInternal;
+  const Component: React.FC<LinkBaseProps> = isExternal ? LinkExternal : LinkInternal;
 
   return (
     <Component
