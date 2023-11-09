8ac0ee96b74cde6450563e7556b6b669f0322a12
Wolmin <lamonos123@gmail.com>
2023-11-09 10:21:38 +0100

    Shorten UP name in specific case

--- ui/shared/HashStringShorten.tsx
+++ ui/shared/HashStringShorten.tsx
@@ -1,8 +1,11 @@
 import { Tooltip, chakra } from '@chakra-ui/react';
 import type { As } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import shortenString from 'lib/shortenString';
+
+import config from '../../configs/app';
+import shortenUniversalProfile from '../../lib/shortenUniversalProfile';
 
 interface Props {
   hash: string;
@@ -11,13 +14,20 @@ interface Props {
 }
 
 const HashStringShorten = ({ hash, isTooltipDisabled, as = 'span' }: Props) => {
+  const [ shortenedString, setShortenedString ] = React.useState(shortenString(hash));
+  useEffect(() => {
+    if (config.UI.views.address.identiconType === 'universal_profile' && hash.includes('#')) {
+      setShortenedString(shortenUniversalProfile(hash));
+    }
+  }, [ hash ]);
+
   if (hash.length <= 8) {
     return <chakra.span as={ as }>{ hash }</chakra.span>;
   }
 
   return (
     <Tooltip label={ hash } isDisabled={ isTooltipDisabled }>
-      <chakra.span as={ as }>{ shortenString(hash) }</chakra.span>
+      <chakra.span as={ as }>{ shortenedString }</chakra.span>
     </Tooltip>
   );
 };
