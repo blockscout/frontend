adffb78d5613b67fb66694e3135c4627202fc5d7
Wolmin <lamonos123@gmail.com>
2023-11-29 15:17:03 +0100

    Merge two queries together

--- ui/snippets/searchBar/useQuickSearchQuery.tsx
+++ ui/snippets/searchBar/useQuickSearchQuery.tsx
@@ -1,6 +1,8 @@
-// import { useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import React from 'react';
+
+import type { SearchResultItem } from '../../../types/api/search';
 
 import useApiQuery from 'lib/api/useApiQuery';
 import useDebounce from 'lib/hooks/useDebounce';
@@ -15,10 +17,10 @@ export default function useQuickSearchQuery() {
   const debouncedSearchTerm = useDebounce(searchTerm, 300);
   const pathname = router.pathname;
 
-  // const query = useApiQuery('quick_search', {
-  //   queryParams: { q: debouncedSearchTerm },
-  //   queryOptions: { enabled: debouncedSearchTerm.trim().length > 0 },
-  // });
+  const quickSearchQuery = useApiQuery('quick_search', {
+    queryParams: { q: debouncedSearchTerm },
+    queryOptions: { enabled: debouncedSearchTerm.trim().length > 0 },
+  });
 
   const redirectCheckQuery = useApiQuery('search_check_redirect', {
     // on pages with regular search bar we check redirect on every search term change
@@ -27,9 +29,19 @@ export default function useQuickSearchQuery() {
     queryOptions: { enabled: Boolean(debouncedSearchTerm) },
   });
 
-  const query = useUniversalProfileQuery('universal_profile', {
+  const upQuery = useUniversalProfileQuery('universal_profile', {
     queryParams: { q: debouncedSearchTerm },
     queryOptions: { enabled: debouncedSearchTerm.trim().length > 0 },
+  });
+
+  const query = useQuery({
+    queryKey: [ 'merged_query', quickSearchQuery, upQuery ],
+    queryFn: () => {
+      const q1 = quickSearchQuery.data as Array<SearchResultItem>;
+      const q2 = upQuery.data as Array<SearchResultItem>;
+
+      return [ ...q1, ...q2 ];
+    },
   });
 
   return React.useMemo(() => ({
