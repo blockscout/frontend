28d466a211d6e2ef84fb5212d4b7ed3d79a4d85b
Alex <aesshoferi@gmail.com>
2023-12-01 15:51:25 +0330

    ui: update favicon based on theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,9 +38,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" sizes="32x32" type="image/png" href="/static/favicon-32x32.png"/>
-          <link rel="icon" sizes="16x16" type="image/png"href="/static/favicon-16x16.png"/>
-          <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png"/>
           <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5"/>
           <meta property="og:title" content="Canto EVM Explorer"/>
           <meta
