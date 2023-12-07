3d419e0c43d26e4a2f3328e47d9f10af6cd20e28
tom <tom@ohhhh.me>
2023-12-07 17:33:22 +0400

    [skip ci] Refactor local development setup and scripts

--- tools/scripts/dev.sh
+++ tools/scripts/dev.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: yarn dev:preset <preset_name>"
-  exit 1
-fi
-
-preset_name="$1"
-config_file="./configs/envs/.env.${preset_name}"
-secrets_file="./configs/envs/.env.secrets"
-
-if [ ! -f "$config_file" ]; then
-    echo "Error: File '$config_file' not found."
-    exit 1
-fi
-
 # download assets for the running instance
 dotenv \
-  -e $config_file \
+  -e .env.development.local \
+  -e .env.local \
+  -e .env.development \
+  -e .env \
   -- bash -c './deploy/scripts/download_assets.sh ./public/assets'
 
 # generate envs.js file and run the app
 dotenv \
   -v NEXT_PUBLIC_GIT_COMMIT_SHA=$(git rev-parse --short HEAD) \
   -v NEXT_PUBLIC_GIT_TAG=$(git describe --tags --abbrev=0) \
-  -e $config_file \
-  -e $secrets_file \
+  -e .env.secrets \
+  -e .env.development.local \
+  -e .env.local \
+  -e .env.development \
+  -e .env \
   -- bash -c './deploy/scripts/make_envs_script.sh && next dev -- -p $NEXT_PUBLIC_APP_PORT' |
 pino-pretty
