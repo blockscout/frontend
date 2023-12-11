04d087ef9f1ccb69fba9a4f8896f9c4149a0171c
Max Alekseenko <nnwen0609@gmail.com>
2023-12-11 13:00:12 +0100

    integrate dappscout-iframe

--- ui/pages/MarketplaceApp.tsx
+++ ui/pages/MarketplaceApp.tsx
@@ -1,7 +1,8 @@
 import { Box, Center, useColorMode } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
+import { DappscoutIframeProvider, useDappscoutIframe } from 'dappscout-iframe';
 import { useRouter } from 'next/router';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import type { MarketplaceAppOverview } from 'types/client/marketplace';
 
@@ -14,6 +15,8 @@ import * as metadata from 'lib/metadata';
 import getQueryParamString from 'lib/router/getQueryParamString';
 import ContentLoader from 'ui/shared/ContentLoader';
 
+import useMarketplaceWallet from '../marketplace/useMarketplaceWallet';
+
 const feature = config.features.marketplace;
 const configUrl = feature.isEnabled ? feature.configUrl : '';
 
@@ -24,33 +27,22 @@ const IFRAME_SANDBOX_ATTRIBUTE = 'allow-forms allow-orientation-lock ' +
 
 const IFRAME_ALLOW_ATTRIBUTE = 'clipboard-read; clipboard-write;';
 
-const MarketplaceApp = () => {
-  const ref = useRef<HTMLIFrameElement>(null);
+type Props = {
+  address: string | undefined;
+  data: MarketplaceAppOverview | undefined;
+  isPending: boolean;
+};
 
-  const apiFetch = useApiFetch();
-  const router = useRouter();
-  const id = getQueryParamString(router.query.id);
+const MarketplaceAppContent = ({ address, data, isPending }: Props) => {
+  const { iframeRef, isReady } = useDappscoutIframe();
 
-  const { isPending, isError, error, data } = useQuery<unknown, ResourceError<unknown>, MarketplaceAppOverview>({
-    queryKey: [ 'marketplace-apps', id ],
-    queryFn: async() => {
-      const result = await apiFetch<Array<MarketplaceAppOverview>, unknown>(configUrl, undefined, { resource: 'marketplace-apps' });
-      if (!Array.isArray(result)) {
-        throw result;
-      }
-
-      const item = result.find((app: MarketplaceAppOverview) => app.id === id);
-      if (!item) {
-        throw { status: 404 };
-      }
-
-      return item;
-    },
-    enabled: feature.isEnabled,
-  });
-
+  const [ iframeKey, setIframeKey ] = useState(0);
   const [ isFrameLoading, setIsFrameLoading ] = useState(isPending);
   const { colorMode } = useColorMode();
+
+  useEffect(() => {
+    setIframeKey((key) => key + 1);
+  }, [ address ]);
 
   const handleIframeLoad = useCallback(() => {
     setIsFrameLoading(false);
@@ -69,9 +61,61 @@ const MarketplaceApp = () => {
         blockscoutNetworkRpc: config.chain.rpcUrl,
       };
 
-      ref?.current?.contentWindow?.postMessage(message, data.url);
+      iframeRef?.current?.contentWindow?.postMessage(message, data.url);
     }
-  }, [ isFrameLoading, data, colorMode, ref ]);
+  }, [ isFrameLoading, data, colorMode, iframeRef ]);
+
+  return (
+    <Center
+      h="100vh"
+      mx={{ base: -4, lg: -6 }}
+    >
+      { (isFrameLoading) && (
+        <ContentLoader/>
+      ) }
+
+      { (data && isReady) && (
+        <Box
+          key={ iframeKey }
+          allow={ IFRAME_ALLOW_ATTRIBUTE }
+          ref={ iframeRef }
+          sandbox={ IFRAME_SANDBOX_ATTRIBUTE }
+          as="iframe"
+          h="100%"
+          w="100%"
+          display={ isFrameLoading ? 'none' : 'block' }
+          src={ data.url }
+          title={ data.title }
+          onLoad={ handleIframeLoad }
+        />
+      ) }
+    </Center>
+  );
+};
+
+const MarketplaceApp = () => {
+  const { address, sendTransaction, signMessage, signTypedData } = useMarketplaceWallet();
+
+  const apiFetch = useApiFetch();
+  const router = useRouter();
+  const id = getQueryParamString(router.query.id);
+
+  const { isPending, isError, error, data } = useQuery<unknown, ResourceError<unknown>, MarketplaceAppOverview>({
+    queryKey: [ 'marketplace-apps', id ],
+    queryFn: async() => {
+      const result = await apiFetch<Array<MarketplaceAppOverview>, unknown>(configUrl, undefined, { resource: 'marketplace-apps' });
+      if (!Array.isArray(result)) {
+        throw result;
+      }
+      const item = result.find((app: MarketplaceAppOverview) => app.id === id);
+      if (!item) {
+        throw { status: 404 };
+      }
+
+      return item;
+    },
+    enabled: feature.isEnabled,
+  });
 
   useEffect(() => {
     if (data) {
@@ -87,29 +131,16 @@ const MarketplaceApp = () => {
   }
 
   return (
-    <Center
-      h="100vh"
-      mx={{ base: -4, lg: -6 }}
+    <DappscoutIframeProvider
+      address={ address }
+      appUrl={ data?.url }
+      rpcUrl={ config.chain.rpcUrl }
+      sendTransaction={ sendTransaction }
+      signMessage={ signMessage }
+      signTypedData={ signTypedData }
     >
-      { (isFrameLoading) && (
-        <ContentLoader/>
-      ) }
-
-      { data && (
-        <Box
-          allow={ IFRAME_ALLOW_ATTRIBUTE }
-          ref={ ref }
-          sandbox={ IFRAME_SANDBOX_ATTRIBUTE }
-          as="iframe"
-          h="100%"
-          w="100%"
-          display={ isFrameLoading ? 'none' : 'block' }
-          src={ data.url }
-          title={ data.title }
-          onLoad={ handleIframeLoad }
-        />
-      ) }
-    </Center>
+      <MarketplaceAppContent address={ address } data={ data } isPending={ isPending }/>
+    </DappscoutIframeProvider>
   );
 };
 
