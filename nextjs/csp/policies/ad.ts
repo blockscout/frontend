754c2c4efd6d1d5f41b6d01f36345670705f067d
isstuev <natix.naf@gmail.com>
2024-01-02 15:49:23 +0100

    add hype provider (dev config)

--- nextjs/csp/policies/ad.ts
+++ nextjs/csp/policies/ad.ts
@@ -7,26 +7,46 @@ import { connectAdbutler, placeAd } from 'ui/shared/ad/adbutlerScript';
 export function ad(): CspDev.DirectiveDescriptor {
   return {
     'connect-src': [
+      // coinzilla
       'coinzilla.com',
       '*.coinzilla.com',
       'https://request-global.czilladx.com',
+
+      // slise
       '*.slise.xyz',
+
+      // hype
+      'api.hypelab-staging.com',
+      'api.hypelab.com',
+      'd1q98dzwj6s2rb.cloudfront.net',
+      '*.ixncdn.com',
+      'api.jxncdn.com',
     ],
     'frame-src': [
+      // coinzilla
       'https://request-global.czilladx.com',
     ],
     'script-src': [
+      // coinzilla
       'coinzillatag.com',
+
+      // adbutler
       'servedbyadbutler.com',
       `'sha256-${ Base64.stringify(sha256(connectAdbutler)) }'`,
       `'sha256-${ Base64.stringify(sha256(placeAd ?? '')) }'`,
+
+      // slise
       '*.slise.xyz',
     ],
     'img-src': [
-      'servedbyadbutler.com',
+      // coinzilla
       'cdn.coinzilla.io',
+
+      // adbutler
+      'servedbyadbutler.com',
     ],
     'font-src': [
+      // coinzilla
       'https://request-global.czilladx.com',
     ],
   };
