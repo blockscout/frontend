58710aecddb50d5322f6f316ba626c102e4a68f3
tom <tom@ohhhh.me>
2024-01-19 13:02:02 +0400

    test fix

--- ui/shared/ErrorBoundary.tsx
+++ ui/shared/ErrorBoundary.tsx
@@ -1,11 +1,14 @@
 import type { NextRouter } from 'next/router';
-import { withRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 interface Props {
   children: React.ReactNode;
   renderErrorScreen: (error?: Error) => React.ReactNode;
   onError?: (error: Error) => void;
+}
+
+interface PropsWithRouter extends Props {
   router: NextRouter;
 }
 
@@ -15,12 +18,12 @@ interface State {
   errorPathname?: string;
 }
 
-class ErrorBoundary extends React.PureComponent<Props, State> {
+class ErrorBoundary extends React.PureComponent<PropsWithRouter, State> {
   state: State = {
     hasError: false,
   };
 
-  static getDerivedStateFromProps(props: Props, state: State) {
+  static getDerivedStateFromProps(props: PropsWithRouter, state: State) {
     if (state.hasError && state.errorPathname) {
       if (props.router.pathname !== state.errorPathname) {
         return { hasError: false, error: undefined, errorPathname: undefined };
@@ -44,4 +47,9 @@ class ErrorBoundary extends React.PureComponent<Props, State> {
   }
 }
 
-export default withRouter(ErrorBoundary);
+const WrappedErrorBoundary = (props: Props) => {
+  const router = useRouter();
+  return <ErrorBoundary { ...props } router={ router }/>;
+};
+
+export default React.memo(WrappedErrorBoundary);
