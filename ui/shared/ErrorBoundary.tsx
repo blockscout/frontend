1fb0ccc4f352cde3d6639498f119ac978785eab8
tom <tom@ohhhh.me>
2024-01-19 10:17:02 +0400

    navigation doesn't work on error pages
    Fixes #1518

--- ui/shared/ErrorBoundary.tsx
+++ ui/shared/ErrorBoundary.tsx
@@ -1,14 +1,18 @@
+import type { NextRouter } from 'next/router';
+import { withRouter } from 'next/router';
 import React from 'react';
 
 interface Props {
   children: React.ReactNode;
   renderErrorScreen: (error?: Error) => React.ReactNode;
   onError?: (error: Error) => void;
+  router: NextRouter;
 }
 
 interface State {
   hasError: boolean;
   error?: Error;
+  errorPathname?: string;
 }
 
 class ErrorBoundary extends React.PureComponent<Props, State> {
@@ -16,11 +20,18 @@ class ErrorBoundary extends React.PureComponent<Props, State> {
     hasError: false,
   };
 
-  static getDerivedStateFromError(error: Error) {
-    return { hasError: true, error };
+  static getDerivedStateFromProps(props: Props, state: State) {
+    if (state.hasError && state.errorPathname) {
+      if (props.router.pathname !== state.errorPathname) {
+        return { hasError: false, error: undefined, errorPathname: undefined };
+      }
+    }
+
+    return null;
   }
 
   componentDidCatch(error: Error) {
+    this.setState({ hasError: true, error, errorPathname: this.props.router.pathname });
     this.props.onError?.(error);
   }
 
@@ -33,4 +44,4 @@ class ErrorBoundary extends React.PureComponent<Props, State> {
   }
 }
 
-export default ErrorBoundary;
+export default withRouter(ErrorBoundary);
