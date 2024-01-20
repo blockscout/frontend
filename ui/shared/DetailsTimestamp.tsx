285a4c5de1892461319b007e72213afa46e641f1
isstuev <natix.naf@gmail.com>
2024-01-20 17:03:28 +0100

    timestamp margin fix

--- ui/shared/DetailsTimestamp.tsx
+++ ui/shared/DetailsTimestamp.tsx
@@ -15,7 +15,7 @@ const DetailsTimestamp = ({ timestamp, isLoading }: Props) => {
   return (
     <>
       <IconSvg name="clock" boxSize={ 5 } color="gray.500" isLoading={ isLoading }/>
-      <Skeleton isLoaded={ !isLoading } ml={ 1 }>
+      <Skeleton isLoaded={ !isLoading } ml={ 2 }>
         { dayjs(timestamp).fromNow() }
       </Skeleton>
       <TextSeparator/>
