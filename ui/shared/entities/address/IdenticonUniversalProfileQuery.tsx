114d468f530b77813e7b2ed5eb6c5d935ecf63ca
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-01-22 08:33:00 -0500

    fix: Add a branch to document my thoughts.
    fix: Repair caching of lukso api calls.
    
    fix: Also adjust code which is getting the name
    
    fix: Disable refresh and stale handling.
    
    fix: Make it stale for 1 hours.
    
    fix: Remove lookup for 0x0000...0000 address on the api
    
    fix: Prevent flickering from one icon to another.

--- ui/shared/entities/address/IdenticonUniversalProfileQuery.tsx
+++ ui/shared/entities/address/IdenticonUniversalProfileQuery.tsx
@@ -1,7 +1,7 @@
-import { Box } from '@chakra-ui/react';
-import type { QueryClient } from '@tanstack/react-query';
-import { useQueryClient } from '@tanstack/react-query';
-import React, { useEffect, useState } from 'react';
+import { Box, Skeleton } from '@chakra-ui/react';
+import type { UseQueryResult } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
 
 import type { UniversalProfileProxyResponse } from '../../../../types/api/universalProfile';
 
@@ -17,19 +17,20 @@ export const formattedLuksoName = (hash: string, name: string | null) => {
   return `@${ name } (${ hash })`;
 };
 
-export const getUniversalProfile = async(address: string, queryClient: QueryClient) => {
-  if (!isUniversalProfileEnabled()) {
-    return undefined;
-  }
-  const query = queryClient.getQueryData<UniversalProfileProxyResponse>([ 'universalProfile', { address: address } ]);
-  if (query !== undefined) {
-    return query;
-  }
-
-  return await queryClient.fetchQuery({
-    queryKey: [ 'universalProfile', { address: address } ],
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const useUniversalProfile = (address: string): UseQueryResult<UniversalProfileProxyResponse | null> => {
+  return useQuery({
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    retryOnMount: false,
+    staleTime: 60 * 60 * 1000,
+    queryKey: [ 'universalProfile', { address } ],
     queryFn: async() => {
-      const upApiUrl = getEnvValue('NEXT_PUBLIC_UNIVERSAL_PROFILES_API_URL') || '';
+      if (!isUniversalProfileEnabled() || /0x0+$/i.test(address)) {
+        return null;
+      }
+      const upApiUrl =
+        getEnvValue('NEXT_PUBLIC_UNIVERSAL_PROFILES_API_URL') || '';
       const networkId = getEnvValue('NEXT_PUBLIC_NETWORK_ID') || '42';
 
       const url = `${ upApiUrl }/v1/${ networkId }/address/${ address }`;
@@ -38,46 +39,41 @@ export const getUniversalProfile = async(address: string, queryClient: QueryClie
         const json = await resp.json();
         return json as UniversalProfileProxyResponse;
       } catch (err) {
-        return undefined;
+        return null;
       }
     },
   });
 };
 
-export const IdenticonUniversalProfile: React.FC<Props> = ({ address, fallbackIcon }) => {
-  const [ up, setUp ] = useState({} as UniversalProfileProxyResponse);
-  const queryClient = useQueryClient();
-  useEffect(() => {
-    (async() => {
-      const upData = await getUniversalProfile(address, queryClient);
-      if (upData !== undefined) {
-        setUp(upData);
-
-        return;
-      }
-    })();
-  }, [ address, setUp, queryClient ]);
-
-  if (up === undefined || up.LSP3Profile === undefined) {
-    return fallbackIcon;
-  }
+export const IdenticonUniversalProfile: React.FC<Props> = ({
+  address,
+  fallbackIcon,
+}) => {
+  const { data: up, isLoading } = useUniversalProfile(address);
 
   let profileImageUrl = '';
-  if (up.LSP3Profile.profileImage !== null) {
-    const lastImageIndex = Object.values(up.LSP3Profile.profileImage).length - 1;
+  if (up?.LSP3Profile?.profileImage !== null && up?.hasProfileImage) {
+    const lastImageIndex =
+      Object.values(up?.LSP3Profile?.profileImage || {}).length - 1;
 
-    profileImageUrl = up.hasProfileImage ? up.LSP3Profile.profileImage[lastImageIndex].url : '';
+    profileImageUrl = up?.hasProfileImage ?
+      up?.LSP3Profile?.profileImage[lastImageIndex].url :
+      '';
   }
 
   return (
-    <Box mr={ 2 }>
-      <lukso-profile
-        size="x-small"
-        profile-url={ profileImageUrl }
-        profile-address={ address }
-        has-identicon={ true }
-      ></lukso-profile>
-    </Box>
+    <Skeleton isLoaded={ !isLoading }>
+      { profileImageUrl ? (
+        <Box mx={ 2 }>
+          <lukso-profile
+            size="x-small"
+            profile-url={ profileImageUrl }
+            profile-address={ address }
+            has-identicon={ true }
+          ></lukso-profile>
+        </Box>
+      ) : fallbackIcon }
+    </Skeleton>
   );
 };
 
