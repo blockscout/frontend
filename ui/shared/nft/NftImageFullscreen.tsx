7ed1aa9f30c8ba1edf72dfc960c9faddedf109bc
isstuev <natix.naf@gmail.com>
2024-01-31 17:23:27 +0100

    fix fullscreen

--- ui/shared/nft/NftImageFullscreen.tsx
+++ ui/shared/nft/NftImageFullscreen.tsx
@@ -11,12 +11,21 @@ interface Props {
   onClose: () => void;
 }
 
-const NftImageWithFullscreen = ({ src, isOpen, onClose }: Props) => {
+const NftImageFullscreen = ({ src, isOpen, onClose }: Props) => {
+  const imgRef = React.useRef<HTMLImageElement>(null);
+
+  const checkWidth = React.useCallback(() => {
+    if (imgRef.current?.getBoundingClientRect().width === 0) {
+      imgRef.current.style.width = '90vw';
+      imgRef.current.style.height = '90vh';
+    }
+  }, [ ]);
+
   return (
     <NftMediaFullscreenModal isOpen={ isOpen } onClose={ onClose }>
-      <Image src={ src } alt="Token instance image" maxH="90vh" maxW="90vw"/>
+      <Image src={ src } alt="Token instance image" maxH="90vh" maxW="90vw" ref={ imgRef } onLoad={ checkWidth }/>
     </NftMediaFullscreenModal>
   );
 };
 
-export default NftImageWithFullscreen;
+export default NftImageFullscreen;
