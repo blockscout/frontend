64a8bed7d32d044949373486dd5276dd0f4c4787
tom <tom@ohhhh.me>
2024-02-01 12:33:51 +0400

    script for finding affected test suites

---
+++ deploy/tools/affected-tests/index.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-console */
+const { execSync } = require('child_process');
+const dependencyTree = require('dependency-tree');
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = path.resolve(__dirname, '../../../');
+const targetFile = path.resolve(rootDir, './playwright/affected-tests.txt');
+const NON_EXISTENT_DEPS = [];
+
+function getAllPwFilesInDirectory(directory) {
+  const files = fs.readdirSync(directory, { recursive: true });
+  return files
+    .filter((file) => file.endsWith('.pw.tsx'))
+    .map((file) => path.join(directory, file));
+}
+
+function getFileDeps(filename) {
+  return dependencyTree.toList({
+    filename,
+    directory: rootDir,
+    filter: (path) => {
+      return path.indexOf('node_modules') === -1;
+    },
+    tsConfig: path.resolve(rootDir, './tsconfig.json'),
+    nonExistent: NON_EXISTENT_DEPS,
+  });
+}
+
+async function getChangedFiles() {
+  const files = execSync(`git diff --name-only main...$(git symbolic-ref --short HEAD)`)
+    .toString()
+    .trim()
+    .split('\n')
+    .filter(Boolean);
+
+  return files.map((file) => path.join(rootDir, file));
+}
+
+async function run() {
+  fs.unlink(targetFile, () => {});
+
+  const changedFiles = await getChangedFiles();
+
+  if (!changedFiles.length) {
+    console.log('No changed files found. Exiting...');
+    return;
+  }
+
+  const targetDir = path.resolve(rootDir, './ui/shared');
+
+  const start = Date.now();
+  const testFilesToRun = getAllPwFilesInDirectory(targetDir)
+    .slice(0, 10)
+    .map((file) => ({
+      file,
+      deps: getFileDeps(file),
+    }))
+    .filter(({ deps }) => deps.some((dep) => changedFiles.includes(dep)));
+  const testFileNamesToRun = testFilesToRun.map(({ file }) => path.relative(rootDir, file));
+
+  if (!testFileNamesToRun.length) {
+    console.log('No tests to run. Exiting...');
+    return;
+  }
+
+  const end = Date.now();
+  console.log('Total time: ', ((end - start) / 1_000).toLocaleString());
+
+  console.log('ChangedFiles: ', changedFiles);
+  console.log('Test to run: ', testFileNamesToRun);
+  console.log('Non existent deps: ', NON_EXISTENT_DEPS);
+
+  fs.writeFileSync(targetFile, testFileNamesToRun.join('\n'));
+}
+
+run();
--- tools/scripts/pw.sh
+++ tools/scripts/pw.sh
@@ -10,7 +10,67 @@ dotenv \
 
 yarn svg:build-sprite
 
+# Check if the "--affected" argument is present in the script args
+check_affected_flag() {
+    local affected_flag=false
+
+    for arg in "$@"; do
+        if [ "$arg" = "--affected" ]; then
+            # Extract the value after the equals sign
+            is_affected_value=${is_affected_arg#*=}
+
+            if [ "$is_affected_value" != "false" ]; then
+                affected_flag=true
+            fi
+            
+            break
+        fi
+    done
+
+    echo "$affected_flag"
+}
+
+# Remove the "--affected" argument from the script args
+filter_arguments() {
+    local args=()
+
+    for arg in "$@"; do
+        if [[ "$arg" != "--affected"* ]]; then
+            args+=("$arg")
+        fi
+    done
+
+    echo "${args[@]}"
+}
+
+get_files_to_run() {
+  local is_affected=$1
+  local files_to_run=""
+
+  if [ "$is_affected" = true ]; then
+      affected_tests_file="./playwright/affected-tests.txt"
+
+      if [ -f "$affected_tests_file" ]; then
+          file_content=$(<"$affected_tests_file")
+          files_to_run="${file_content//$'\n'/$' '}"
+      else 
+          echo "No affected tests found. Running all tests."
+      fi
+  fi
+
+  echo "$files_to_run"
+}
+
+args=$(filter_arguments "$@")
+affected_flag=$(check_affected_flag "$@")
+files_to_run=$(get_files_to_run "$affected_flag")
+
+echo "Running Playwright tests with the following arguments: $args"
+echo "Affected flag: $affected_flag"
+echo "Files to run: $files_to_run"
+
 dotenv \
   -v NODE_OPTIONS=\"--max-old-space-size=4096\" \
   -e $config_file \
-  -- playwright test -c playwright-ct.config.ts "$@"
+  -- playwright test -c playwright-ct.config.ts $files_to_run "${args[@]}"
+
