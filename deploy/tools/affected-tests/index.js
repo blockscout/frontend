9c81818f15b7c6eebc051f4544a9da431f2d2082
tom <tom@ohhhh.me>
2024-02-02 08:46:35 +0400

    special directories cases

--- deploy/tools/affected-tests/index.js
+++ deploy/tools/affected-tests/index.js
@@ -4,9 +4,15 @@ const dependencyTree = require('dependency-tree');
 const fs = require('fs');
 const path = require('path');
 
-const rootDir = path.resolve(__dirname, '../../../');
-const targetFile = path.resolve(rootDir, './playwright/affected-tests.txt');
+const ROOT_DIR = path.resolve(__dirname, '../../../');
+
+const TARGET_FILE = path.resolve(ROOT_DIR, './playwright/affected-tests.txt');
+
 const NON_EXISTENT_DEPS = [];
+
+const DIRECTORIES_WITH_TESTS = [
+  path.resolve(ROOT_DIR, './ui/shared'),
+];
 
 function getAllPwFilesInDirectory(directory) {
   const files = fs.readdirSync(directory, { recursive: true });
@@ -18,18 +24,20 @@ function getAllPwFilesInDirectory(directory) {
 function getFileDeps(filename) {
   return dependencyTree.toList({
     filename,
-    directory: rootDir,
+    directory: ROOT_DIR,
     filter: (path) => {
       return path.indexOf('node_modules') === -1;
     },
-    tsConfig: path.resolve(rootDir, './tsconfig.json'),
+    tsConfig: path.resolve(ROOT_DIR, './tsconfig.json'),
     nonExistent: NON_EXISTENT_DEPS,
   });
 }
 
 async function getChangedFiles() {
-  // eslint-disable-next-line max-len
-  const command = `git diff --name-only origin/${ process.env.GITHUB_BASE_REF || 'main' } origin/${ process.env.GITHUB_HEAD_REF || '$(git branch --show-current)' } -- ${ rootDir }`;
+  const command = process.env.CI ?
+    `git diff --name-only origin/${ process.env.GITHUB_BASE_REF } ${ process.env.GITHUB_SHA } -- ${ ROOT_DIR }` :
+    `git diff --name-only main $(git branch --show-current) -- ${ ROOT_DIR }`;
+
   console.log('Executing command: ', command);
   const files = execSync(command)
     .toString()
@@ -37,33 +45,82 @@ async function getChangedFiles() {
     .split('\n')
     .filter(Boolean);
 
-  return files.map((file) => path.join(rootDir, file));
+  return files.map((file) => path.join(ROOT_DIR, file));
+}
+
+function checkChangesInChakraTheme(changedFiles) {
+  const themeDir = path.resolve(ROOT_DIR, './theme');
+  return changedFiles.some((file) => file.startsWith(themeDir));
+}
+
+function checkChangesInSvgSprite(changedFiles) {
+  const iconDir = path.resolve(ROOT_DIR, './icons');
+  const areIconsChanged = changedFiles.some((file) => file.startsWith(iconDir));
+
+  if (!areIconsChanged) {
+    return false;
+  }
+
+  const svgNamesFile = path.resolve(ROOT_DIR, './public/icons/name.d.ts');
+  const isSvgNamesChanged = changedFiles.some((file) => file === svgNamesFile);
+
+  if (!isSvgNamesChanged) {
+    // If only the icons have changed and not the names in the SVG file, we will need to run all tests.
+    // This is because we cannot correctly identify the test files that depend on these changes.
+    return true;
+  }
+
+  // If the icon names have changed, then there should be changes in the components that use them.
+  // Otherwise, typescript would complain about that.
+  return false;
+}
+
+function createTargetFile(content) {
+  fs.writeFileSync(TARGET_FILE, content);
+
 }
 
 async function run() {
-  fs.unlink(targetFile, () => {});
+  // NOTES:
+  // - The absence of TARGET_FILE implies that all tests should be run.
+  // - The empty TARGET_FILE implies that no tests should be run.
+
+  fs.unlink(TARGET_FILE, () => {});
 
   const changedFiles = await getChangedFiles();
-  console.log('ChangedFiles: ', changedFiles);
 
   if (!changedFiles.length) {
+    createTargetFile('');
     console.log('No changed files found. Exiting...');
     return;
   }
 
-  const targetDir = path.resolve(rootDir, './ui/shared');
+  console.log('Changed files in the branch: ', changedFiles);
+
+  if (checkChangesInChakraTheme(changedFiles)) {
+    console.log('Changes in Chakra theme detected. It is advisable to run all test suites. Exiting...');
+    return;
+  }
+
+  if (checkChangesInSvgSprite(changedFiles)) {
+    console.log('There are some changes in the SVG sprite that cannot be linked to a specific component. It is advisable to run all test suites. Exiting...');
+    return;
+  }
 
   const start = Date.now();
-  const testFilesToRun = getAllPwFilesInDirectory(targetDir)
+
+  const allTestFiles = DIRECTORIES_WITH_TESTS.reduce((acc, dir) => {
+    return acc.concat(getAllPwFilesInDirectory(dir));
+  }, []);
+
+  const testFilesToRun = allTestFiles
     .slice(0, 10)
-    .map((file) => ({
-      file,
-      deps: getFileDeps(file),
-    }))
+    .map((file) => ({ file, deps: getFileDeps(file) }))
     .filter(({ deps }) => deps.some((dep) => changedFiles.includes(dep)));
-  const testFileNamesToRun = testFilesToRun.map(({ file }) => path.relative(rootDir, file));
+  const testFileNamesToRun = testFilesToRun.map(({ file }) => path.relative(ROOT_DIR, file));
 
   if (!testFileNamesToRun.length) {
+    createTargetFile('');
     console.log('No tests to run. Exiting...');
     return;
   }
@@ -74,7 +131,7 @@ async function run() {
   console.log('Tests to run: ', testFileNamesToRun);
   console.log('Non existent deps: ', NON_EXISTENT_DEPS);
 
-  fs.writeFileSync(targetFile, testFileNamesToRun.join('\n'));
+  createTargetFile(testFileNamesToRun.join('\n'));
 }
 
 run();
