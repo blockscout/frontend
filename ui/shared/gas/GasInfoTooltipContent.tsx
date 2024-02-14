d738004c908aea20daad5a45aa8a6027dcfcc1e3
tom <tom@ohhhh.me>
2024-02-14 18:30:27 +0400

    update tooltip layout

--- ui/shared/gas/GasInfoTooltipContent.tsx
+++ ui/shared/gas/GasInfoTooltipContent.tsx
@@ -1,10 +1,13 @@
-import { DarkMode, Grid, GridItem } from '@chakra-ui/react';
+import { Box, DarkMode, Flex, Grid } from '@chakra-ui/react';
 import React from 'react';
 
 import type { HomeStats } from 'types/api/stats';
 
+import { route } from 'nextjs-routes';
+
 import dayjs from 'lib/date/dayjs';
 
+import LinkInternal from '../LinkInternal';
 import GasInfoTooltipRow from './GasInfoTooltipRow';
 import GasInfoUpdateTimer from './GasInfoUpdateTimer';
 
@@ -21,21 +24,26 @@ const GasInfoTooltipContent = ({ data, dataUpdatedAt }: Props) => {
 
   return (
     <DarkMode>
-      <Grid templateColumns="repeat(2, max-content)" rowGap={ 2 } columnGap={ 4 } padding={ 4 } fontSize="xs" lineHeight={ 4 }>
+      <Flex flexDir="column" padding={ 4 } fontSize="xs" lineHeight={ 4 } rowGap={ 3 }>
         { data.gas_price_updated_at && (
-          <>
-            <GridItem color="text_secondary">Last update</GridItem>
-            <GridItem color="text_secondary" display="flex" justifyContent="flex-end" columnGap={ 2 }>
+          <Flex justifyContent="space-between">
+            <Box color="text_secondary">Last update</Box>
+            <Flex color="text_secondary" justifyContent="flex-end" columnGap={ 2 }>
               { dayjs(data.gas_price_updated_at).format('MMM DD, HH:mm:ss') }
               { data.gas_prices_update_in !== 0 &&
               <GasInfoUpdateTimer key={ dataUpdatedAt } startTime={ dataUpdatedAt } duration={ data.gas_prices_update_in }/> }
-            </GridItem>
-          </>
+            </Flex>
+          </Flex>
         ) }
-        <GasInfoTooltipRow name="Slow" info={ data.gas_prices.slow }/>
-        <GasInfoTooltipRow name="Normal" info={ data.gas_prices.average }/>
-        <GasInfoTooltipRow name="Fast" info={ data.gas_prices.fast }/>
-      </Grid>
+        <Grid rowGap={ 2 } columnGap="10px" gridTemplateColumns="repeat(3, minmax(min-content, auto))">
+          <GasInfoTooltipRow name="Slow" info={ data.gas_prices.slow }/>
+          <GasInfoTooltipRow name="Normal" info={ data.gas_prices.average }/>
+          <GasInfoTooltipRow name="Fast" info={ data.gas_prices.fast }/>
+        </Grid>
+        <LinkInternal href={ route({ pathname: '/gas-tracker' }) }>
+            Gas tracker overview
+        </LinkInternal>
+      </Flex>
     </DarkMode>
   );
 };
--- ui/shared/gas/GasInfoTooltipRow.tsx
+++ ui/shared/gas/GasInfoTooltipRow.tsx
@@ -1,9 +1,9 @@
-import { GridItem, chakra } from '@chakra-ui/react';
+import { Box, chakra } from '@chakra-ui/react';
 import React from 'react';
 
 import type { GasPriceInfo } from 'types/api/stats';
 
-import { asymp, space } from 'lib/html-entities';
+import { space } from 'lib/html-entities';
 import GasPrice from 'ui/shared/gas/GasPrice';
 
 interface Props {
@@ -12,27 +12,18 @@ interface Props {
 }
 
 const GasInfoTooltipRow = ({ name, info }: Props) => {
-  const content = (() => {
-    if (!info || info.price === null) {
-      return 'N/A';
-    }
-
-    return (
-      <>
-        <GasPrice data={ info }/>
-        { info.time && (
-          <chakra.span color="text_secondary">
-            { space }per tx { asymp } { (info.time / 1000).toLocaleString(undefined, { maximumFractionDigits: 1 }) }s
-          </chakra.span>
-        ) }
-      </>
-    );
-  })();
-
   return (
     <>
-      <GridItem color="blue.100">{ name }</GridItem>
-      <GridItem color="text" textAlign="right">{ content }</GridItem>
+      <Box>
+        <chakra.span>{ name }</chakra.span>
+        { info && info.time && (
+          <chakra.span color="text_secondary">
+            { space }{ (info.time / 1000).toLocaleString(undefined, { maximumFractionDigits: 1 }) }s
+          </chakra.span>
+        ) }
+      </Box>
+      <GasPrice data={ info }/>
+      <GasPrice data={ info } unitMode="secondary" color="text_secondary" textAlign="right"/>
     </>
   );
 };
