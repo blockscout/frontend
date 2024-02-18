ab2f498c1f3b8953a5f62ee31203232785255309
isstuev <natix.naf@gmail.com>
2024-02-18 19:55:58 +0100

    better mock

--- mocks/contract/audits.ts
+++ mocks/contract/audits.ts
@@ -2,7 +2,15 @@ import type { SmartContractSecurityAudits } from 'types/api/contract';
 
 export const contractAudits: SmartContractSecurityAudits = {
   items: [
-    { audit_company_name: 'Company-Name 1', audit_publish_date: '2024-01-31', audit_report_url: 'https://ya.ru' },
-    { audit_company_name: 'Company-Name 1', audit_publish_date: '2024-01-31', audit_report_url: 'https://ya.ru' },
+    {
+      audit_company_name: 'OpenZeppelin',
+      audit_publish_date: '2023-03-01',
+      audit_report_url: 'https://blog.openzeppelin.com/eip-4337-ethereum-account-abstraction-incremental-audit',
+    },
+    {
+      audit_company_name: 'OpenZeppelin',
+      audit_publish_date: '2023-03-01',
+      audit_report_url: 'https://blog.openzeppelin.com/eip-4337-ethereum-account-abstraction-incremental-audit',
+    },
   ],
 };
