621c968aea3e4701cf50d0c90e3129779a889f8c
Max Alekseenko <nnwen0609@gmail.com>
2024-02-29 17:27:14 +0100

    improve marketplace tests

--- ui/pages/Marketplace.pw.tsx
+++ ui/pages/Marketplace.pw.tsx
@@ -1,5 +1,4 @@
-import { test as base, expect, devices } from '@playwright/experimental-ct-react';
-import type { Page, Route } from '@playwright/test';
+import { test as base, expect } from '@playwright/experimental-ct-react';
 import React from 'react';
 
 import { buildExternalAssetFilePath } from 'configs/app/utils';
@@ -12,15 +11,21 @@ import Marketplace from './Marketplace';
 
 const MARKETPLACE_CONFIG_URL = app.url + buildExternalAssetFilePath('NEXT_PUBLIC_MARKETPLACE_CONFIG_URL', 'https://marketplace-config.json') || '';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const testFn = async({ mount, page }: { mount: any; page: Page }) => {
-  await page.route(MARKETPLACE_CONFIG_URL, (route: Route) => route.fulfill({
+const test = base.extend({
+  context: contextWithEnvs([
+    { name: 'NEXT_PUBLIC_MARKETPLACE_CONFIG_URL', value: MARKETPLACE_CONFIG_URL },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ]) as any,
+});
+
+test('base view +@mobile +@dark-mode', async({ mount, page }) => {
+  await page.route(MARKETPLACE_CONFIG_URL, (route) => route.fulfill({
     status: 200,
     body: JSON.stringify(appsMock),
   }));
 
   await Promise.all(appsMock.map(app =>
-    page.route(app.logo, (route: Route) =>
+    page.route(app.logo, (route) =>
       route.fulfill({
         status: 200,
         path: './playwright/mocks/image_s.jpg',
@@ -35,28 +40,4 @@ const testFn = async({ mount, page }: { mount: any; page: Page }) => {
   );
 
   await expect(component).toHaveScreenshot();
-};
-
-let test = base.extend({
-  context: contextWithEnvs([
-    { name: 'NEXT_PUBLIC_MARKETPLACE_CONFIG_URL', value: MARKETPLACE_CONFIG_URL },
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ]) as any,
-});
-
-test('base view +@dark-mode', testFn);
-
-test.describe('mobile', () => {
-  test.use({ viewport: devices['iPhone 13 Pro'].viewport });
-
-  test('base view', testFn);
-
-  test = test.extend({
-    context: contextWithEnvs([
-      { name: 'NEXT_PUBLIC_MARKETPLACE_SUGGEST_IDEAS_FORM', value: '' },
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ]) as any,
-  });
-
-  test('without suggest ideas button', testFn);
 });
