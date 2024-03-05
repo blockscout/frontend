548a622cd124ae775c2bc5f0bdebe6452b5e8080
tom <tom@ohhhh.me>
2024-03-05 18:03:54 +0400

    fix tests

--- lib/hexToBytes.ts
+++ lib/hexToBytes.ts
@@ -1,7 +1,8 @@
-// hex is `0x{string}`
+// hex can be with prefix  - `0x{string}` - or without it - `{string}`
 export default function hexToBytes(hex: string) {
   const bytes = [];
-  for (let c = 2; c < hex.length; c += 2) {
+  const startIndex = hex.startsWith('0x') ? 2 : 0;
+  for (let c = startIndex; c < hex.length; c += 2) {
     bytes.push(parseInt(hex.substring(c, c + 2), 16));
   }
   return bytes;
