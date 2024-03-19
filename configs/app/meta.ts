57d3e630175124d5cd3b3e14c9e284591c45f939
FreePoix <17864213831@163.com>
2024-03-20 00:02:41 +0800

    fix: defaultImageUrl

--- configs/app/meta.ts
+++ configs/app/meta.ts
@@ -6,15 +6,15 @@ const defaultImageUrl = app.baseUrl + '/static/og_placeholder.png';
 const protocol = getEnvValue('NEXT_PUBLIC_APP_PROTOCOL');
 const host = getEnvValue('NEXT_PUBLIC_APP_HOST');
 const port = getEnvValue('NEXT_PUBLIC_APP_PORT');
-const path = getExternalAssetFilePath('NEXT_PUBLIC_OG_IMAGE_URL') || defaultImageUrl;
-const ogImageUrl = new URL(path, `${ protocol }://${ host }:${ port }`);
+const path = getExternalAssetFilePath('NEXT_PUBLIC_OG_IMAGE_URL');
+const ogImageUrl = new URL(path || '', `${ protocol }://${ host }:${ port }`);
 
 const meta = Object.freeze({
   promoteBlockscoutInTitle: parseEnvJson<boolean>(getEnvValue('NEXT_PUBLIC_PROMOTE_BLOCKSCOUT_IN_TITLE')) ?? true,
   og: {
     title: getEnvValue('NEXT_PUBLIC_OG_TITLE') || '',
     description: getEnvValue('NEXT_PUBLIC_OG_DESCRIPTION') || '',
-    imageUrl: ogImageUrl.href,
+    imageUrl: path ? ogImageUrl.href : defaultImageUrl,
   },
 });
 
