67e1ab9c619c43035edd0e351c09067e9c7b39e6
tom <tom@ohhhh.me>
2024-03-22 09:23:44 +0300

    more pw test fixes

--- playwright/TestApp.tsx
+++ playwright/TestApp.tsx
@@ -1,16 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { GrowthBookProvider } from '@growthbook/growthbook-react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
 import React from 'react';
-import { http } from 'viem';
 import { WagmiProvider } from 'wagmi';
-import { mainnet } from 'wagmi/chains';
 
 import type { Props as PageProps } from 'nextjs/getServerSideProps';
 
 import { AppContextProvider } from 'lib/contexts/app';
 import { SocketProvider } from 'lib/socket/context';
+import wagmiConfig from 'lib/web3/wagmiConfig';
 import * as app from 'playwright/utils/app';
 import theme from 'theme';
 
@@ -35,24 +33,6 @@ const defaultAppContext = {
   },
 };
 
-// >>> Web3 stuff
-const WALLET_CONNECT_PROJECT_ID = 'PROJECT_ID';
-
-const wagmiConfig = defaultWagmiConfig({
-  chains: [ mainnet ],
-  projectId: WALLET_CONNECT_PROJECT_ID,
-  transports: {
-    [mainnet.id]: http(),
-  },
-  metadata: {
-    name: `Playwright test app explorer`,
-    description: 'Playwright test app explorer',
-    url: app.url,
-    icons: [],
-  },
-});
-// <<<<
-
 const TestApp = ({ children, withSocket, appContext = defaultAppContext }: Props) => {
   const [ queryClient ] = React.useState(() => new QueryClient({
     defaultOptions: {
@@ -69,7 +49,7 @@ const TestApp = ({ children, withSocket, appContext = defaultAppContext }: Props
         <SocketProvider url={ withSocket ? `ws://${ app.domain }:${ app.socketPort }` : undefined }>
           <AppContextProvider { ...appContext }>
             <GrowthBookProvider>
-              <WagmiProvider config={ wagmiConfig }>
+              <WagmiProvider config={ wagmiConfig! }>
                 { children }
               </WagmiProvider>
             </GrowthBookProvider>
