92a9a5e676657dc0717b0b52504fd455ae2e992c
tom <tom@ohhhh.me>
2024-04-04 10:02:54 +0200

    [skip ci] remove 404 from /metrics route

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -7,10 +7,6 @@ const isEnabled = process.env.PROMETHEUS_METRICS_ENABLED === 'true';
 isEnabled && promClient.collectDefaultMetrics();
 
 export default async function metricsHandler(req: NextApiRequest, res: NextApiResponse) {
-  if (!isEnabled) {
-    return res.status(404).send('Not found');
-  }
-
   const metrics = await promClient.register.metrics();
   res.setHeader('Content-type', promClient.register.contentType);
   res.send(metrics);
