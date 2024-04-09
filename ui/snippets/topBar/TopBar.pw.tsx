5e24b828e607a8b1b23e10d3d670e14e2b49c398
tom <tom@ohhhh.me>
2024-04-09 13:26:20 +0200

    tests

--- ui/snippets/topBar/TopBar.pw.tsx
+++ ui/snippets/topBar/TopBar.pw.tsx
@@ -11,7 +11,8 @@ import TopBar from './TopBar';
 const test = base.extend({
   context: contextWithEnvs([
     { name: 'NEXT_PUBLIC_SWAP_BUTTON_URL', value: 'uniswap' },
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    { name: 'NEXT_PUBLIC_NETWORK_SECONDARY_COIN_SYMBOL', value: 'DUCK' },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ]) as any,
 });
 
@@ -32,4 +33,19 @@ test('default view +@dark-mode +@mobile', async({ mount, page }) => {
 
   await component.getByLabel('User settings').click();
   await expect(page).toHaveScreenshot({ clip: { x: 0, y: 0, width: 1500, height: 400 } });
+});
+
+test('with secondary coin price +@mobile', async({ mount, page }) => {
+  await page.route(buildApiUrl('stats'), (route) => route.fulfill({
+    status: 200,
+    body: JSON.stringify(statsMock.withSecondaryCoin),
+  }));
+
+  const component = await mount(
+    <TestApp>
+      <TopBar/>
+    </TestApp>,
+  );
+
+  await expect(component).toHaveScreenshot();
 });
