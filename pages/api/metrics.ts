421db9375a52123f47d990f1cb6fbb61f3753f32
tom <tom@ohhhh.me>
2024-04-15 13:30:24 +0200

    [skip ci] add prefix to default metrics

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -4,7 +4,7 @@ import * as promClient from 'prom-client';
 // eslint-disable-next-line no-restricted-properties
 const isEnabled = process.env.PROMETHEUS_METRICS_ENABLED === 'true';
 
-isEnabled && promClient.collectDefaultMetrics();
+isEnabled && promClient.collectDefaultMetrics({ prefix: 'frontend_' });
 
 export default async function metricsHandler(req: NextApiRequest, res: NextApiResponse) {
   const metrics = await promClient.register.metrics();
