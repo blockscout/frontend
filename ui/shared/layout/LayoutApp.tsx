cb844b8af32c5e10fbde2dd564adad64e7195590
Max Alekseenko <nnwen0609@gmail.com>
2024-04-16 19:57:09 +0200

    fix app layout for mobiles

--- ui/shared/layout/LayoutApp.tsx
+++ ui/shared/layout/LayoutApp.tsx
@@ -1,3 +1,4 @@
+import { Global, css } from '@emotion/react';
 import React from 'react';
 
 import type { Props } from './types';
@@ -9,24 +10,43 @@ import * as Layout from './components';
 
 const LayoutDefault = ({ children }: Props) => {
   return (
-    <Layout.Container overflowY="hidden" height="100vh">
-      <Layout.TopRow/>
-      <HeaderMobile/>
-      <Layout.MainArea>
-        <Layout.MainColumn
-          paddingTop={{ base: 16, lg: 0 }}
-          paddingBottom={ 0 }
-          paddingX={{ base: 4, lg: 6 }}
-          height={{ base: 'calc(100vh - 92px)', sm: 'auto' }} // 92px = Layout.TopRow + HeaderMobile
-        >
-          <AppErrorBoundary>
-            <Layout.Content pt={{ base: 0, lg: 2 }} flexGrow={ 1 }>
-              { children }
-            </Layout.Content>
-          </AppErrorBoundary>
-        </Layout.MainColumn>
-      </Layout.MainArea>
-    </Layout.Container>
+    <>
+      <Global
+        styles={ css`
+          html {
+            height: -webkit-fill-available;
+          }
+          body {
+            height: 100vh;
+            height: -webkit-fill-available;
+            display: flex;
+            flex-direction: column;
+          }
+          #__next {
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+          }
+        ` }
+      />
+      <Layout.Container overflowY="hidden" flex={ 1 } display="flex" flexDir="column">
+        <Layout.TopRow/>
+        <HeaderMobile hideSearchBar/>
+        <Layout.MainArea minH="auto" flex={ 1 }>
+          <Layout.MainColumn
+            paddingTop={{ base: 0, lg: 0 }}
+            paddingBottom={ 0 }
+            paddingX={{ base: 4, lg: 6 }}
+          >
+            <AppErrorBoundary>
+              <Layout.Content pt={{ base: 0, lg: 2 }} flexGrow={ 1 }>
+                { children }
+              </Layout.Content>
+            </AppErrorBoundary>
+          </Layout.MainColumn>
+        </Layout.MainArea>
+      </Layout.Container>
+    </>
   );
 };
 
