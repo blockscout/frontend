e1ffaf011a3642cb4815fdf0690b611e4884fdf1
n06ody <4312182lyp@gmail.com>
2024-04-16 10:46:14 +0800

    Fix api build url bug

--- lib/api/buildUrl.ts
+++ lib/api/buildUrl.ts
@@ -6,6 +6,13 @@ import isNeedProxy from './isNeedProxy';
 import { RESOURCES } from './resources';
 import type { ApiResource, ResourceName, ResourcePathParams } from './resources';
 
+function trimPrefix(str: string, prefix: string): string {
+  if (str.startsWith(prefix)) {
+    return str.slice(prefix.length);
+  }
+  return str;
+}
+
 export default function buildUrl<R extends ResourceName>(
   resourceName: R,
   pathParams?: ResourcePathParams<R>,
@@ -15,7 +22,7 @@ export default function buildUrl<R extends ResourceName>(
   const baseUrl = isNeedProxy() ? config.app.baseUrl : (resource.endpoint || config.api.endpoint);
   const basePath = resource.basePath !== undefined ? resource.basePath : config.api.basePath;
   const path = isNeedProxy() ? '/node-api/proxy' + basePath + resource.path : basePath + resource.path;
-  const url = new URL(compile(path)(pathParams), baseUrl);
+  const url = new URL(trimPrefix(compile(path)(pathParams), '/'), baseUrl);
 
   queryParams && Object.entries(queryParams).forEach(([ key, value ]) => {
     // there are some pagination params that can be null or false for the next page
