eadc19a52e689eddeeb202a7fc55e65e722f3e61
Suveer Grover <119030604+GoGeek91@users.noreply.github.com>
2024-04-24 12:05:47 +0530

    fix:logo size

--- ui/snippets/networkMenu/NetworkLogo.tsx
+++ ui/snippets/networkMenu/NetworkLogo.tsx
@@ -1,11 +1,11 @@
-import type { StyleProps } from "@chakra-ui/react";
-import { Box, Image, useColorModeValue, Skeleton } from "@chakra-ui/react";
-import React from "react";
+import type { StyleProps } from '@chakra-ui/react';
+import { Box, Image, useColorModeValue, Skeleton } from '@chakra-ui/react';
+import React from 'react';
 
-import { route } from "nextjs-routes";
+import { route } from 'nextjs-routes';
 
-import config from "configs/app";
-import IconSvg from "ui/shared/IconSvg";
+import config from 'configs/app';
+import IconSvg from 'ui/shared/IconSvg';
 
 interface Props {
   isCollapsed?: boolean;
@@ -22,33 +22,33 @@ const LogoFallback = ({
   isSmall?: boolean;
   imageProps?: StyleProps;
 }) => {
-  const field = isSmall ? "icon" : "logo";
-  const logoColor = useColorModeValue("blue.600", "white");
+  const field = isSmall ? 'icon' : 'logo';
+  const logoColor = useColorModeValue('blue.600', 'white');
 
-  const display = isSmall
-    ? {
-        base: "none",
-        lg: isCollapsed === false ? "none" : "block",
-        xl: isCollapsed ? "block" : "none",
-      }
-    : {
-        base: "block",
-        lg: isCollapsed === false ? "block" : "none",
-        xl: isCollapsed ? "none" : "block",
-      };
+  const display = isSmall ?
+    {
+      base: 'none',
+      lg: isCollapsed === false ? 'none' : 'block',
+      xl: isCollapsed ? 'block' : 'none',
+    } :
+    {
+      base: 'block',
+      lg: isCollapsed === false ? 'block' : 'none',
+      xl: isCollapsed ? 'none' : 'block',
+    };
 
   if (config.UI.sidebar[field].default) {
-    return <Skeleton w="100%" borderRadius="sm" display={display} />;
+    return <Skeleton w="100%" borderRadius="sm" display={ display }/>;
   }
 
   return (
     <IconSvg
-      name={isSmall ? "networks/icon-placeholder" : "networks/logo-placeholder"}
+      name={ isSmall ? 'networks/icon-placeholder' : 'networks/logo-placeholder' }
       width="auto"
       height="100%"
-      color={logoColor}
-      display={display}
-      {...imageProps}
+      color={ logoColor }
+      display={ display }
+      { ...imageProps }
     />
   );
 };
@@ -56,65 +56,65 @@ const LogoFallback = ({
 const NetworkLogo = ({ isCollapsed, onClick, imageProps }: Props) => {
   const logoSrc = useColorModeValue(
     config.UI.sidebar.logo.default,
-    config.UI.sidebar.logo.dark || config.UI.sidebar.logo.default
+    config.UI.sidebar.logo.dark || config.UI.sidebar.logo.default,
   );
   const iconSrc = useColorModeValue(
     config.UI.sidebar.icon.default,
-    config.UI.sidebar.icon.dark || config.UI.sidebar.icon.default
+    config.UI.sidebar.icon.dark || config.UI.sidebar.icon.default,
   );
-  const darkModeFilter = { filter: "brightness(0) invert(1)" };
+  const darkModeFilter = { filter: 'brightness(0) invert(1)' };
   const logoStyle = useColorModeValue({}, !config.UI.sidebar.logo.dark ? darkModeFilter : {});
   const iconStyle = useColorModeValue({}, !config.UI.sidebar.icon.dark ? darkModeFilter : {});
 
   return (
     <Box
       as="a"
-      href={route({ pathname: "/" })}
+      href={ route({ pathname: '/' }) }
       width={{
-        base: "120px",
-        lg: isCollapsed === false ? "120px" : "30px",
-        xl: isCollapsed ? "30px" : "120px",
+        base: '140px',
+        lg: isCollapsed === false ? '140px' : '35px',
+        xl: isCollapsed ? '35px' : '140px',
       }}
       height={{
-        base: "24px",
-        lg: isCollapsed === false ? "24px" : "30px",
-        xl: isCollapsed ? "30px" : "24px",
+        base: '35px',
+        lg: isCollapsed === false ? '35px' : '30px',
+        xl: isCollapsed ? '30px' : '35px',
       }}
       display="inline-flex"
       overflow="hidden"
-      onClick={onClick}
-      flexShrink={0}
+      onClick={ onClick }
+      flexShrink={ 0 }
       aria-label="Link to main page"
     >
-      {/* big logo */}
+      { /* big logo */ }
       <Image
         w="auto"
         h="100%"
-        src={logoSrc}
-        alt={`${config.chain.name} network logo`}
-        fallback={<LogoFallback isCollapsed={isCollapsed} imageProps={imageProps} />}
+        src={ logoSrc }
+        alt={ `${ config.chain.name } network logo` }
+        fallback={ <LogoFallback isCollapsed={ isCollapsed } imageProps={ imageProps }/> }
         display={{
-          base: "block",
-          lg: isCollapsed === false ? "block" : "none",
-          xl: isCollapsed ? "none" : "block",
+          base: 'block',
+          lg: isCollapsed === false ? 'block' : 'none',
+          xl: isCollapsed ? 'none' : 'block',
         }}
-        style={logoStyle}
-        {...imageProps}
+        style={ logoStyle }
+        { ...imageProps }
       />
-      {/* small logo */}
+      { /* small logo */ }
       <Image
         w="auto"
         h="100%"
-        src={iconSrc}
-        alt={`${config.chain.name} network logo`}
-        fallback={<LogoFallback isCollapsed={isCollapsed} imageProps={imageProps} isSmall />}
+        src={ iconSrc }
+        alt={ `${ config.chain.name } network logo` }
+        fallback={ <LogoFallback isCollapsed={ isCollapsed } imageProps={ imageProps } isSmall/> }
         display={{
-          base: "none",
-          lg: isCollapsed === false ? "none" : "block",
-          xl: isCollapsed ? "block" : "none",
+          base: 'none',
+          lg: isCollapsed === false ? 'none' : 'block',
+          xl: isCollapsed ? 'block' : 'none',
         }}
-        style={iconStyle}
-        {...imageProps}
+        style={ iconStyle }
+        { ...imageProps }
       />
     </Box>
   );
