0b60ada3742bf0d553589ad6de75ab90e46c141a
tom <tom@ohhhh.me>
2024-05-15 09:28:20 +0200

    add CSP for metadata service

--- nextjs/csp/policies/app.ts
+++ nextjs/csp/policies/app.ts
@@ -56,6 +56,7 @@ export function app(): CspDev.DirectiveDescriptor {
       getFeaturePayload(config.features.verifiedTokens)?.api.endpoint,
       getFeaturePayload(config.features.addressVerification)?.api.endpoint,
       getFeaturePayload(config.features.nameService)?.api.endpoint,
+      getFeaturePayload(config.features.addressMetadata)?.api.endpoint,
       marketplaceFeaturePayload && 'api' in marketplaceFeaturePayload ? marketplaceFeaturePayload.api.endpoint : '',
 
       // chain RPC server
