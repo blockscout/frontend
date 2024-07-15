5e9c6a156be736824419a8dd20e3316472b01786
Max Alekseenko <nnwen0609@gmail.com>
2024-07-15 13:35:36 +0200

    return back wallet validation

--- ui/marketplace/Rating/useRatings.tsx
+++ ui/marketplace/Rating/useRatings.tsx
@@ -84,8 +84,8 @@ export default function useRatings() {
   }, [ address ]);
 
   useEffect(() => {
-    const { isPlaceholderData/**, data**/ } = addressCountersQuery;
-    const canRate = address && !isPlaceholderData /**&& Number(data?.transactions_count) >= 10**/;
+    const { isPlaceholderData, data } = addressCountersQuery;
+    const canRate = address && !isPlaceholderData && Number(data?.transactions_count) >= 10;
     setCanRate(canRate);
   }, [ address, addressCountersQuery ]);
 
