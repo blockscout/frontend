be17dbf0c88cd3970e630bff58099cc523d170ad
Kenjii <kenjii@0xmakase.co.jp>
2024-07-19 14:18:53 +0900

    Playwrightのテストで失敗するので一旦英語に戻す

--- i18n.ts
+++ i18n.ts
@@ -7,7 +7,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'ja',
+    fallbackLng: 'en',
     debug: false,
     interpolation: {
       escapeValue: false,
