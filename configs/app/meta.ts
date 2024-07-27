b9984aa009d04e72b71af6d6c1919b0ea810b0b8
Cheng Wei <chgweiz@gmail.com>
2024-07-27 23:22:07 +0800

    Fix: using `===` to check `NEXT_PUBLIC_PROMOTE_BLOCKSCOUT_IN_TITLE`

--- configs/app/meta.ts
+++ configs/app/meta.ts
@@ -4,7 +4,7 @@ import { getEnvValue, getExternalAssetFilePath } from './utils';
 const defaultImageUrl = '/static/og_placeholder.png';
 
 const meta = Object.freeze({
-  promoteBlockscoutInTitle: getEnvValue('NEXT_PUBLIC_PROMOTE_BLOCKSCOUT_IN_TITLE') || 'true',
+  promoteBlockscoutInTitle: getEnvValue('NEXT_PUBLIC_PROMOTE_BLOCKSCOUT_IN_TITLE') === 'true',
   og: {
     description: getEnvValue('NEXT_PUBLIC_OG_DESCRIPTION') || '',
     imageUrl: app.baseUrl + (getExternalAssetFilePath('NEXT_PUBLIC_OG_IMAGE_URL') || defaultImageUrl),
