2a0bc74fdc61bed58c808b7aec428dadf7897185
tom <tom@ohhhh.me>
2024-07-30 08:44:19 +0200

    fix tests

--- ui/address/contract/ContractCode.pw.tsx
+++ ui/address/contract/ContractCode.pw.tsx
@@ -21,7 +21,10 @@ test.describe.configure({ mode: 'serial' });
 
 let addressApiUrl: string;
 
-test.beforeEach(async({ mockApiResponse }) => {
+test.beforeEach(async({ mockApiResponse, page }) => {
+  await page.route('https://cdn.jsdelivr.net/npm/monaco-editor@0.33.0/**', (route) => {
+    route.abort();
+  });
   addressApiUrl = await mockApiResponse('address', addressMock.contract, { pathParams: { hash: addressMock.contract.hash } });
 });
 
