7d70f119c2ccc35c3c222ac562cbdbfe8d39bfa9
isstuev <isstuev@gmail.com>
2024-08-01 16:10:51 +0200

    arbitrum latest batches and deposits

--- ui/pages/Home.tsx
+++ ui/pages/Home.tsx
@@ -4,8 +4,9 @@ import React from 'react';
 import config from 'configs/app';
 import useIsMobile from 'lib/hooks/useIsMobile';
 import ChainIndicators from 'ui/home/indicators/ChainIndicators';
+import LatestArbitrumL2Batches from 'ui/home/latestBatches/LatestArbitrumL2Batches';
+import LatestZkEvmL2Batches from 'ui/home/latestBatches/LatestZkEvmL2Batches';
 import LatestBlocks from 'ui/home/LatestBlocks';
-import LatestZkEvmL2Batches from 'ui/home/LatestZkEvmL2Batches';
 import Stats from 'ui/home/Stats';
 import Transactions from 'ui/home/Transactions';
 import AdBanner from 'ui/shared/ad/AdBanner';
@@ -61,7 +62,9 @@ const Home = () => {
       </Flex>
       { isMobile && <AdBanner mt={ 6 } mx="auto" display="flex" justifyContent="center"/> }
       <Flex mt={ 8 } direction={{ base: 'column', lg: 'row' }} columnGap={ 12 } rowGap={ 6 }>
-        { rollupFeature.isEnabled && rollupFeature.type === 'zkEvm' ? <LatestZkEvmL2Batches/> : <LatestBlocks/> }
+        { rollupFeature.isEnabled && rollupFeature.type === 'zkEvm' && <LatestZkEvmL2Batches/> }
+        { rollupFeature.isEnabled && rollupFeature.type === 'arbitrum' && <LatestArbitrumL2Batches/> }
+        { !(rollupFeature.isEnabled && (rollupFeature.type === 'arbitrum' || rollupFeature.type === 'zkEvm')) && <LatestBlocks/> }
         <Box flexGrow={ 1 }>
           <Transactions/>
         </Box>
