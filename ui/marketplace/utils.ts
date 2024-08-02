9da3114b2e2fdf3bd68062d1ec26f28b42d8f75f
Max Alekseenko <nnwen0609@gmail.com>
2024-08-02 18:34:55 +0200

    fix marketplace sorting display

--- ui/marketplace/utils.ts
+++ ui/marketplace/utils.ts
@@ -1,16 +1,19 @@
 import type { NextRouter } from 'next/router';
 
+import config from 'configs/app';
 import getQueryParamString from 'lib/router/getQueryParamString';
 import removeQueryParam from 'lib/router/removeQueryParam';
 import type { TOption } from 'ui/shared/sort/Option';
+
+const feature = config.features.marketplace;
 
 export type SortValue = 'rating' | 'security_score';
 
 export const SORT_OPTIONS: Array<TOption<SortValue>> = [
   { title: 'Default', id: undefined },
-  { title: 'Rating', id: 'rating' },
-  { title: 'Security score', id: 'security_score' },
-];
+  (feature.isEnabled && feature.rating) && { title: 'Rating', id: 'rating' },
+  (feature.isEnabled && feature.securityReportsUrl) && { title: 'Security score', id: 'security_score' },
+].filter(Boolean) as Array<TOption<SortValue>>;
 
 export function getAppUrl(url: string | undefined, router: NextRouter) {
   if (!url) {
