feda5a6b12add1b8aab58ee6070ff5b8861a90c8
zkMe-jolan <jolan@zkme.xyz>
2024-08-16 11:08:08 +0800

    feat: add session ttl

--- lib/session/config.ts
+++ lib/session/config.ts
@@ -1,10 +1,15 @@
+import type { SessionOptions } from 'iron-session';
+
 import { getEnvValue } from 'configs/app/utils';
 
-export const sessionOptions = {
-  cookieName: 'mechian-session-token',
+const ttl = getEnvValue('SESSION_TTL');
+
+export const sessionOptions: SessionOptions = {
+  cookieName: `${ getEnvValue('NEXT_PUBLIC_NETWORK_SHORT_NAME') }-session-token`,
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   password: getEnvValue('SESSION_PASSWORD')!,
   cookieOptions: {
     secure: getEnvValue('NEXT_PUBLIC_APP_ENV') === 'production',
   },
+  ttl: ttl ? Number(ttl) : undefined,
 };
