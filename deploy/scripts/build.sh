982adb8c5c79c8ec248af56675cd90f6013a7c21
omnus <omnusunmo@gmail.com>
2024-09-10 11:30:18 +1200

    Update build.sh

--- deploy/scripts/build.sh
+++ deploy/scripts/build.sh
@@ -4,5 +4,6 @@ yarn svg:build-sprite
 echo ""
 
 # generate envs.js file
--- bash -c './make_envs_script.sh && next dev -p $NEXT_PUBLIC_APP_PORT' |
+dotenv \
+  -- bash -c './deploy/scripts/make_envs_script.sh && next dev -p $NEXT_PUBLIC_APP_PORT' |
 pino-pretty
