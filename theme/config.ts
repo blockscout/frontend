55a620f43571e0a1a349451b63e44d4cc2a34947
zkMe-mike <mike@zkme.xyz>
2024-09-09 14:52:04 +0800

    force light mode as default

--- theme/config.ts
+++ theme/config.ts
@@ -3,7 +3,8 @@ import { type ThemeConfig } from '@chakra-ui/react';
 import appConfig from 'configs/app';
 
 const config: ThemeConfig = {
-  initialColorMode: appConfig.UI.colorTheme.default?.colorMode ?? 'system',
+  // initialColorMode: appConfig.UI.colorTheme.default?.colorMode ?? 'system',
+  initialColorMode: appConfig.UI.colorTheme.default?.colorMode ?? 'light', // force light mode as default
   useSystemColorMode: false,
   disableTransitionOnChange: false,
 };
