1c2218f49bd5d830b0fee8d61b0114083937d330
omnus <omnusunmo@gmail.com>
2024-09-11 09:13:28 +1200

    Update app.ts

--- nextjs/csp/policies/app.ts
+++ nextjs/csp/policies/app.ts
@@ -63,6 +63,7 @@ export function app(): CspDev.DirectiveDescriptor {
       'https://blessnet.calderaexplorer.xyz', // Caldera
       'wss://relay.walletconnect.org', // Walletconnect
       'https://relay.walletconnect.org', // Walletconnect
+      'https://vercel.live', // Vercel
 
       // github (spec for api-docs page)
       'raw.githubusercontent.com',
