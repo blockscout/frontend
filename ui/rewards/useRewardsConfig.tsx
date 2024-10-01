0657ea5060393ad6407d80448200eb724162c18e
Max Alekseenko <nnwen0609@gmail.com>
2024-10-01 18:01:59 +0200

    rename func

--- ui/rewards/useRewardsConfig.tsx
+++ ui/rewards/useRewardsConfig.tsx
@@ -1,7 +1,7 @@
 import config from 'configs/app';
 import useApiQuery from 'lib/api/useApiQuery';
 
-export default function useReferrals() {
+export default function useRewardsConfig() {
   return useApiQuery('rewards_config', {
     queryOptions: {
       enabled: config.features.rewards.isEnabled,
