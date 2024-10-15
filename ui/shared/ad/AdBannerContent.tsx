5b3a7a00e2b3269c3cb11953afb1a6392b5baf39
ishihssihshihishsi <ishkhanbalgudanian@gmail.com>
2024-10-15 16:56:27 +0300

    fixed banner removal code

--- ui/shared/ad/AdBannerContent.tsx
+++ ui/shared/ad/AdBannerContent.tsx
@@ -1,18 +1,19 @@
-import { chakra, Skeleton } from '@chakra-ui/react';
-import React from 'react';
+import { chakra } from '@chakra-ui/react';
+// import { chakra, Skeleton } from '@chakra-ui/react';
+// import React from 'react';
 
 import type { BannerPlatform } from './types';
 import type { AdBannerProviders } from 'types/client/adProviders';
 
-import config from 'configs/app';
+// import config from 'configs/app';
 
-import AdbutlerBanner from './AdbutlerBanner';
-import CoinzillaBanner from './CoinzillaBanner';
+// import AdbutlerBanner from './AdbutlerBanner';
+// import CoinzillaBanner from './CoinzillaBanner';
 // import GetitBanner from './GetitBanner';
-import HypeBanner from './HypeBanner';
-import SliseBanner from './SliseBanner';
+// import HypeBanner from './HypeBanner';
+// import SliseBanner from './SliseBanner';
 
-const feature = config.features.adsBanner;
+// const feature = config.features.adsBanner;
 
 interface Props {
   className?: string;
@@ -21,34 +22,35 @@ interface Props {
   provider: AdBannerProviders;
 }
 
-const AdBannerContent = ({ className, isLoading, provider, platform }: Props) => {
+// const AdBannerContent = ({ className, isLoading, provider, platform }: Props) => {
+const AdBannerContent = ({ }: Props) => {
   return null;
-  const content = (() => {
-    switch (provider) {
-      case 'adbutler':
-        return <AdbutlerBanner platform={ platform }/>;
-      case 'coinzilla':
-        return <CoinzillaBanner platform={ platform }/>;
-      // case 'getit':
-      //   return <GetitBanner platform={ platform }/>;
-      case 'hype':
-        return <HypeBanner platform={ platform }/>;
-      case 'slise':
-        return <SliseBanner platform={ platform }/>;
-    }
-  })();
+  // const content = (() => {
+  //   switch (provider) {
+  //     case 'adbutler':
+  //       return <AdbutlerBanner platform={ platform }/>;
+  //     case 'coinzilla':
+  //       return <CoinzillaBanner platform={ platform }/>;
+  //     // case 'getit':
+  //     //   return <GetitBanner platform={ platform }/>;
+  //     case 'hype':
+  //       return <HypeBanner platform={ platform }/>;
+  //     case 'slise':
+  //       return <SliseBanner platform={ platform }/>;
+  //   }
+  // })();
 
-  return (
-    <Skeleton
-      className={ className }
-      isLoaded={ !isLoading }
-      borderRadius="none"
-      maxW={ ('adButler' in feature && feature.adButler) ? feature.adButler.config.desktop.width : '728px' }
-      w="100%"
-    >
-      { content }
-    </Skeleton>
-  );
+  // return (
+  //   <Skeleton
+  //     className={ className }
+  //     isLoaded={ !isLoading }
+  //     borderRadius="none"
+  //     maxW={ ('adButler' in feature && feature.adButler) ? feature.adButler.config.desktop.width : '728px' }
+  //     w="100%"
+  //   >
+  //     { content }
+  //   </Skeleton>
+  // );
 };
 
 export default chakra(AdBannerContent);
