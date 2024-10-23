b42d960a54836bcb21dd19140d39841c0bc35dbf
Max Alekseenko <nnwen0609@gmail.com>
2024-10-23 12:27:54 +0200

    change text

--- lib/contexts/rewards.tsx
+++ lib/contexts/rewards.tsx
@@ -57,8 +57,8 @@ const RewardsContext = createContext<TRewardsContext>({
 
 // Message to sign for the rewards program
 function getMessageToSign(address: string, nonce: string, isLogin?: boolean, refCode?: string) {
-  const signInText = 'Sign-In for the Blockscout points program.';
-  const signUpText = 'Sign-Up for the Blockscout points program. I accept Terms of Service: https://merits.blockscout.com/tos. I love capybaras.';
+  const signInText = 'Sign-In for the Blockscout Merits program.';
+  const signUpText = 'Sign-Up for the Blockscout Merits program. I accept Terms of Service: https://merits.blockscout.com/tos. I love capybaras.';
   const referralText = refCode ? ` Referral code: ${ refCode }` : '';
   const body = isLogin ? signInText : signUpText + referralText;
   return [
