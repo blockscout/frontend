3ed36799cab3bb4cab56f36ecd284490a9ea5192
tom <tom@ohhhh.me>
2024-11-01 15:45:13 +0100

    add redirect for bech32 addresses

--- lib/address/bech32.ts
+++ lib/address/bech32.ts
@@ -1,11 +1,41 @@
-import { toBech32Address as toBech32AddressCrypto } from '@zilliqa-js/crypto';
+import { bech32 } from '@scure/base';
 
 import config from 'configs/app';
 
+export const DATA_PART_REGEXP = /^[\da-z]{38}$/;
+
 export function toBech32Address(hash: string) {
-  if (!config.UI.views.address.hashFormat.bech32Prefix) {
-    return hash;
+  if (config.UI.views.address.hashFormat.bech32Prefix) {
+    try {
+      const words = bech32.toWords(Buffer.from(hash.replace('0x', ''), 'hex'));
+      return bech32.encode(config.UI.views.address.hashFormat.bech32Prefix, words);
+    } catch (error) {}
   }
 
-  return toBech32AddressCrypto(hash).replace('zil', config.UI.views.address.hashFormat.bech32Prefix);
+  return hash;
+}
+
+export function isBech32Address(hash: string) {
+  if (!config.UI.views.address.hashFormat.bech32Prefix) {
+    return false;
+  }
+
+  if (!hash.startsWith(`${ config.UI.views.address.hashFormat.bech32Prefix }1`)) {
+    return false;
+  }
+
+  const strippedHash = hash.replace(`${ config.UI.views.address.hashFormat.bech32Prefix }1`, '');
+  return DATA_PART_REGEXP.test(strippedHash);
+}
+
+export function fromBech32Address(hash: string) {
+  if (config.UI.views.address.hashFormat.bech32Prefix) {
+    try {
+      const { words } = bech32.decode(hash as `${ string }1${ string }`);
+      const hex = bech32.fromWords(words);
+      return `0x${ Buffer.from(hex).toString('hex') }`;
+    } catch (error) {}
+  }
+
+  return hash;
 }
