0a9b9c62b88a104a2151eb586327f733113da5b0
tom <tom@ohhhh.me>
2024-11-15 19:02:59 +0100

    [skip ci] adjust instance name

--- configs/app/features/rollbar.ts
+++ configs/app/features/rollbar.ts
@@ -10,7 +10,7 @@ const instance = (() => {
     return envValue;
   }
 
-  return app.host?.replace('.blockscout.com', '').replaceAll('-', '_');
+  return app.host?.replace('.blockscout.com', '').replace('.k8s-dev', '').replaceAll('-', '_');
 })();
 const environment = getEnvValue('NEXT_PUBLIC_APP_ENV') || 'development';
 const codeVersion = getEnvValue('NEXT_PUBLIC_GIT_TAG') || getEnvValue('NEXT_PUBLIC_GIT_COMMIT_SHA');
