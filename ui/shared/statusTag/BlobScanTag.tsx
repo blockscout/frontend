2f05dc662a5945dd76aba1744e6b2a4640f6f274
Joel <itsjoelpm@gmail.com>
2024-11-28 12:35:33 +0300

    chore: blobscan tag

---
+++ ui/shared/statusTag/BlobScanTag.tsx
@@ -0,0 +1,23 @@
+import { TagLabel, Tooltip } from '@chakra-ui/react';
+import React from 'react';
+
+import Tag from 'ui/shared/chakra/Tag';
+import IconSvg from 'ui/shared/IconSvg';
+
+const BlobScanTag = () => {
+  return (
+    <Tooltip>
+      <Tag
+        color="#fff"
+        backgroundColor="#52224D"
+        display="flex"
+        padding="2px 5px"
+      >
+        <IconSvg name="gear_slim" boxSize={ 2.5 } mr={ 1 } flexShrink={ 0 }/>
+        <TagLabel display="block">blobscan-archiver</TagLabel>
+      </Tag>
+    </Tooltip>
+  );
+};
+
+export default BlobScanTag;
