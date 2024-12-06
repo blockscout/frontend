dcfd87a13bbed806fbc994379d5e6f33f21cbd9e
tom <tom@ohhhh.me>
2024-12-06 15:31:43 +0100

    migrate from native select to the custom one

---
+++ ui/shared/select/Select.tsx
@@ -0,0 +1,56 @@
+import { PopoverTrigger, chakra, useDisclosure, useRadioGroup } from '@chakra-ui/react';
+import React from 'react';
+
+import type { SelectOption } from './types';
+
+import Popover from 'ui/shared/chakra/Popover';
+
+import SelectButton from './SelectButton';
+import SelectContent from './SelectContent';
+
+interface Props<Value extends string> {
+  className?: string;
+  isLoading?: boolean;
+  options: Array<SelectOption<Value>>;
+  name: string;
+  defaultValue?: Value;
+  onChange: (value: Value) => void;
+}
+
+const Select = <Value extends string>({ className, isLoading, options, name, defaultValue, onChange }: Props<Value>) => {
+  const { isOpen, onToggle, onClose } = useDisclosure();
+
+  const handleChange = React.useCallback((value: Value) => {
+    onChange(value);
+    onClose();
+  }, [ onChange, onClose ]);
+
+  const { value, getRootProps, getRadioProps, setValue } = useRadioGroup({
+    name,
+    defaultValue,
+    onChange: handleChange,
+  });
+
+  React.useEffect(() => {
+    if (defaultValue) {
+      setValue(defaultValue);
+    }
+  }, [ defaultValue, setValue ]);
+
+  return (
+    <Popover isOpen={ isOpen } onClose={ onClose } placement="bottom-start" isLazy>
+      <PopoverTrigger>
+        <SelectButton
+          className={ className }
+          onClick={ onToggle }
+          isOpen={ isOpen }
+          isLoading={ isLoading }
+          label={ options.find((option) => option.value === value)?.label || String(value) }
+        />
+      </PopoverTrigger>
+      <SelectContent options={ options } getRootProps={ getRootProps } getRadioProps={ getRadioProps } value={ value }/>
+    </Popover>
+  );
+};
+
+export default React.memo(chakra(Select));
