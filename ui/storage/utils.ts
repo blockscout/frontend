ef5f8075b9dcec703f6a7d4c2f9ea3f2ea8b66f6
zkMe-jack <jack@zkme.xyz>
2024-12-10 17:48:26 +0800

    fix: timeTool method

--- ui/storage/utils.ts
+++ ui/storage/utils.ts
@@ -25,8 +25,8 @@ export const timeTool = (time: string) => {
   const seconds = Math.floor(diff / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
-  const days = Math.floor(hours / 60);
-  const months = Math.floor(days / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
   const years = Math.floor(months / 12);
 
   if (years > 0) {
