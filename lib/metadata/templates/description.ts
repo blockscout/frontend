5667735b112a4ec614061d6bcd75c378ae866069
isstuev <isstuev@gmail.com>
2024-12-13 14:34:35 +0100

    change default description

--- lib/metadata/templates/description.ts
+++ lib/metadata/templates/description.ts
@@ -2,7 +2,7 @@
 import type { Route } from 'nextjs-routes';
 
 // equal og:description
-const DEFAULT_TEMPLATE = 'Blockscout is the #1 open-source blockchain explorer available today. 100+ chains and counting rely on Blockscout data availability, APIs, and ecosystem tools to support their networks.';
+const DEFAULT_TEMPLATE = 'Open-source block explorer by Blockscout. Search transactions, verify smart contracts, analyze addresses, and track network activity. Complete blockchain data and APIs for the %network_title% network.';
 
 // FIXME all page descriptions will be updated later
 const TEMPLATE_MAP: Record<Route['pathname'], string> = {
