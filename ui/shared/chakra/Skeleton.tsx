d9b83cf58d25e326337e6a18463e76ec0dfc0613
isstuev <isstuev@gmail.com>
2025-01-06 10:24:13 +0100

    eslint add Skeleton

--- ui/shared/chakra/Skeleton.tsx
+++ ui/shared/chakra/Skeleton.tsx
@@ -1,4 +1,5 @@
 import type { SkeletonProps } from '@chakra-ui/react';
+// eslint-disable-next-line no-restricted-imports
 import { Skeleton as ChakraSkeleton } from '@chakra-ui/react';
 import { forwardRef } from 'react';
 
