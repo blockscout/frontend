9866ab5cbc3f3b05a98ea8cdf2f2e6bdb09b15d2
Memo Khoury <majd.khoury93@gmail.com>
2025-01-30 13:59:02 +0000

    fix build

--- ui/pool/PoolInfo.tsx
+++ ui/pool/PoolInfo.tsx
@@ -5,7 +5,6 @@ import type { Pool } from 'types/api/pools';
 
 import Skeleton from 'ui/shared/chakra/Skeleton';
 import * as DetailsInfoItem from 'ui/shared/DetailsInfoItem';
-import DetailsSponsoredItem from 'ui/shared/DetailsSponsoredItem';
 import TokenEntity from 'ui/shared/entities/token/TokenEntity';
 
 type Props = {
@@ -106,8 +105,6 @@ const PoolInfo = ({ data, isPlaceholderData }: Props) => {
           { data.dex.name }
         </Skeleton>
       </DetailsInfoItem.Value>
-
-      <DetailsSponsoredItem isLoading={ isPlaceholderData }/>
     </Grid>
   );
 };
