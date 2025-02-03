dc91b5e00479a4f15203d5b27cd38e3d85e9b1e5
Eugene Allenov <e.allenov@gmail.com>
2025-02-03 20:54:35 +0300

    add new design

--- ui/snippets/topBar/settings/Settings.tsx
+++ ui/snippets/topBar/settings/Settings.tsx
@@ -5,7 +5,7 @@ import Popover from 'ui/shared/chakra/Popover';
 import IconSvg from 'ui/shared/IconSvg';
 
 import SettingsAddressFormat from './SettingsAddressFormat';
-import SettingsColorTheme from './SettingsColorTheme';
+// import SettingsColorTheme from './SettingsColorTheme';
 import SettingsIdentIcon from './SettingsIdentIcon';
 
 const Settings = () => {
@@ -26,7 +26,6 @@ const Settings = () => {
       </PopoverTrigger>
       <PopoverContent overflowY="hidden" w="auto" fontSize="sm">
         <PopoverBody boxShadow="2xl" p={ 4 }>
-          <SettingsColorTheme onSelect={ onClose }/>
           <Box borderColor="divider" borderWidth="1px" my={ 3 }/>
           <SettingsIdentIcon/>
           <SettingsAddressFormat/>
