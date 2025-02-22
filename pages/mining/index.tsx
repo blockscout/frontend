5d3c470285ec920ccae67182c12a14d1b38f51bb
qingye <2863960964@qq.com>
2025-02-22 13:21:11 +0800

    test:mining page 404 bug

--- pages/mining/index.tsx
+++ pages/mining/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import React from 'react';
 import dynamic from 'next/dynamic';
 import PageNextJs from 'nextjs/PageNextJs';
-const MarketplaceInMining = dynamic(() => import('ui/pages/MarketplaceInMining'));
+const MarketplaceInMining = dynamic(() => import('ui/pages/MarketplaceInMining'), { ssr: false });
 
 const Page: NextPage = () => (
   <PageNextJs pathname="/mining">
