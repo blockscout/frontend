c3ff0d15397790512b043f88bc2aa1057e9616fe
isstuev <isstuev@gmail.com>
2025-02-26 16:10:56 +0100

    fix incorrect color mode

--- lib/contexts/chakra.tsx
+++ lib/contexts/chakra.tsx
@@ -6,6 +6,7 @@ import {
 import type { ChakraProviderProps } from '@chakra-ui/react';
 import React from 'react';
 
+import { get, NAMES } from 'lib/cookies';
 import theme from 'theme/theme';
 
 interface Props extends ChakraProviderProps {
@@ -13,10 +14,13 @@ interface Props extends ChakraProviderProps {
 }
 
 export function ChakraProvider({ cookies, children }: Props) {
+  // When a cookie header is present, cookieStorageManagerSSR looks for a "chakra-ui-color-mode" cookie.
+  // If it doesn’t find one, it doesn’t consider theme’s initialColorMode. Instead, it is defaulting to light mode
+  // So we need to use localStorageManager instead of cookieStorageManagerSSR to get the correct default color mode
   const colorModeManager =
-      typeof cookies === 'string' ?
-        cookieStorageManagerSSR(typeof document !== 'undefined' ? document.cookie : cookies) :
-        localStorageManager;
+    typeof cookies === 'string' && get(NAMES.COLOR_MODE, cookies) ?
+      cookieStorageManagerSSR(typeof document !== 'undefined' ? document.cookie : cookies) :
+      localStorageManager;
 
   return (
     <ChakraProviderDefault colorModeManager={ colorModeManager } theme={ theme }>
