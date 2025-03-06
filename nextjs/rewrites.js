273977b59da9a3c111a78969b36d6d6bd38f5b78
qingye <2863960964@qq.com>
2025-03-06 09:50:38 +0800

    联调dbc价格api

--- nextjs/rewrites.js
+++ nextjs/rewrites.js
@@ -2,6 +2,10 @@ async function rewrites() {
   return [
     { source: '/node-api/proxy/:slug*', destination: '/api/proxy' },
     { source: '/node-api/:slug*', destination: '/api/:slug*' },
+    {
+      source: '/nestapi/:path*',
+      destination: 'http://8.214.55.62:3001/:path*', // :path* 直接附加到根路径
+    },
   ].filter(Boolean);
 }
 
