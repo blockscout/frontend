543efb02e27819443cc54869dc615f4c53433fca
zkMe-jolan <jolan@zkme.xyz>
2025-03-11 16:24:35 +0800

    refactor: comment out unused session handling code in faucet page

--- pages/faucet.tsx
+++ pages/faucet.tsx
@@ -1,10 +1,10 @@
-import { getIronSession } from 'iron-session';
+// import { getIronSession } from 'iron-session';
 import type { NextPage, GetServerSideProps } from 'next';
 import React, { useState } from 'react';
 
 import PageNextJs from 'nextjs/PageNextJs';
 
-import { sessionOptions } from 'lib/session/config';
+// import { sessionOptions } from 'lib/session/config';
 import Faucet from 'ui/faucet/Faucet';
 import PageTitle from 'ui/shared/Page/PageTitle';
 
@@ -27,16 +27,15 @@ const Page: NextPage<Props> = ({ initialVerified }: Props) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps<Props> = async(context) => {
-  const { req, res } = context;
+export const getServerSideProps: GetServerSideProps<Props> = async() => {
+  // const { req, res } = context;
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const session = await getIronSession<{ user: any }>(req, res, sessionOptions);
-  const user = session.user;
+  // const session = await getIronSession<{ user: any }>(req, res, sessionOptions);
+  // const user = session.user;
 
   return {
     props: {
-      initialVerified: Boolean(user),
+      initialVerified: false,
     },
   };
 };
