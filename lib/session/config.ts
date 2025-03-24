8d0032e5cd6d2da08026ba653d431049a8367b36
zkMe-jolan <jolan@zkme.xyz>
2025-03-24 14:22:47 +0800

    fix: update session cookie name formatting to replace spaces with hyphens

--- lib/session/config.ts
+++ lib/session/config.ts
@@ -5,8 +5,7 @@ import { getEnvValue } from 'configs/app/utils';
 const ttl = getEnvValue('NEXT_PUBLIC_SESSION_TTL');
 
 export const sessionOptions: SessionOptions = {
-  cookieName: `${ getEnvValue('NEXT_PUBLIC_NETWORK_SHORT_NAME') }-session-token`,
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  cookieName: `${ getEnvValue('NEXT_PUBLIC_NETWORK_SHORT_NAME')!.split(' ').join('-') }-session-token`,
   password: getEnvValue('NEXT_PUBLIC_SESSION_PASSWORD')!,
   cookieOptions: {
     secure: getEnvValue('NEXT_PUBLIC_APP_ENV') === 'production',
