66925514f6a6a6b95f127d239a16a0f434560c99
zkMe-jack <jack@zkme.xyz>
2025-04-21 10:11:37 +0800

    feat: update scheme_id type to Array<string> in ExplorerTransaction and adjust related logic in Issuance and useTxQuery

--- ui/tx/useTxQuery.tsx
+++ ui/tx/useTxQuery.tsx
@@ -85,7 +85,7 @@ export default function useTxQuery(params?: Params): TxQuery {
             expiration_date: rp1.expiration_date[index],
           };
         }),
-        SchemaID: rp1.scheme_id || '/',
+        SchemaID: rp1.scheme_id[0] || '/',
         block_number: rp1.block_number,
         confirmations: rp1.block_confirmations,
         transaction_status: rp1.transaction_status,
