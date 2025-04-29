764832487820b33ea6bc527141ef97ee52d44bc7
tom <tom@ohhhh.me>
2025-04-29 15:14:55 +0200

    operation entity

--- ui/pages/TacOperation.tsx
+++ ui/pages/TacOperation.tsx
@@ -1,12 +1,17 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import { useAppContext } from 'lib/contexts/app';
+import getQueryParamString from 'lib/router/getQueryParamString';
 import TacOperationDetails from 'ui/operation/tac/TacOperationDetails';
 import TextAd from 'ui/shared/ad/TextAd';
+import OperationEntity from 'ui/shared/entities/operation/OperationEntity';
 import PageTitle from 'ui/shared/Page/PageTitle';
 
 const TacOperation = () => {
   const appProps = useAppContext();
+  const router = useRouter();
+  const hash = getQueryParamString(router.query.hash);
 
   const backLink = React.useMemo(() => {
     const hasGoBackLink = appProps.referrer && appProps.referrer.endsWith('/operations');
@@ -21,6 +26,10 @@ const TacOperation = () => {
     };
   }, [ appProps.referrer ]);
 
+  const titleSecondRow = (
+    <OperationEntity hash={ hash } noLink variant="subheading"/>
+  );
+
   return (
     <>
       <TextAd mb={ 6 }/>
@@ -28,6 +37,7 @@ const TacOperation = () => {
         title="Operation page"
         backLink={ backLink }
         isLoading={ false }
+        secondRow={ titleSecondRow }
       />
       <TacOperationDetails isLoading={ false }/>
     </>
---
+++ ui/shared/entities/operation/OperationEntity.tsx
@@ -0,0 +1,84 @@
+import { chakra } from '@chakra-ui/react';
+import React from 'react';
+
+import { route } from 'nextjs-routes';
+
+import * as EntityBase from 'ui/shared/entities/base/components';
+
+import { distributeEntityProps } from '../base/utils';
+
+type LinkProps = EntityBase.LinkBaseProps & Pick<EntityProps, 'hash'>;
+
+const Link = chakra((props: LinkProps) => {
+  const defaultHref = route({ pathname: '/operation/[hash]', query: { hash: props.hash } });
+
+  return (
+    <EntityBase.Link
+      { ...props }
+      href={ props.href ?? defaultHref }
+    >
+      { props.children }
+    </EntityBase.Link>
+  );
+});
+
+const Icon = (props: EntityBase.IconBaseProps) => {
+  return (
+    <EntityBase.Icon
+      { ...props }
+      name={ props.name ?? 'operation_slim' }
+      borderRadius="none"
+    />
+  );
+};
+
+type ContentProps = Omit<EntityBase.ContentBaseProps, 'text'> & Pick<EntityProps, 'hash'>;
+
+const Content = chakra((props: ContentProps) => {
+  return (
+    <EntityBase.Content
+      { ...props }
+      text={ props.hash }
+    />
+  );
+});
+
+type CopyProps = Omit<EntityBase.CopyBaseProps, 'text'> & Pick<EntityProps, 'hash'>;
+
+const Copy = (props: CopyProps) => {
+  return (
+    <EntityBase.Copy
+      { ...props }
+      text={ props.hash }
+    />
+  );
+};
+
+const Container = EntityBase.Container;
+
+export interface EntityProps extends EntityBase.EntityBaseProps {
+  hash: string;
+}
+
+const OperationEntity = (props: EntityProps) => {
+  const partsProps = distributeEntityProps(props);
+  const content = <Content { ...partsProps.content }/>;
+
+  return (
+    <Container { ...partsProps.container }>
+      <Icon { ...partsProps.icon }/>
+      { props.noLink ? content : <Link { ...partsProps.link }>{ content }</Link> }
+      <Copy { ...partsProps.copy }/>
+    </Container>
+  );
+};
+
+export default React.memo(chakra(OperationEntity));
+
+export {
+  Container,
+  Link,
+  Icon,
+  Content,
+  Copy,
+};
