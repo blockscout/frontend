41090a219a84ce97308be6bd679f0d05c222f68e
zkme-zhenghui <zhenghui@zkme.io>
2025-06-17 19:00:30 +0800

    update the og image

--- configs/app/meta.ts
+++ configs/app/meta.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import { getEnvValue, getExternalAssetFilePath } from './utils';
 
-const defaultImageUrl = '/static/og_placeholder2.png';
+const defaultImageUrl = '/static/og_placeholder_3.png';
 
 const meta = Object.freeze({
   promoteBlockscoutInTitle: getEnvValue('NEXT_PUBLIC_PROMOTE_BLOCKSCOUT_IN_TITLE') === 'false' ? false : true,
