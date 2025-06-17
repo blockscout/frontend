0f672a81b17f1dd8cbf91226a48756b9a7907195
tom <tom@ohhhh.me>
2025-06-17 16:02:56 +0200

    add loading state to icon shield

--- ui/shared/entities/base/components.tsx
+++ ui/shared/entities/base/components.tsx
@@ -139,12 +139,12 @@ const Icon = ({ isLoading, noIcon, variant, name, color, borderRadius, marginRig
   return (
     <Box position="relative">
       { iconElementWithHint }
-      <IconShield { ...shield }/>
+      <IconShield isLoading={ isLoading } { ...shield }/>
     </Box>
   );
 };
 
-type IconShieldProps = (ImageProps | IconSvgProps);
+type IconShieldProps = (ImageProps | IconSvgProps) & { isLoading?: boolean };
 
 const IconShield = (props: IconShieldProps) => {
 
@@ -160,15 +160,16 @@ const IconShield = (props: IconShieldProps) => {
     // Because the highlighted styles are described as CSS classes, we must do the same for the shield border color.
     // borderColor: 'global.body.bg',
     // backgroundColor: 'global.body.bg',
+    className: 'entity__shield',
   };
 
   if ('src' in props) {
-    return <Image className="entity__shield" { ...styles } { ...props }/>;
+    return props.isLoading ? <Skeleton loading { ...styles }/> : <Image { ...styles } { ...props }/>;
   }
 
   const svgProps = props as IconSvgProps;
 
-  return <IconSvg className="entity__shield" { ...styles } { ...svgProps }/>;
+  return <IconSvg { ...styles } { ...svgProps }/>;
 };
 
 export interface ContentBaseProps extends Pick<EntityBaseProps, 'className' | 'isLoading' | 'truncation' | 'tailLength' | 'noTooltip' | 'variant'> {
