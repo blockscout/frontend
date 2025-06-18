c90bfb0328ea92852714424a70c162dad438b838
okorieebube <okorieebube1@gmail.com>
2025-06-18 18:47:52 +0100

    fix: favicon issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -48,11 +48,12 @@ class MyDocument extends Document {
           <script src="/assets/envs.js"/>
 
           { /* FAVICON */ }
-          <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-white.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-white.png"/>
-          <link rel="icon" type="image/png" sizes="48x48" href="/assets/favicon/favicon-white.png"/>
-          <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/favicon-white.png"/>
-          <link rel="icon" type="image/png" sizes="192x192" href="/assets/favicon/favicon-white.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="48x48" href="/assets/favicon/favicon-48x48.png"/>
+          <link rel="shortcut icon" href="/assets/favicon/favicon.ico"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon-180x180.png"/>
+          <link rel="icon" type="image/png" sizes="192x192" href="/assets/favicon/android-chrome-192x192.png"/>
           <link rel="preload" as="image" href={ svgSprite.href }/>
         </Head>
         <body>
