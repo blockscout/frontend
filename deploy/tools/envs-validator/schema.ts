4a312e467f14df59af39c0643415d9e5549a2ef9
Max Alekseenko <nnwen0609@gmail.com>
2025-06-24 16:54:00 +0200

    change validation schema

--- deploy/tools/envs-validator/schema.ts
+++ deploy/tools/envs-validator/schema.ts
@@ -690,6 +690,46 @@ const externalTxsConfigSchema: yup.ObjectSchema<TxExternalTxsConfig> = yup.objec
   explorer_url_template: yup.string().required(),
 });
 
+const address3rdPartyWidgetsConfigSchema = yup
+  .object()
+  .shape({
+    NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS_CONFIG_URL: yup
+      .mixed()
+      .test('shape', 'Invalid schema were provided for NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS_CONFIG_URL, it should have name, url, icon, title, value', (data) => {
+        const isUndefined = data === undefined;
+        const parsedData = typeof data === 'string' ? JSON.parse(data) : data;
+        const valueSchema = yup.lazy((objValue) => {
+          let schema = yup.object();
+          Object.keys(objValue).forEach((key) => {
+            schema = schema.shape({
+              [key]: yup.object<Address3rdPartyWidget>().shape({
+                name: yup.string().required(),
+                url: yup.string().required(),
+                icon: yup.string().required(),
+                title: yup.string().required(),
+                hint: yup.string().optional(),
+                valuePath: yup.string().required(),
+                pages: yup.array().of(yup.string().oneOf(ADDRESS_3RD_PARTY_WIDGET_PAGES)).required(),
+                chainIds: yup.object<Record<string, string>>().optional(),
+              }),
+            });
+          });
+          return schema;
+        });
+        return isUndefined || valueSchema.isValidSync(parsedData);
+      }),
+    NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS: yup
+      .array()
+      .transform(replaceQuotes)
+      .json()
+      .of(yup.string())
+      .when('NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS_CONFIG_URL', {
+        is: (value: string) => value,
+        then: (schema) => schema,
+        otherwise: (schema) => schema.max(-1, 'NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS cannot not be used if NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS_CONFIG_URL is not provided'),
+      }),
+  });
+
 const schema = yup
   .object()
   .noUnknown(true, (params) => {
@@ -1086,41 +1126,6 @@ const schema = yup
 
         return isUndefined || valueSchema.isValidSync(data);
       }),
-    NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS_CONFIG_URL: yup
-      .mixed()
-      .test('shape', 'Invalid schema were provided for NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS_CONFIG_URL, it should have name, url, icon, title, value', (data) => {
-        const isUndefined = data === undefined;
-        const parsedData = typeof data === 'string' ? JSON.parse(data) : data;
-        const valueSchema = yup.lazy((objValue) => {
-          let schema = yup.object();
-          Object.keys(objValue).forEach((key) => {
-            schema = schema.shape({
-              [key]: yup.object<Address3rdPartyWidget>().shape({
-                name: yup.string().required(),
-                url: yup.string().required(),
-                icon: yup.string().required(),
-                title: yup.string().required(),
-                hint: yup.string().optional(),
-                valuePath: yup.string().required(),
-                pages: yup.array().of(yup.string().oneOf(ADDRESS_3RD_PARTY_WIDGET_PAGES)).required(),
-                chainIds: yup.object<Record<string, string>>().optional(),
-              }),
-            });
-          });
-          return schema;
-        });
-        return isUndefined || valueSchema.isValidSync(parsedData);
-      }),
-      NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS: yup
-      .array()
-      .transform(replaceQuotes)
-      .json()
-      .of(yup.string())
-      .when('NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS_CONFIG_URL', {
-        is: (value: string) => value,
-        then: (schema) => schema,
-        otherwise: (schema) => schema.max(-1, 'NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS cannot not be used if NEXT_PUBLIC_ADDRESS_3RD_PARTY_WIDGETS_CONFIG_URL is not provided'),
-      }),
 
     // 6. External services envs
     NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID: yup.string(),
@@ -1142,6 +1147,7 @@ const schema = yup
   .concat(beaconChainSchema)
   .concat(bridgedTokensSchema)
   .concat(sentrySchema)
-  .concat(tacSchema);
+  .concat(tacSchema)
+  .concat(address3rdPartyWidgetsConfigSchema);
 
 export default schema;
