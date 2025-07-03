9bd281032d26bdd94831070c2b0e743b3529d131
Javad Khalilian <khalilian.javad@gmail.com>
2025-07-03 12:39:29 +0200

    use testnet backend in dev mode

--- nextjs/rewrites.js
+++ nextjs/rewrites.js
@@ -1,7 +1,28 @@
+const proxyToTestnet = (cid) => [
+  {
+    source: '/socket/:path*',
+    destination: `https://chain-${ cid }.evm-testnet-blockscout.chainweb.com/socket/:path*`,
+  },
+  {
+    source: '/stats/:path*',
+    destination: `https://chain-${ cid }.evm-testnet-blockscout.chainweb.com/stats/:path*`,
+  },
+  {
+    source: '/visualizer/:path*',
+    destination: `https://chain-${ cid }.evm-testnet-blockscout.chainweb.com/visualizer/:path*`,
+  },
+  {
+    source: '/api/:path*',
+    destination: `https://chain-${ cid }.evm-testnet-blockscout.chainweb.com/api/:path*`,
+  },
+];
+
 async function rewrites() {
   return [
     { source: '/node-api/proxy/:slug*', destination: '/api/proxy' },
     { source: '/node-api/:slug*', destination: '/api/:slug*' },
+    ...(process.env.NODE_ENV === 'development' ? proxyToTestnet(process.env.CHAINWEB_CHAIN_ID) : []),
+
   ].filter(Boolean);
 }
 
