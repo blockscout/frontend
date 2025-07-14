1583a204307dc92d16fa5198046c390df7e912a7
Richard Oh <n24512@protonmail.com>
2025-07-14 14:59:32 -0400

    migrates all clusters pw tests from page.route to mockApiResponse

--- ui/pages/Clusters.pw.tsx
+++ ui/pages/Clusters.pw.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { clustersDirectoryMock } from 'mocks/clusters/directory';
 import { clustersLeaderboardMock } from 'mocks/clusters/leaderboard';
 import { ENVS_MAP } from 'playwright/fixtures/mockEnvs';
-import { test, expect } from 'playwright/lib';
+import { test, expect, devices } from 'playwright/lib';
 
 import Clusters from './Clusters';
 
@@ -14,34 +14,45 @@ test.beforeEach(async({ mockEnvs, mockTextAd }) => {
 });
 
 test.describe('Clusters Directory Page', () => {
-  test('clusters directory with data @mobile', async({ render, page, mockAssetResponse }) => {
-    await mockAssetResponse('https://cdn.clusters.xyz/profile-image/campnetwork/lol', './playwright/mocks/image_s.jpg');
-    await mockAssetResponse('https://cdn.clusters.xyz/profile-image/duck/quack', './playwright/mocks/image_s.jpg');
-    await mockAssetResponse('https://cdn.clusters.xyz/profile-image/test/cluster', './playwright/mocks/image_s.jpg');
-    await page.route('**/v1/trpc/names.search*', (route) => {
-      route.fulfill({
-        status: 200,
-        json: clustersDirectoryMock,
+  test.describe('mobile', () => {
+    test.use({ viewport: devices['iPhone 13 Pro'].viewport });
+
+    test('clusters directory with data @mobile', async({ render, mockApiResponse, mockAssetResponse }) => {
+      await mockAssetResponse('https://cdn.clusters.xyz/profile-image/campnetwork/lol', './playwright/mocks/image_s.jpg');
+      await mockAssetResponse('https://cdn.clusters.xyz/profile-image/duck/quack', './playwright/mocks/image_s.jpg');
+      await mockAssetResponse('https://cdn.clusters.xyz/profile-image/test/cluster', './playwright/mocks/image_s.jpg');
+      await mockApiResponse('clusters:get_directory', clustersDirectoryMock, {
+        queryParams: {
+          input: JSON.stringify({
+            offset: 0,
+            limit: 50,
+            orderBy: 'createdAt-desc',
+            query: '',
+          }),
+        },
       });
-    });
-    await page.route('**/v1/trpc/names.leaderboard*', (route) => {
-      route.fulfill({
-        status: 200,
-        json: clustersLeaderboardMock,
+      await mockApiResponse('clusters:get_leaderboard', clustersLeaderboardMock, {
+        queryParams: {
+          input: JSON.stringify({
+            offset: 0,
+            limit: 50,
+            orderBy: 'rank-asc',
+          }),
+        },
       });
+
+      const component = await render(
+        <div>
+          <Box h={{ base: '134px', lg: 6 }}/>
+          <Clusters/>
+        </div>,
+      );
+
+      await expect(component.getByText('campnetwork/lol').first()).toBeVisible();
+      await expect(component.getByText('duck/quack').first()).toBeVisible();
+      await expect(component.getByText('test/cluster').first()).toBeVisible();
+
+      await expect(component).toHaveScreenshot();
     });
-
-    const component = await render(
-      <div>
-        <Box h={{ base: '134px', lg: 6 }}/>
-        <Clusters/>
-      </div>,
-    );
-
-    await expect(component.getByText('campnetwork/lol').first()).toBeVisible();
-    await expect(component.getByText('duck/quack').first()).toBeVisible();
-    await expect(component.getByText('test/cluster').first()).toBeVisible();
-
-    await expect(component).toHaveScreenshot();
   });
 });
