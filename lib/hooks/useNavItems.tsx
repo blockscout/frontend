10fe80ba81bee77fc4c9cd241c75f379fa934faf
Richard Oh <n24512@protonmail.com>
2025-07-18 13:19:45 -0400

    removes repeating var from blockchainNavItems array

--- lib/hooks/useNavItems.tsx
+++ lib/hooks/useNavItems.tsx
@@ -212,7 +212,6 @@ export default function useNavItems(): ReturnType {
         userOps,
         blocks,
         epochs,
-        epochs,
         topAccounts,
         validators,
         verifiedContracts,
