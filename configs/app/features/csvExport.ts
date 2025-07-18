f94e030b7e64f0bebad09cea966fd5bd2f7adfdd
Franklin <nwanzechuka1993@gmail.com>
2025-07-18 23:02:07 +0100

    Update csvExport.ts

--- configs/app/features/csvExport.ts
+++ configs/app/features/csvExport.ts
@@ -5,26 +5,19 @@ import services from '../services';
 const title = 'Export data to CSV file';
 
 const config: Feature<{ reCaptcha: { siteKey: string } }> = (() => {
+  if (services.reCaptchaV2.siteKey) {
+    return Object.freeze({
+      title,
+      isEnabled: true,
+      reCaptcha: {
+        siteKey: services.reCaptchaV2.siteKey,
+      },
+    });
+  }
   return Object.freeze({
     title,
-    isEnabled: Boolean(services.reCaptchaV2.siteKey),
-    reCaptcha: {
-      siteKey: services.reCaptchaV2.siteKey ?? '',
-    },
+    isEnabled: true,
   });
-  // if (services.reCaptchaV2.siteKey) {
-  //   return Object.freeze({
-  //     title,
-  //     isEnabled: true,
-  //     reCaptcha: {
-  //       siteKey: services.reCaptchaV2.siteKey,
-  //     },
-  //   });
-  // }
-  // return Object.freeze({
-  //   title,
-  //   isEnabled: false,
-  // });
 })();
 
 export default config;
