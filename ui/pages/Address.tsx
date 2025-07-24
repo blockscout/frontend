9edd7b47914df9c9f0627ca41e0fcca644e470c1
tom goriunov <tom@ohhhh.me>
2025-07-24 13:40:41 +0200

    Contract tab: finding matching bytecode states (#2680)
    * Contract tab: finding matching bytecode states
    
    Fixes #2678
    
    * handle case when there is only one sub-tab on contract tab
    
    * tests
    
    * prevent calling the smart contract resource twice when opening the tab
    
    * [wip] add debug logs
    
    * change api host for op sepolia
    
    * rollback debug and update screenshots

--- ui/pages/Address.tsx
+++ ui/pages/Address.tsx
@@ -39,7 +39,6 @@ import AddressTokenTransfers from 'ui/address/AddressTokenTransfers';
 import AddressTxs from 'ui/address/AddressTxs';
 import AddressUserOps from 'ui/address/AddressUserOps';
 import AddressWithdrawals from 'ui/address/AddressWithdrawals';
-import useContractTabs from 'ui/address/contract/useContractTabs';
 import { CONTRACT_TAB_IDS } from 'ui/address/contract/utils';
 import AddressFavoriteButton from 'ui/address/details/AddressFavoriteButton';
 import AddressMetadataAlert from 'ui/address/details/AddressMetadataAlert';
@@ -172,12 +171,6 @@ const AddressPageContent = () => {
 
   const xStarQuery = useFetchXStarScore({ hash });
 
-  const contractTabs = useContractTabs(
-    addressQuery.data,
-    config.features.mudFramework.isEnabled ? (mudTablesCountQuery.isPlaceholderData || addressQuery.isPlaceholderData) : addressQuery.isPlaceholderData,
-    Boolean(config.features.mudFramework.isEnabled && mudTablesCountQuery.data && mudTablesCountQuery.data > 0),
-  );
-
   const tabs: Array<TabItemRegular> = React.useMemo(() => {
     return [
       {
@@ -203,9 +196,9 @@ const AddressPageContent = () => {
         },
         component: (
           <AddressContract
-            tabs={ contractTabs.tabs }
-            shouldRender={ !isTabsLoading }
-            isLoading={ contractTabs.isLoading }
+            addressData={ addressQuery.data }
+            isLoading={ isTabsLoading }
+            hasMudTab={ Boolean(config.features.mudFramework.isEnabled && mudTablesCountQuery.data && mudTablesCountQuery.data > 0) }
           />
         ),
         subTabs: CONTRACT_TAB_IDS,
@@ -309,7 +302,6 @@ const AddressPageContent = () => {
   }, [
     addressQuery,
     countersQuery,
-    contractTabs,
     addressTabsCountersQuery.data,
     userOpsAccountQuery.data,
     isTabsLoading,
