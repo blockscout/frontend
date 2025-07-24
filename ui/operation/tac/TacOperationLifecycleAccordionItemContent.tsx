5f4c5dbfb0cdb35d45f4e1ea25543654dc48c458
tom <tom@ohhhh.me>
2025-07-24 15:38:16 +0200

    fix tac operation and zilliqa quorum snippets

--- ui/operation/tac/TacOperationLifecycleAccordionItemContent.tsx
+++ ui/operation/tac/TacOperationLifecycleAccordionItemContent.tsx
@@ -23,7 +23,7 @@ const TacOperationLifecycleAccordionItemContent = ({ isLast, data }: Props) => {
       borderColor="border.divider"
     >
       <Grid
-        gridTemplateColumns="112px 1fr"
+        gridTemplateColumns="112px minmax(0, 1fr)"
         alignItems="flex-start"
         columnGap={ 3 }
         rowGap={ 1 }
@@ -48,7 +48,6 @@ const TacOperationLifecycleAccordionItemContent = ({ isLast, data }: Props) => {
             </GridItem>
             <GridItem
               display="inline-flex"
-              flexWrap="wrap"
               alignItems="center"
               py="6px"
             >
