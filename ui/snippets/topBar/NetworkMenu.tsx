95cc8d19bb4163db4fcb14110e9910cfd6af89c5
tom <tom@ohhhh.me>
2025-07-25 11:54:53 +0200

    UX: add tooltip (setting/chains)
    Fixes #2877

--- ui/snippets/topBar/NetworkMenu.tsx
+++ ui/snippets/topBar/NetworkMenu.tsx
@@ -1,33 +1,52 @@
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
 
 import { IconButton } from 'toolkit/chakra/icon-button';
 import { PopoverRoot, PopoverTrigger } from 'toolkit/chakra/popover';
+import { Tooltip } from 'toolkit/chakra/tooltip';
+import { useDisclosure } from 'toolkit/hooks/useDisclosure';
 import IconSvg from 'ui/shared/IconSvg';
 import NetworkMenuContentDesktop from 'ui/snippets/networkMenu/NetworkMenuContentDesktop';
 import useNetworkMenu from 'ui/snippets/networkMenu/useNetworkMenu';
 
 const NetworkMenu = () => {
   const menu = useNetworkMenu();
+  const tooltip = useDisclosure();
+
+  const handlePopoverOpenChange = React.useCallback(({ open }: { open: boolean }) => {
+    open && tooltip.onClose();
+    menu.onOpenChange({ open });
+  }, [ menu, tooltip ]);
+
+  const handleTooltipOpenChange = React.useCallback(({ open }: { open: boolean }) => {
+    if (!menu.open) {
+      tooltip.onOpenChange({ open });
+    }
+  }, [ menu, tooltip ]);
 
   return (
     <PopoverRoot
       positioning={{ placement: 'bottom-start', offset: { mainAxis: 6 } }}
       lazyMount
       open={ menu.open }
-      onOpenChange={ menu.onOpenChange }
+      onOpenChange={ handlePopoverOpenChange }
     >
-      <PopoverTrigger>
-        <IconButton
-          variant="link"
-          boxSize={ 5 }
-          aria-label="Network menu"
-          borderRadius="sm"
-          onClick={ menu.onToggle }
-          p={ 0.5 }
-        >
-          <IconSvg name="networks" boxSize="full"/>
-        </IconButton>
-      </PopoverTrigger>
+      <Tooltip content="Chains list" disableOnMobile open={ tooltip.open } onOpenChange={ handleTooltipOpenChange }>
+        <Flex alignItems="center">
+          <PopoverTrigger>
+            <IconButton
+              variant="link"
+              boxSize={ 5 }
+              aria-label="Network menu"
+              borderRadius="sm"
+              onClick={ menu.onToggle }
+              p={ 0.5 }
+            >
+              <IconSvg name="networks" boxSize="full"/>
+            </IconButton>
+          </PopoverTrigger>
+        </Flex>
+      </Tooltip>
       <NetworkMenuContentDesktop items={ menu.data } tabs={ menu.availableTabs }/>
     </PopoverRoot>
   );
