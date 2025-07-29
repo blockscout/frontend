6ee7219f3fbb9e51ad6d547d88acbcb442634c19
isstuev <isstuev@gmail.com>
2025-07-29 14:47:08 +0200

    remove backlink

--- ui/pages/ZetaChainCCTX.tsx
+++ ui/pages/ZetaChainCCTX.tsx
@@ -2,7 +2,6 @@ import { useRouter } from 'next/router';
 import React from 'react';
 
 import useApiQuery from 'lib/api/useApiQuery';
-import { useAppContext } from 'lib/contexts/app';
 import throwOnResourceLoadError from 'lib/errors/throwOnResourceLoadError';
 import getQueryParamString from 'lib/router/getQueryParamString';
 import { zetaChainCCTX } from 'mocks/zetaChain/zetaChainCCTX';
@@ -13,7 +12,6 @@ import ZetaChainCCTXDetails from 'ui/txs/zetaChain/ZetaChainCCTXDetails';
 
 const ZetaChainCCTX = () => {
   const router = useRouter();
-  const appProps = useAppContext();
 
   const hash = getQueryParamString(router.query.hash);
 
@@ -26,30 +24,11 @@ const ZetaChainCCTX = () => {
 
   throwOnResourceLoadError(cctxQuery);
 
-  const backLink = React.useMemo(() => {
-    const hasGoBackLink = appProps.referrer && appProps.referrer.includes('/txs') && (
-      appProps.referrer.includes('tab=cctx_pending') ||
-      appProps.referrer.includes('tab=cctx_mined') ||
-      appProps.referrer.includes('tab=cross_chain') ||
-      !appProps.referrer.includes('tab')
-    );
-
-    if (!hasGoBackLink) {
-      return;
-    }
-
-    return {
-      label: 'Back to crass-chain transactions list',
-      url: appProps.referrer,
-    };
-  }, [ appProps.referrer ]);
-
   return (
     <>
       <TextAd mb={ 6 }/>
       <PageTitle
         title="Cross-chain tx details"
-        backLink={ backLink }
         secondRow={ <TxEntity hash={ hash } noLink noIcon noCopy={ false } variant="subheading" mr={{ base: 0, lg: 2 }}/> }
       />
       <ZetaChainCCTXDetails data={ cctxQuery.data } isLoading={ cctxQuery.isPlaceholderData }/>
