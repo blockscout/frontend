e0eac34cf6249b849c0a9185754c91363aa9fbb2
tom <tom@ohhhh.me>
2025-07-31 20:00:11 +0200

    change package for decompressing socket data

--- ui/blocks/flashblocks/parseSocketEventData.tsx
+++ ui/blocks/flashblocks/parseSocketEventData.tsx
@@ -1,20 +1,21 @@
-import brotliPromise from 'brotli-dec-wasm';
+import { decompress } from 'brotli-compress/js';
 
 import type { FlashblockItemApi } from 'types/api/flashblocks';
 
 export default async function parseSocketEventData(event: MessageEvent) {
   try {
-    const brotli = await brotliPromise;
     const textData = await event.data.text();
     if (textData.trim().startsWith('{')) {
       return JSON.parse(textData) as FlashblockItemApi;
     } else {
       const arrayBuffer = await event.data.arrayBuffer();
       const u8Data = new Uint8Array(arrayBuffer);
-      const decompressedData = Buffer.from(brotli.decompress(u8Data)).toString('utf-8');
+      const decompressedData = Buffer.from(await decompress(u8Data)).toString('utf-8');
       return JSON.parse(decompressedData) as FlashblockItemApi;
     }
   } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
     return;
   }
 }
