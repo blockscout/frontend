b43836497999257d6a9a15ebb1c68c4e2fc162ef
isstuev <isstuev@gmail.com>
2025-08-07 00:18:44 +0200

    some fixes

--- ui/txs/zetaChain/ZetaChainCCTXDetailsLifecycleOut.tsx
+++ ui/txs/zetaChain/ZetaChainCCTXDetailsLifecycleOut.tsx
@@ -2,28 +2,31 @@ import { Flex, Grid, Text } from '@chakra-ui/react';
 import { BigNumber } from 'bignumber.js';
 import React from 'react';
 
-import type { ZetaChainCCTXOutboundParams, ZetaChainCCTXStatus } from 'types/api/zetaChain';
+import type { ZetaChainCCTXOutboundParams, ZetaChainCCTXResponse } from 'types/api/zetaChain';
 
 import config from 'configs/app';
 import { Skeleton } from 'toolkit/chakra/skeleton';
 import TxEntity from 'ui/shared/entities/tx/TxEntity';
 import TxEntityWithExternalChain from 'ui/shared/entities/tx/TxEntityWithExternalChain';
-// import StatusTag from 'ui/shared/statusTag/StatusTag';
+import StatusTag from 'ui/shared/statusTag/StatusTag';
 import ZetaChainAddressEntity from 'ui/shared/zetaChain/ZetaChainAddressEntity';
+import ZetaChainCCTXValue from 'ui/shared/zetaChain/ZetaChainCCTXValue';
 import useZetaChainConfig from 'ui/zetaChain/useZetaChainConfig';
 
 type Props = {
-  tx: ZetaChainCCTXOutboundParams;
-  txStatus: ZetaChainCCTXStatus;
+  outboundParam: ZetaChainCCTXOutboundParams;
+  tx: ZetaChainCCTXResponse;
   isLoading: boolean;
   isLast: boolean;
 };
 
-const ZetaChainCCTXDetailsLifecycleOut = ({ tx, txStatus, isLoading, isLast }: Props) => {
+const ZetaChainCCTXDetailsLifecycleOut = ({ outboundParam, tx, isLoading, isLast }: Props) => {
   const { data: chainsConfig } = useZetaChainConfig();
-  const chainTo = chainsConfig?.find((chain) => chain.chain_id === tx.receiver_chain_id);
+  const chainTo = chainsConfig?.find((chain) => chain.chain_id === outboundParam.receiver_chain_id);
 
-  if (txStatus === 'PENDING_INBOUND') {
+  const gasDecimals = config.chain.currency.decimals;
+
+  if (tx.cctx_status.status === 'PENDING_INBOUND') {
     return null;
   }
 
@@ -31,38 +34,50 @@ const ZetaChainCCTXDetailsLifecycleOut = ({ tx, txStatus, isLoading, isLast }: P
   let text: string = '';
   let color: string = '';
 
-  if (txStatus === 'OUTBOUND_MINED') {
+  if (tx.cctx_status.status === 'OUTBOUND_MINED') {
     content = (
       <>
         <Text color="text.secondary" fontWeight="medium">Transaction</Text>
         { chainTo?.chain_id.toString() !== config.chain.id ? (
-          <TxEntityWithExternalChain chain={ chainTo } hash={ tx.hash } noIcon noCopy={ false }/>
+          <TxEntityWithExternalChain chain={ chainTo } hash={ outboundParam.hash } noIcon noCopy={ false }/>
         ) : (
-          <TxEntity hash={ tx.hash } noIcon noCopy={ false }/>
+          <TxEntity hash={ outboundParam.hash } noIcon noCopy={ false }/>
         ) }
+        <Text color="text.secondary" fontWeight="medium">Status</Text>
+        <StatusTag type="ok" text="Success"/>
         <Text color="text.secondary" fontWeight="medium">Receiver</Text>
         <ZetaChainAddressEntity
-          hash={ tx.receiver }
-          chainId={ tx.receiver_chain_id.toString() }
+          hash={ outboundParam.receiver }
+          chainId={ outboundParam.receiver_chain_id.toString() }
           isLoading={ isLoading }
           truncation="constant"
         />
+        <Text color="text.secondary" fontWeight="medium">Transferred</Text>
+        <ZetaChainCCTXValue
+          coinType={ outboundParam.coin_type }
+          tokenSymbol={ tx.token_symbol }
+          amount={ outboundParam.amount }
+          decimals={ tx.decimals?.toString() }
+          isLoading={ isLoading }
+        />
         <Text color="text.secondary" fontWeight="medium">Gas used</Text>
-        <Text>{ BigNumber(tx.gas_used || 0).toFormat() }</Text>
+        <Text>{ BigNumber(outboundParam.gas_used || 0).div(10 ** gasDecimals).toFormat() }</Text>
       </>
     );
     text = `Sent tx to ${ chainTo?.chain_name || 'Unknown chain' }`;
     color = 'text.success';
-  } else if (txStatus === 'PENDING_REVERT') {
+  } else if (tx.cctx_status.status === 'PENDING_REVERT') {
     if (!isLast) {
       content = (
         <>
           <Text color="text.secondary" fontWeight="medium">Transaction</Text>
           { chainTo?.chain_id.toString() !== config.chain.id ? (
-            <TxEntityWithExternalChain chain={ chainTo } hash={ tx.hash } noIcon noCopy={ false }/>
+            <TxEntityWithExternalChain chain={ chainTo } hash={ outboundParam.hash } noIcon noCopy={ false }/>
           ) : (
-            <TxEntity hash={ tx.hash } noIcon noCopy={ false }/>
+            <TxEntity hash={ outboundParam.hash } noIcon noCopy={ false }/>
           ) }
+          <Text color="text.secondary" fontWeight="medium">Status</Text>
+          <StatusTag type="error" text="Failed"/>
         </>
       );
       text = `Destination tx failed`;
@@ -72,8 +87,8 @@ const ZetaChainCCTXDetailsLifecycleOut = ({ tx, txStatus, isLoading, isLast }: P
         <>
           <Text color="text.secondary" fontWeight="medium">Reverting to</Text>
           <ZetaChainAddressEntity
-            hash={ tx.receiver }
-            chainId={ tx.receiver_chain_id.toString() }
+            hash={ outboundParam.receiver }
+            chainId={ outboundParam.receiver_chain_id.toString() }
             isLoading={ isLoading }
             truncation="constant"
           />
@@ -82,32 +97,34 @@ const ZetaChainCCTXDetailsLifecycleOut = ({ tx, txStatus, isLoading, isLast }: P
       text = `Waiting for revert to ${ chainTo?.chain_name || 'Unknown chain' }`;
       color = 'text.secondary';
     }
-  } else if (txStatus === 'PENDING_OUTBOUND') {
+  } else if (tx.cctx_status.status === 'PENDING_OUTBOUND') {
     content = (
       <>
         <Text color="text.secondary" fontWeight="medium">Destination</Text>
         <ZetaChainAddressEntity
-          hash={ tx.receiver }
-          chainId={ tx.receiver_chain_id.toString() }
+          hash={ outboundParam.receiver }
+          chainId={ outboundParam.receiver_chain_id.toString() }
           isLoading={ isLoading }
           truncation="constant"
         />
         <Text color="text.secondary" fontWeight="medium">Nonce</Text>
-        <Text>{ tx.tss_nonce }</Text>
+        <Text>{ outboundParam.tss_nonce }</Text>
       </>
     );
     text = `Waiting for outbound tx to ${ chainTo?.chain_name || 'Unknown chain' }`;
     color = 'text.secondary';
-  } else if (txStatus === 'REVERTED') {
+  } else if (tx.cctx_status.status === 'REVERTED') {
     if (!isLast) {
       content = (
         <>
           <Text color="text.secondary" fontWeight="medium">Transaction</Text>
           { chainTo?.chain_id.toString() !== config.chain.id ? (
-            <TxEntityWithExternalChain chain={ chainTo } hash={ tx.hash } noIcon noCopy={ false }/>
+            <TxEntityWithExternalChain chain={ chainTo } hash={ outboundParam.hash } noIcon noCopy={ false }/>
           ) : (
-            <TxEntity hash={ tx.hash } noIcon noCopy={ false }/>
+            <TxEntity hash={ outboundParam.hash } noIcon noCopy={ false }/>
           ) }
+          <Text color="text.secondary" fontWeight="medium">Status</Text>
+          <StatusTag type="error" text="Failed"/>
         </>
       );
       text = `Destination tx failed`;
@@ -115,26 +132,44 @@ const ZetaChainCCTXDetailsLifecycleOut = ({ tx, txStatus, isLoading, isLast }: P
     } else {
       content = (
         <>
-          <Text color="text.secondary" fontWeight="medium">Reverted to</Text>
+          <Text color="text.secondary" fontWeight="medium">Origin</Text>
           <ZetaChainAddressEntity
-            hash={ tx.receiver }
-            chainId={ tx.receiver_chain_id.toString() }
+            hash={ outboundParam.receiver }
+            chainId={ outboundParam.receiver_chain_id.toString() }
             isLoading={ isLoading }
             truncation="constant"
           />
+          <Text color="text.secondary" fontWeight="medium">Transaction</Text>
+          { chainTo?.chain_id.toString() !== config.chain.id ? (
+            <TxEntityWithExternalChain chain={ chainTo } hash={ outboundParam.hash } noIcon noCopy={ false }/>
+          ) : (
+            <TxEntity hash={ outboundParam.hash } noIcon noCopy={ false }/>
+          ) }
+          <Text color="text.secondary" fontWeight="medium">Status</Text>
+          <StatusTag type="ok" text="Success"/>
+          <Text color="text.secondary" fontWeight="medium">Transferred</Text>
+          <ZetaChainCCTXValue
+            coinType={ outboundParam.coin_type }
+            tokenSymbol={ tx.token_symbol }
+            amount={ outboundParam.amount }
+            decimals={ tx.decimals?.toString() }
+            isLoading={ isLoading }
+          />
+          <Text color="text.secondary" fontWeight="medium">Gas used</Text>
+          <Text>{ BigNumber(outboundParam.gas_used || 0).div(10 ** gasDecimals).toFormat() } { config.chain.currency.symbol }</Text>
         </>
       );
       text = `Reverted to ${ chainTo?.chain_name || 'Unknown chain' }`;
       color = 'text.success';
     }
-  } else if (txStatus === 'ABORTED') {
+  } else if (tx.cctx_status.status === 'ABORTED') {
     if (!isLast) {
       content = (
         <>
           <Text color="text.secondary" fontWeight="medium">Receiver</Text>
           <ZetaChainAddressEntity
-            hash={ tx.receiver }
-            chainId={ tx.receiver_chain_id.toString() }
+            hash={ outboundParam.receiver }
+            chainId={ outboundParam.receiver_chain_id.toString() }
             isLoading={ isLoading }
             truncation="constant"
           />
@@ -147,8 +182,8 @@ const ZetaChainCCTXDetailsLifecycleOut = ({ tx, txStatus, isLoading, isLast }: P
         <>
           <Text color="text.secondary" fontWeight="medium">Sender</Text>
           <ZetaChainAddressEntity
-            hash={ tx.receiver }
-            chainId={ tx.receiver_chain_id.toString() }
+            hash={ outboundParam.receiver }
+            chainId={ outboundParam.receiver_chain_id.toString() }
             isLoading={ isLoading }
             truncation="constant"
           />
@@ -160,7 +195,7 @@ const ZetaChainCCTXDetailsLifecycleOut = ({ tx, txStatus, isLoading, isLast }: P
   }
 
   return (
-    <Skeleton loading={ isLoading } key={ tx.hash } w="100%">
+    <Skeleton loading={ isLoading } key={ outboundParam.hash } w="100%">
       { /* color is incorrect, idk where to get the right one */ }
       <Flex color={ color } maxH="20px" alignItems="center" mb={ 2.5 }>
         { text }
