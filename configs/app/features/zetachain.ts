1fa188dcbc7b90218c6921dfc31fe964cecf1023
isstuev <isstuev@gmail.com>
2025-08-18 14:57:38 +0200

    cctx search

--- configs/app/features/zetachain.ts
+++ configs/app/features/zetachain.ts
@@ -1,18 +1,22 @@
 import type { Feature } from './types';
 
 import apis from '../apis';
-import { getExternalAssetFilePath } from '../utils';
+import { getEnvValue, getExternalAssetFilePath } from '../utils';
 
 const title = 'ZetaChain transactions';
 
 const chainsConfigUrl = getExternalAssetFilePath('NEXT_PUBLIC_ZETACHAIN_SERVICE_CHAINS_CONFIG_URL');
+const cosmosTxUrlTemplate = getEnvValue('NEXT_PUBLIC_ZETACHAIN_COSMOS_TX_URL_TEMPLATE');
+const cosmosAddressUrlTemplate = getEnvValue('NEXT_PUBLIC_ZETACHAIN_COSMOS_ADDRESS_URL_TEMPLATE');
 
-const config: Feature<{ chainsConfigUrl: string }> = (() => {
-  if (apis.zetachain && chainsConfigUrl) {
+const config: Feature<{ chainsConfigUrl: string; cosmosTxUrlTemplate: string; cosmosAddressUrlTemplate: string }> = (() => {
+  if (apis.zetachain && chainsConfigUrl && cosmosTxUrlTemplate && cosmosAddressUrlTemplate) {
     return Object.freeze({
       title,
       isEnabled: true,
       chainsConfigUrl,
+      cosmosTxUrlTemplate,
+      cosmosAddressUrlTemplate,
     });
   }
 
