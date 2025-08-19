8d4f8f02eacf4fffb39fd7b7567bf4eb22e5f609
tom <tom@ohhhh.me>
2025-08-19 18:30:30 +0200

    update text size

--- ui/megaEth/uptime/UptimeStatus.tsx
+++ ui/megaEth/uptime/UptimeStatus.tsx
@@ -33,7 +33,7 @@ const UptimeStatus = ({ status, onReconnect }: Props) => {
       { statusTag }
       <Button variant="link" gap={ 1 } onClick={ onReconnect } disabled={ status === 'connected' }>
         <IconSvg name="refresh" boxSize={ 5 }/>
-        <chakra.span hideBelow="lg">Refresh</chakra.span>
+        <chakra.span hideBelow="lg" fontSize="sm">Refresh</chakra.span>
       </Button>
     </HStack>
   );
