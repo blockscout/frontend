e10f2e6511ad38bf619672d88cb62394fb95a0df
tom <tom@ohhhh.me>
2025-08-21 16:17:06 +0200

    update swagger default server

--- ui/apiDocs/utils.ts
+++ ui/apiDocs/utils.ts
@@ -25,7 +25,7 @@ export const REST_API_SECTIONS = [
     swagger: {
       url: feature.coreApiSwaggerUrl,
       requestInterceptor: (req: SwaggerRequest) => {
-        const DEFAULT_SERVER = 'blockscout.com/poa/core';
+        const DEFAULT_SERVER = 'eth.blockscout.com';
 
         if (!req.loadSpec) {
           const newUrl = new URL(req.url.replace(DEFAULT_SERVER, config.apis.general.host));
