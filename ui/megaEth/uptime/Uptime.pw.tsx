e8fcfc836a9a4008e6dabc1e2770ba04ef0df0b7
tom <tom@ohhhh.me>
2025-08-26 14:46:47 +0200

    fix test

--- ui/megaEth/uptime/Uptime.pw.tsx
+++ ui/megaEth/uptime/Uptime.pw.tsx
@@ -7,6 +7,8 @@ import { MINUTE, SECOND } from 'toolkit/utils/consts';
 
 import Uptime from './Uptime';
 
+const now = new Date('2022-11-11T12:00:00Z').getTime();
+
 const REALTIME_DATA = {
   realtime: {
     end_block: 14416819,
@@ -23,29 +25,29 @@ const REALTIME_DATA = {
 const HISTORY_FULL_DATA = {
   data: {
     historical_gas_per_second_3h: [
-      { timestamp: (Date.now() - 5 * MINUTE) / SECOND, value: 78093010 },
-      { timestamp: (Date.now() - 4 * MINUTE) / SECOND, value: 120212720 },
-      { timestamp: (Date.now() - 3 * MINUTE) / SECOND, value: 49049412 },
-      { timestamp: (Date.now() - 2 * MINUTE) / SECOND, value: 70900270 },
-      { timestamp: (Date.now() - MINUTE) / SECOND, value: 97225784 },
+      { timestamp: (now - 5 * MINUTE) / SECOND, value: 78093010 },
+      { timestamp: (now - 4 * MINUTE) / SECOND, value: 120212720 },
+      { timestamp: (now - 3 * MINUTE) / SECOND, value: 49049412 },
+      { timestamp: (now - 2 * MINUTE) / SECOND, value: 70900270 },
+      { timestamp: (now - MINUTE) / SECOND, value: 97225784 },
     ],
     historical_gas_per_second_24h: [],
     historical_gas_per_second_7d: [],
     historical_tps_3h: [
-      { timestamp: (Date.now() - 5 * MINUTE) / SECOND, value: 196 },
-      { timestamp: (Date.now() - 4 * MINUTE) / SECOND, value: 203.2 },
-      { timestamp: (Date.now() - 3 * MINUTE) / SECOND, value: 197.6 },
-      { timestamp: (Date.now() - 2 * MINUTE) / SECOND, value: 215.4 },
-      { timestamp: (Date.now() - MINUTE) / SECOND, value: 218.2 },
+      { timestamp: (now - 5 * MINUTE) / SECOND, value: 196 },
+      { timestamp: (now - 4 * MINUTE) / SECOND, value: 203.2 },
+      { timestamp: (now - 3 * MINUTE) / SECOND, value: 197.6 },
+      { timestamp: (now - 2 * MINUTE) / SECOND, value: 215.4 },
+      { timestamp: (now - MINUTE) / SECOND, value: 218.2 },
     ],
     historical_tps_24h: [],
     historical_tps_7d: [],
     historical_mini_block_interval_3h: [
-      { timestamp: (Date.now() - 5 * MINUTE) / SECOND, value: 10.537408 },
-      { timestamp: (Date.now() - 4 * MINUTE) / SECOND, value: 10.593221 },
-      { timestamp: (Date.now() - 3 * MINUTE) / SECOND, value: 10.537408 },
-      { timestamp: (Date.now() - 2 * MINUTE) / SECOND, value: 10.593221 },
-      { timestamp: (Date.now() - MINUTE) / SECOND, value: 10.570825 },
+      { timestamp: (now - 5 * MINUTE) / SECOND, value: 10.537408 },
+      { timestamp: (now - 4 * MINUTE) / SECOND, value: 10.593221 },
+      { timestamp: (now - 3 * MINUTE) / SECOND, value: 10.537408 },
+      { timestamp: (now - 2 * MINUTE) / SECOND, value: 10.593221 },
+      { timestamp: (now - MINUTE) / SECOND, value: 10.570825 },
     ],
     historical_mini_block_interval_24h: [],
     historical_mini_block_interval_7d: [],
@@ -55,9 +57,9 @@ const HISTORY_FULL_DATA = {
 
 const HISTORY_DELTA_DATA = {
   data: {
-    historical_gas_per_second_3h: { timestamp: Date.now() / SECOND, value: 87762230 },
-    historical_tps_3h: { timestamp: Date.now() / SECOND, value: 211.8 },
-    historical_mini_block_interval_3h: { timestamp: Date.now() / SECOND, value: 10.593221 },
+    historical_gas_per_second_3h: { timestamp: now / SECOND, value: 87762230 },
+    historical_tps_3h: { timestamp: now / SECOND, value: 211.8 },
+    historical_mini_block_interval_3h: { timestamp: now / SECOND, value: 10.593221 },
   },
   type: 'history_delta',
 };
