f5523746bb001028bd95b2935336d5b0f15c925f
tom <tom@ohhhh.me>
2025-10-10 15:30:37 +0200

    get icon url from chainscout

--- ui/optimismSuperchain/components/ChainIcon.tsx
+++ ui/optimismSuperchain/components/ChainIcon.tsx
@@ -4,7 +4,6 @@ import type { ChainConfig } from 'types/multichain';
 
 import getChainTooltipText from 'lib/multichain/getChainTooltipText';
 import getIconUrl from 'lib/multichain/getIconUrl';
-import { useColorModeValue } from 'toolkit/chakra/color-mode';
 import type { ImageProps } from 'toolkit/chakra/image';
 import { Image } from 'toolkit/chakra/image';
 import { Skeleton } from 'toolkit/chakra/skeleton';
@@ -20,11 +19,18 @@ interface Props extends ImageProps {
 const ChainIcon = ({ data, boxSize = 5, borderRadius = 'full', isLoading, withTooltip, ...rest }: Props) => {
   const placeholder = <IconSvg name="networks/icon-placeholder" boxSize={ boxSize } color="text.secondary"/>;
 
-  const iconUrl = useColorModeValue(getIconUrl(data, 'light'), getIconUrl(data, 'dark'));
+  const iconUrl = getIconUrl(data);
 
-  const src = data.config.chain.id === '1' ? '/static/ethereum-icon.svg' : iconUrl;
-
-  const content = <Image src={ src } boxSize={ boxSize } borderRadius={ borderRadius } fallback={ placeholder } { ...rest }/>;
+  const content = (
+    <Image
+      src={ iconUrl }
+      boxSize={ boxSize }
+      borderRadius={ borderRadius }
+      fallback={ placeholder }
+      alt={ `${ data.config.chain.name } chain icon` }
+      { ...rest }
+    />
+  );
 
   if (isLoading) {
     return <Skeleton boxSize={ boxSize } borderRadius={ borderRadius } { ...rest } loading/>;
