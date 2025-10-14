cdcfffbc53734c97244823442e6f2ba2fc882233
tom <tom@ohhhh.me>
2025-10-14 18:27:48 +0200

    Add operator fee display for optimistic rollups

--- mocks/txs/tx.ts
+++ mocks/txs/tx.ts
@@ -283,6 +283,7 @@ export const l2tx: Transaction = {
   l1_fee_scalar: '1.0',
   l1_gas_used: '17060',
   l1_fee: '1584574188135760',
+  operator_fee: '2769347953',
 };
 
 export const stabilityTx: Transaction = {
