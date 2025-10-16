ae2d2b29a37c2b97300be997c493c0059843294b
Max Alekseenko <nnwen0609@gmail.com>
2025-10-16 10:56:30 +0200

    fix test

--- ui/marketplace/essentialDapps/revoke/Revoke.pw.tsx
+++ ui/marketplace/essentialDapps/revoke/Revoke.pw.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import * as opSuperchainMock from 'mocks/multichain/opSuperchain';
 import type { TestFnArgs } from 'playwright/lib';
 import { test, expect } from 'playwright/lib';
 
 import Revoke from './Revoke';
 
 const ESSENTIAL_DAPPS_CONFIG = JSON.stringify({
-  revoke: { chains: [ '1' ] },
+  revoke: { chains: [ opSuperchainMock.chainDataA.config.chain.id ] },
 });
 
 test('base view +@dark-mode +@mobile', async({ render, mockEnvs, mockEssentialDappsChainsConfig }: TestFnArgs) => {
