f21b0365f8ab7c637c706c523aed7a4a138e24dc
isstuev <isstuev@gmail.com>
2025-10-16 16:01:00 +0200

    add margin for dex and other links

--- ui/shared/tx/interpretation/TxInterpretation.tsx
+++ ui/shared/tx/interpretation/TxInterpretation.tsx
@@ -173,7 +173,7 @@ const TxInterpretationElementByType = (
       })();
 
       return (
-        <chakra.span display="inline-flex" alignItems="center" verticalAlign="top" _notFirst={{ marginLeft: 1 }} gap={ 1 }>
+        <chakra.span display="inline-flex" alignItems="center" verticalAlign="top" _notFirst={{ marginLeft: 1 }} gap={ 1 } mr={ 2 }>
           { icon && <Image src={ icon } alt={ value.name } width={ 5 } height={ 5 }/> }
           { name }
         </chakra.span>
