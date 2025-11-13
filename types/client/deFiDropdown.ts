50d0f69f4c172b723d5c7e4b817d3d224ab92d97
tom <tom@ohhhh.me>
2025-11-13 15:32:09 +0100

    UI/UX: Dropdown DeFi button fixes
    Resolves #3104

--- types/client/deFiDropdown.ts
+++ types/client/deFiDropdown.ts
@@ -2,7 +2,7 @@ import type { IconName } from 'ui/shared/IconSvg';
 
 export type DeFiDropdownItem = {
   text: string;
-  icon: IconName;
+  icon?: IconName;
 } & (
   { dappId: string; url?: never } |
   { url: string; dappId?: never }
