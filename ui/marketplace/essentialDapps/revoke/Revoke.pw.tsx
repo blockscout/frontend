f448b8726480a17d923e8f95953872d6cfcec113
Igor Stuev <108066576+isstuev@users.noreply.github.com>
2025-12-18 20:34:34 +0700

    replace bg-position animation with faster one (#3189)
    * replace bg-position animation with faster one
    
    * fix tests

--- ui/marketplace/essentialDapps/revoke/Revoke.pw.tsx
+++ ui/marketplace/essentialDapps/revoke/Revoke.pw.tsx
@@ -10,12 +10,13 @@ const ESSENTIAL_DAPPS_CONFIG = JSON.stringify({
   revoke: { chains: [ opSuperchainMock.chainDataA.id ] },
 });
 
-test('base view +@dark-mode +@mobile', async({ render, mockEnvs, mockEssentialDappsChainsConfig }: TestFnArgs) => {
+test('base view +@dark-mode +@mobile', async({ render, mockEnvs, mockEssentialDappsChainsConfig, mockAssetResponse }: TestFnArgs) => {
   await mockEnvs([
     [ 'NEXT_PUBLIC_MARKETPLACE_ENABLED', 'true' ],
     [ 'NEXT_PUBLIC_MARKETPLACE_ESSENTIAL_DAPPS_CONFIG', ESSENTIAL_DAPPS_CONFIG ],
   ]);
   await mockEssentialDappsChainsConfig();
+  await mockAssetResponse(opSuperchainMock.chainDataA.logo as string, './playwright/mocks/image_s.jpg');
 
   const component = await render(<Revoke/>);
 
