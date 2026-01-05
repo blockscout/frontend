8d15222120064aa135b4968b5d4739d748211d60
tom goriunov <tom@ohhhh.me>
2026-01-05 13:19:47 +0100

    Add possibility to change timestamp format (#3201)
    * add global setting to change between local and utc time
    
    * add control to change time format on detailed views
    
    * tweaks
    
    * adapt uptime graphs to work with utc time from settings
    
    * fix tests
    
    * add maxDiffPixels for SearchResults test
    
    * fix test

--- ui/pages/BlockCountdown.pw.tsx
+++ ui/pages/BlockCountdown.pw.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import { test, expect } from 'playwright/lib';
+import * as pwConfig from 'playwright/utils/config';
 
 import BlockCountdown from './BlockCountdown';
 
-test('short period until the block +@mobile', async({ render, mockApiResponse }) => {
+test.describe('short period until the block', () => {
   const height = '1234567890';
   const hooksConfig = {
     router: {
@@ -12,25 +13,38 @@ test('short period until the block +@mobile', async({ render, mockApiResponse })
     },
   };
 
-  await mockApiResponse('general:block_countdown', {
-    result: {
-      CountdownBlock: height,
-      CurrentBlock: '1234567700',
-      RemainingBlock: '190',
-      EstimateTimeInSec: String(24 * 60 * 60 + 3 * 60 * 60 + 42 * 60 + 11),
-    },
-  }, {
-    queryParams: {
-      module: 'block',
-      action: 'getblockcountdown',
-      blockno: height,
-    },
+  test.beforeEach(async({ mockApiResponse }) => {
+    await mockApiResponse('general:block_countdown', {
+      result: {
+        CountdownBlock: height,
+        CurrentBlock: '1234567700',
+        RemainingBlock: '190',
+        EstimateTimeInSec: String(24 * 60 * 60 + 3 * 60 * 60 + 42 * 60 + 11),
+      },
+    }, {
+      queryParams: {
+        module: 'block',
+        action: 'getblockcountdown',
+        blockno: height,
+      },
+    });
   });
-  const component = await render(<BlockCountdown hideCapybaraRunner/>, { hooksConfig });
-  await expect(component).toHaveScreenshot();
+
+  test('desktop', async({ render }) => {
+    const component = await render(<BlockCountdown hideCapybaraRunner/>, { hooksConfig });
+    await expect(component).toHaveScreenshot();
+  });
+
+  test.describe('mobile', () => {
+    test.use({ viewport: pwConfig.viewport.mobile });
+    test('base view', async({ render }) => {
+      const component = await render(<BlockCountdown hideCapybaraRunner/>, { hooksConfig });
+      await expect(component).toHaveScreenshot();
+    });
+  });
 });
 
-test('long period until the block +@mobile', async({ render, mockApiResponse }) => {
+test.describe('long period until the block', () => {
   const height = '123456789012345678901234567890';
   const hooksConfig = {
     router: {
@@ -38,20 +52,33 @@ test('long period until the block +@mobile', async({ render, mockApiResponse }) 
     },
   };
 
-  await mockApiResponse('general:block_countdown', {
-    result: {
-      CountdownBlock: height,
-      CurrentBlock: '1234567700',
-      RemainingBlock: '123456789012345678900000000190',
-      EstimateTimeInSec: String(1234567890 * 24 * 60 * 60 + 3 * 60 * 60 + 42 * 60 + 11),
-    },
-  }, {
-    queryParams: {
-      module: 'block',
-      action: 'getblockcountdown',
-      blockno: height,
-    },
+  test.beforeEach(async({ mockApiResponse }) => {
+    await mockApiResponse('general:block_countdown', {
+      result: {
+        CountdownBlock: height,
+        CurrentBlock: '1234567700',
+        RemainingBlock: '123456789012345678900000000190',
+        EstimateTimeInSec: String(1234567890 * 24 * 60 * 60 + 3 * 60 * 60 + 42 * 60 + 11),
+      },
+    }, {
+      queryParams: {
+        module: 'block',
+        action: 'getblockcountdown',
+        blockno: height,
+      },
+    });
   });
-  const component = await render(<BlockCountdown hideCapybaraRunner/>, { hooksConfig });
-  await expect(component).toHaveScreenshot();
+
+  test('desktop', async({ render }) => {
+    const component = await render(<BlockCountdown hideCapybaraRunner/>, { hooksConfig });
+    await expect(component).toHaveScreenshot();
+  });
+
+  test.describe('mobile', () => {
+    test.use({ viewport: pwConfig.viewport.mobile });
+    test('base view', async({ render }) => {
+      const component = await render(<BlockCountdown hideCapybaraRunner/>, { hooksConfig });
+      await expect(component).toHaveScreenshot();
+    });
+  });
 });
