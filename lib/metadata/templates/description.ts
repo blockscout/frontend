f2d7302f4c53d65ccc0875dcd5f524635bcdf02f
isstuev <isstuev@gmail.com>
2026-02-10 11:48:38 +0100

    fix types

--- lib/metadata/templates/description.ts
+++ lib/metadata/templates/description.ts
@@ -98,6 +98,7 @@ const TEMPLATE_MAP: Record<Route['pathname'], string> = {
   '/api/csrf': DEFAULT_TEMPLATE,
   '/api/healthz': DEFAULT_TEMPLATE,
   '/api/config': DEFAULT_TEMPLATE,
+  '/api/monitoring/pageview': DEFAULT_TEMPLATE,
 };
 
 const TEMPLATE_MAP_ENHANCED: Partial<Record<Route['pathname'], string>> = {
