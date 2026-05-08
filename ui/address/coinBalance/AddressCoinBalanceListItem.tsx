7f548636e0e1bfdc21b817a052f5f67d9879e465
Elemont <elemontfx@outlook.com>
2026-05-08 14:37:45 +1000

    fix(frontend): stack balance history mobile rows

--- ui/address/coinBalance/AddressCoinBalanceListItem.tsx
+++ ui/address/coinBalance/AddressCoinBalanceListItem.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import type { AddressCoinBalanceHistoryItem } from 'types/api/address';
 import type { ClusterChainConfig } from 'types/multichain';
 
+import config from 'configs/app';
 import { currencyUnits } from 'lib/units';
 import { Skeleton } from 'toolkit/chakra/skeleton';
 import { ZERO } from 'toolkit/utils/consts';
@@ -14,9 +15,7 @@ import TxEntity from 'ui/shared/entities/tx/TxEntity';
 import ListItemMobile from 'ui/shared/ListItemMobile/ListItemMobile';
 import NativeTokenIcon from 'ui/shared/NativeTokenIcon';
 import TimeWithTooltip from 'ui/shared/time/TimeWithTooltip';
-import NativeCoinValue from 'ui/shared/value/NativeCoinValue';
 import SimpleValue from 'ui/shared/value/SimpleValue';
-import TokenValue from 'ui/shared/value/TokenValue';
 import { WEI_DECIMALS } from 'ui/shared/value/utils';
 
 type Props = AddressCoinBalanceHistoryItem & {
@@ -29,68 +28,52 @@ const AddressCoinBalanceListItem = (props: Props) => {
   const decimals = Number(props.token?.decimals ?? WEI_DECIMALS);
   const deltaBn = BigNumber(props.delta).div(BigNumber(10).pow(decimals));
   const isPositiveDelta = deltaBn.gte(ZERO);
-  const value = props.token ? (
-    <TokenValue
-      token={ props.token }
-      amount={ props.value }
-      loading={ props.isLoading }
-      fontWeight={ 600 }
-      maxW="100%"
-    />
-  ) : (
-    <NativeCoinValue
-      amount={ props.value }
-      loading={ props.isLoading }
-      fontWeight={ 600 }
-      maxW="100%"
-    />
-  );
+  const ticker = props.token?.symbol || currencyUnits.ether;
   const asset = props.token ? (
     <TokenEntity
       token={ props.token }
       isLoading={ props.isLoading }
       fontWeight={ 700 }
-      maxW="180px"
+      maxW="100%"
+      noCopy
     />
   ) : (
     <Flex alignItems="center" columnGap={ 2 } minW={ 0 }>
       <NativeTokenIcon boxSize={ 5 } isLoading={ props.isLoading }/>
       <Skeleton loading={ props.isLoading } fontWeight={ 700 } overflow="hidden" textOverflow="ellipsis">
-        { currencyUnits.ether }
+        { config.chain.currency.name || currencyUnits.ether }
       </Skeleton>
     </Flex>
   );
 
   return (
     <ListItemMobile rowGap={ 2 }>
-      <Flex justifyContent="space-between" w="100%" columnGap={ 3 } alignItems="flex-start">
-        <Flex minW={ 0 } maxW="64%">
-          { value }
-        </Flex>
-        <Flex flexShrink={ 0 } maxW="36%" justifyContent="flex-end">
-          <Skeleton loading={ props.isLoading } maxW="100%" overflow="hidden">
-            <Stat.Root
-              display="inline-flex"
-              alignItems="center"
-              maxW="100%"
-              flexGrow="0"
-              colorPalette={ isPositiveDelta ? 'green' : 'red' }
-              size="sm"
-            >
-              <Stat.ValueText fontWeight={ 600 } maxW="100%" overflow="hidden">
-                <SimpleValue
-                  value={ deltaBn }
-                  loading={ props.isLoading }
-                />
-              </Stat.ValueText>
-              { isPositiveDelta ? <Stat.UpIndicator/> : <Stat.DownIndicator/> }
-            </Stat.Root>
-          </Skeleton>
-        </Flex>
+      <Flex w="100%" minW={ 0 }>
+        <Skeleton loading={ props.isLoading } maxW="100%" overflow="hidden">
+          <Stat.Root
+            display="inline-flex"
+            alignItems="center"
+            maxW="100%"
+            flexGrow="0"
+            colorPalette={ isPositiveDelta ? 'green' : 'red' }
+            size="sm"
+          >
+            { isPositiveDelta ? <Stat.UpIndicator flexShrink={ 0 }/> : <Stat.DownIndicator flexShrink={ 0 }/> }
+            <Stat.ValueText fontWeight={ 600 } maxW="100%" overflow="hidden" display="inline-flex" columnGap={ 1 }>
+              <SimpleValue
+                value={ deltaBn }
+                loading={ props.isLoading }
+              />
+              <Skeleton loading={ props.isLoading } flexShrink={ 0 }>
+                { ticker }
+              </Skeleton>
+            </Stat.ValueText>
+          </Stat.Root>
+        </Skeleton>
       </Flex>
       <Flex columnGap={ 2 } w="100%" alignItems="center" minW={ 0 }>
         <Skeleton loading={ props.isLoading } fontWeight={ 500 } flexShrink={ 0 }>Asset</Skeleton>
-        <Flex minW={ 0 }>
+        <Flex minW={ 0 } flex="1">
           { asset }
         </Flex>
       </Flex>
@@ -106,13 +89,13 @@ const AddressCoinBalanceListItem = (props: Props) => {
       </Flex>
       { props.transaction_hash && (
         <Flex columnGap={ 2 } w="100%" minW={ 0 }>
-          <Skeleton loading={ props.isLoading } fontWeight={ 500 } flexShrink={ 0 }>Txs</Skeleton>
+          <Skeleton loading={ props.isLoading } fontWeight={ 500 } flexShrink={ 0 }>Txid</Skeleton>
           <TxEntity
             hash={ props.transaction_hash }
             isLoading={ props.isLoading }
             noIcon
             fontWeight={ 700 }
-            maxW="150px"
+            maxW="100%"
           />
         </Flex>
       ) }
